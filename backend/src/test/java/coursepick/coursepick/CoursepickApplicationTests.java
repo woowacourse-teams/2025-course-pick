886ccf91615f560c55283d2d1743d6cdcffdbe73
이창근 <dlckdrms0517@gmail.com>
2025-07-10 14:35:13 +0900

    백엔드 프로젝트 세팅 (#2)
    * chore: gitignore 추가
    
    * feat: 프로젝트 세팅
    
    * docs: 백엔드 개발문서 작성

---
+++ backend/src/test/java/coursepick/coursepick/CoursepickApplicationTests.java
@@ -0,0 +1,12 @@
+package coursepick.coursepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursepickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
