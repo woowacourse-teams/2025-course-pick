4eda2b53ab2c0e5f58dd49d69b685cc81c8e0fd3
Giovanni Junseo Kim <giovannijunseokim@gmail.com>
2025-07-16 16:59:49 +0900

    코스 조회 도메인 구현 (#27)
    * feat: JUnit5 및 Kotest 설정 추가
    
    * feat: 위도 값 객체 구현
    
    * test: Latitude 유효성 검사 테스트 추가
    
    * feat: 경도 값 객체 구현
    
    * test: Longitude 유효성 검사 테스트 추가
    
    * refactor: 위도, 경도 값 Double로 변경
    
    * feat: 코스 이름 객체 구현
    
    * test: CourseName 유효성 검사 테스트 추가
    
    * style: ktlint 적용
    
    * feat: 거리 도메인 구현
    
    * test: 거리 도메인 테스트 추가
    
    * feat: 길이 도메인 모델 생성
    
    * test: Length 도메인 테스트 작성
    
    * feat: Coordinate 도메인 모델 생성
    
    * feat: Course 도메인 모델 생성
    
    * test: Course 도메인 테스트 추가
    
    * style: ktlint 적용
    
    ---------
    
    Co-authored-by: 지원 <cheerfulboys@naver.com>
    Co-authored-by: DoableTuple <doabletuple@gmail.com>

---
+++ android/app/src/main/java/io/coursepick/coursepick/domain/Course.kt
@@ -0,0 +1,13 @@
+package io.coursepick.coursepick.domain
+
+data class Course(
+    private val id: Long,
+    private val name: CourseName,
+    private val distance: Distance,
+    private val length: Length,
+    private val coordinates: List<Coordinate>,
+) {
+    init {
+        require(coordinates.isNotEmpty())
+    }
+}
---
+++ android/app/src/test/java/io/coursepick/coursepick/domain/CourseNameTest.kt
@@ -0,0 +1,13 @@
+package io.coursepick.coursepick.domain
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class CourseNameTest {
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "0123456789012345678901234567890"])
+    fun `코스 이름은 2자 이상 30자 이하이다`(value: String) {
+        assertThrows<IllegalArgumentException> { CourseName(value) }
+    }
+}
