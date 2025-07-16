9a801d4a7cc6c10d130b2b5845497018767c0351
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-07-16 13:49:36 +0900

    fix: ktlint 적용되지 않는 버그 수정

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,5 +2,14 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.ktlint)
+    alias(libs.plugins.ktlint) apply false
+}
+
+allprojects {
+    apply(
+        plugin =
+            rootProject.libs.plugins.ktlint
+                .get()
+                .pluginId,
+    )
 }
