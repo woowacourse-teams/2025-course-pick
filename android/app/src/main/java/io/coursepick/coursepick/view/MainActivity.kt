d6353df35d1044594eaae811b5711383f5a8b676
DoableTuple <doabletuple@gmail.com>
2025-07-17 20:22:28 +0900

    refactor: MainActivity 패키지 이동

--- android/app/src/main/java/io/coursepick/coursepick/view/MainActivity.kt
+++ android/app/src/main/java/io/coursepick/coursepick/view/MainActivity.kt
@@ -1,20 +1,37 @@
-package io.coursepick.coursepick
+package io.coursepick.coursepick.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import io.coursepick.coursepick.R
+import io.coursepick.coursepick.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val courseAdapter =
+            CourseAdapter { course: CourseItem ->
+                viewModel.select(course)
+            }
+        binding.adapter = courseAdapter
+
+        viewModel.state.observe(this) { state: MainUiState ->
+            // Success
+            courseAdapter.submitList(state.courses)
         }
     }
 }
