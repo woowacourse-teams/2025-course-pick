b70b3fe9467a15c288d7a76a2a3338bc14e15abc
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-07-18 21:12:01 +0900

    feat: Course를 지도에 그리는 기능 구현

--- android/app/src/main/java/io/coursepick/coursepick/KakaoMapManager.kt
+++ android/app/src/main/java/io/coursepick/coursepick/KakaoMapManager.kt
@@ -12,11 +12,20 @@ import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
 import com.kakao.vectormap.camera.CameraUpdate
 import com.kakao.vectormap.camera.CameraUpdateFactory
+import com.kakao.vectormap.route.RouteLineLayer
+import com.kakao.vectormap.route.RouteLineOptions
+import com.kakao.vectormap.route.RouteLineSegment
+import com.kakao.vectormap.route.RouteLineStyle
+import com.kakao.vectormap.route.RouteLineStyles
+import com.kakao.vectormap.route.RouteLineStylesSet
+import io.coursepick.coursepick.domain.Course
 
 class KakaoMapManager(
     private val mapView: MapView,
     private val locationProvider: LocationProvider = LocationProvider(mapView.context),
 ) {
+    private var kakaoMap: KakaoMap? = null
+
     init {
         mapView.start(
             object : MapLifeCycleCallback() {
@@ -27,7 +36,8 @@ class KakaoMapManager(
             object : KakaoMapReadyCallback() {
                 @SuppressLint("MissingPermission")
                 override fun onMapReady(kakaoMap: KakaoMap) {
-                    moveToCurrentLocation(kakaoMap)
+                    this@KakaoMapManager.kakaoMap = kakaoMap
+                    moveToCurrentLocation()
                 }
             },
         )
@@ -37,17 +47,44 @@ class KakaoMapManager(
 
     fun pause() = mapView.pause()
 
+    fun draw(course: Course) {
+        val kakaoMap: KakaoMap = kakaoMap ?: return
+        val layer: RouteLineLayer = kakaoMap.routeLineManager?.layer ?: return
+
+        val latLngs: List<LatLng> = course.toLatLngs()
+        val stylesSet: RouteLineStylesSet =
+            RouteLineStylesSet.from(
+                "blueStyles",
+                RouteLineStyles.from(RouteLineStyle.from(16f, 0xFF0000FF.toInt())),
+            )
+        val segment: RouteLineSegment =
+            RouteLineSegment
+                .from(latLngs)
+                .setStyles(stylesSet.getStyles(0))
+        val options: RouteLineOptions =
+            RouteLineOptions
+                .from(segment)
+                .setStylesSet(stylesSet)
+
+        layer.addRouteLine(options)
+    }
+
     @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
-    private fun moveToCurrentLocation(kakaoMap: KakaoMap) {
+    private fun moveToCurrentLocation() {
         locationProvider.fetchCurrentLocation(
             onSuccess = { location: Location ->
                 val latLng = LatLng.from(location.latitude, location.longitude)
                 val cameraUpdate: CameraUpdate = CameraUpdateFactory.newCenterPosition(latLng)
-                kakaoMap.moveCamera(cameraUpdate)
+                kakaoMap?.moveCamera(cameraUpdate)
             },
-            onFailure = {
-                Log.e("Location", "위치 조회 실패: ${it.message}")
+            onFailure = { exception: Exception ->
+                Log.e("Location", "위치 조회 실패: ${exception.message}")
             },
         )
     }
+
+    private fun Course.toLatLngs() =
+        coordinates.map { coordinate ->
+            LatLng.from(coordinate.latitude.value, coordinate.longitude.value)
+        }
 }
