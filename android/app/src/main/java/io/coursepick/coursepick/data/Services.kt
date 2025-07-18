f43dfb5a5ce083cc0aa0338fe785444a9bc5eeed
DoableTuple <doabletuple@gmail.com>
2025-07-18 18:57:54 +0900

    refactor: CourseService 초기화 로직 개선

--- android/app/src/main/java/io/coursepick/coursepick/data/Services.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/Services.kt
@@ -6,32 +6,26 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Converter
 import retrofit2.Retrofit
 
 object Services {
-    private val client: OkHttpClient = client()
-    private val converterFactory: Converter.Factory =
-        Json.asConverterFactory("application/json".toMediaType())
+    private val loggingLevel =
+        if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    private val interceptor = HttpLoggingInterceptor().setLevel(loggingLevel)
+
+    private val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
     private val retrofit =
         Retrofit
             .Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .client(client)
-            .addConverterFactory(converterFactory)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
 
     val courseService: CourseService = retrofit.create(CourseService::class.java)
-
-    private fun client(): OkHttpClient {
-        val isDebugBuild: Boolean = BuildConfig.DEBUG
-        val loggingLevel =
-            if (isDebugBuild) {
-                HttpLoggingInterceptor.Level.BODY
-            } else {
-                HttpLoggingInterceptor.Level.NONE
-            }
-        val interceptor = HttpLoggingInterceptor().setLevel(loggingLevel)
-        return OkHttpClient.Builder().addInterceptor(interceptor).build()
-    }
 }
