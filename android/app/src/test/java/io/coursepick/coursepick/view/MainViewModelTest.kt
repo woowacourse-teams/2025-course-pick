0c7597b5d062929fbfd03e9c8f9515a3cbe4895b
지원 <cheerfulboys@naver.com>
2025-07-18 14:23:46 +0900

    test: MainViewModel 단위 테스트 추가

---
+++ android/app/src/test/java/io/coursepick/coursepick/view/MainViewModelTest.kt
@@ -0,0 +1,97 @@
+package io.coursepick.coursepick.view
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import io.coursepick.coursepick.view.fixtures.COURSE_20
+import io.coursepick.coursepick.view.fixtures.MOCK_COURSES
+import io.coursepick.coursepick.view.fixtures.MockRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+@ExtendWith(InstantTaskExecutorExtension::class)
+class MainViewModelTest {
+    private val mockRepository = MockRepository(MOCK_COURSES)
+    private lateinit var mainViewModel: MainViewModel
+
+    @Test
+    fun `초기 상태는 첫 번째 코스가 선택된 상태이다`() {
+        // given
+        mainViewModel = MainViewModel(mockRepository)
+        val expected =
+            MainUiState(
+                MOCK_COURSES.mapIndexed { index, course ->
+                    CourseItem(course, selected = index == 0)
+                },
+            )
+
+        // then
+        assertThat(mainViewModel.state.getOrAwaitValue()).isEqualTo(expected)
+    }
+
+    @Test
+    fun `하나의 코스를 선택하면 나머지는 해제되고 해당 코스만 선택된다`() {
+        // given
+        mainViewModel = MainViewModel(mockRepository)
+        val expected =
+            MainUiState(
+                MOCK_COURSES.map { course ->
+                    CourseItem(course, selected = course == COURSE_20)
+                },
+            )
+
+        // when
+        mainViewModel.select(CourseItem(COURSE_20, selected = false))
+
+        // then
+        assertThat(mainViewModel.state.getOrAwaitValue()).isEqualTo(expected)
+    }
+
+    @Test
+    fun `이미 선택된 코스가 선택되면 그대로 반환한다`() {
+        // given
+        mainViewModel = MainViewModel(mockRepository)
+        mainViewModel.select(CourseItem(COURSE_20, selected = false))
+
+        val expected =
+            MainUiState(
+                MOCK_COURSES.map { course ->
+                    CourseItem(course, selected = course == COURSE_20)
+                },
+            )
+
+        // when
+        mainViewModel.select(CourseItem(COURSE_20, selected = true))
+
+        // then
+        assertThat(mainViewModel.state.getOrAwaitValue()).isEqualTo(expected)
+    }
+}
+
+fun <T> LiveData<T>.getOrAwaitValue(
+    time: Long = 2,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer =
+        object : Observer<T> {
+            override fun onChanged(value: T) {
+                data = value
+                latch.countDown()
+                this@getOrAwaitValue.removeObserver(this)
+            }
+        }
+
+    this.observeForever(observer)
+
+    if (!latch.await(time, timeUnit)) {
+        throw TimeoutException("LiveData value was never set.")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    return data as T
+}
