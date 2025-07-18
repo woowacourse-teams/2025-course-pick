7570780c8892264914c7ccf78f7eba9c89d4ba81
DoableTuple <doabletuple@gmail.com>
2025-07-18 20:24:11 +0900

    feat: 코스 목록 API 응답을 도메인 모델로 변환하는 함수 추가

--- android/app/src/main/java/io/coursepick/coursepick/data/GetCoursesResponseItem.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/GetCoursesResponseItem.kt
@@ -1,5 +1,12 @@
 package io.coursepick.coursepick.data
 
+import io.coursepick.coursepick.domain.Coordinate
+import io.coursepick.coursepick.domain.Course
+import io.coursepick.coursepick.domain.CourseName
+import io.coursepick.coursepick.domain.Distance
+import io.coursepick.coursepick.domain.Latitude
+import io.coursepick.coursepick.domain.Length
+import io.coursepick.coursepick.domain.Longitude
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -20,4 +27,23 @@ data class GetCoursesResponseItem(
         val distance: Double?,
         val length: Double?,
     )
+
+    companion object {
+        fun GetCoursesResponseItem.toCourse(): Course {
+            val coordinates: List<Coordinate> =
+                geometry?.coordinates?.map { coordinate: List<Double?>? ->
+                    val latitude = Latitude(coordinate?.get(0) ?: 0.0)
+                    val longitude = Longitude(coordinate?.get(1) ?: 0.0)
+                    Coordinate(latitude, longitude)
+                } ?: emptyList()
+
+            return Course(
+                id = 0,
+                name = CourseName(properties?.name ?: ""),
+                distance = Distance(properties?.distance?.toInt() ?: 0),
+                length = Length(properties?.distance?.toInt() ?: 0),
+                coordinates = coordinates,
+            )
+        }
+    }
 }
