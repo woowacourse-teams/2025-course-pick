6d725bd1f722dd9011c7a75fb5d3b563a33d96ea
DoableTuple <doabletuple@gmail.com>
2025-07-18 17:34:41 +0900

    refactor: Http 로그가 Debug 빌드에서만 출력되도록 변경

--- android/app/src/main/java/io/coursepick/coursepick/data/Services.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/Services.kt
@@ -6,20 +6,32 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
 import retrofit2.Retrofit
 
 object Services {
-    private val client = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor()).build()
-
-    private val contentType = "application/json".toMediaType()
-
+    private val client: OkHttpClient = client()
+    private val converterFactory: Converter.Factory =
+        Json.asConverterFactory("application/json".toMediaType())
     private val retrofit =
         Retrofit
             .Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .client(client)
-            .addConverterFactory(Json.asConverterFactory(contentType))
+            .addConverterFactory(converterFactory)
             .build()
 
     val courseService: CourseService = retrofit.create(CourseService::class.java)
+
+    private fun client(): OkHttpClient {
+        val isDebugBuild: Boolean = BuildConfig.DEBUG
+        val loggingLevel =
+            if (isDebugBuild) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        val interceptor = HttpLoggingInterceptor().setLevel(loggingLevel)
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
 }
