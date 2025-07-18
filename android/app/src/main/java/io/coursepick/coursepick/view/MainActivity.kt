6f04acda3652033eb195ba36d2588d3c57e5c8cd
지원 <cheerfulboys@naver.com>
2025-07-18 20:32:53 +0900

    feat: 뒤로가기 버튼 2번 누르면 종료 기능 추가

--- android/app/src/main/java/io/coursepick/coursepick/view/MainActivity.kt
+++ android/app/src/main/java/io/coursepick/coursepick/view/MainActivity.kt
@@ -1,17 +1,30 @@
 package io.coursepick.coursepick.view
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import io.coursepick.coursepick.R
 import io.coursepick.coursepick.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val viewModel: MainViewModel by viewModels()
     private val courseAdapter by lazy { CourseAdapter(viewModel::select) }
+
+    private var backPressedTime: Long = 0
+    private val finishIntervalTime = 2000L
+
+    private val callback =
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                this@MainActivity.exitOnDoubleBackPressed()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +39,26 @@ class MainActivity : AppCompatActivity() {
 
         binding.adapter = courseAdapter
         setUpObservers(courseAdapter)
+        this.onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private fun exitOnDoubleBackPressed() {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - backPressedTime >= finishIntervalTime) {
+            backPressedTime = currentTime
+            showPressBackAgainToast()
+        } else {
+            finish()
+        }
+    }
+
+    private fun showPressBackAgainToast() {
+        Toast
+            .makeText(
+                this,
+                R.string.toast_back_press_exit,
+                Toast.LENGTH_SHORT,
+            ).show()
     }
 
     private fun setUpObservers(courseAdapter: CourseAdapter) {
