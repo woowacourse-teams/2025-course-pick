234417afc1efece977a44867bf1218bfd8b42555
doabletuple <161921046+doabletuple@users.noreply.github.com>
2025-07-18 19:09:15 +0900

    코스 목록 조회 API 연결 (#49)
    * feat: Retrofit 의존성 추가
    
    * feat: HTTPS 통신을 위한 설정
    
    * feat: kotlinx.serialization 의존성 추가
    
    * fix: kotlinx.serialization 버전 하향
    
    * feat: GetCoursesResponse 구현
    
    * feat: local.properties에 Base url 추가
    
    * feat: OkHttp 의존성 추가
    
    * fix: OkHttp 버전 하향
    
    * feat: 서버 통신을 위한 의존성 추가
    
    * feat: 코스 목록 DTO 추가
    
    * feat: 코스 목록 조회를 위한 서비스 구현
    
    * refactor: courseService 접근 제한자 변경
    
    * refactor: courseService 접근 제한자 변경
    
    * refactor: 코스 DTO 관련 클래스들을 하나로 통합
    
    * refactor: Http 로그가 Debug 빌드에서만 출력되도록 변경
    
    * refactor: 코스 DTO 이름 변경
    
    * refactor: CourseService 초기화 로직 개선
    
    ---------
    
    Co-authored-by: Giovanni Junseo Kim <kingjunseo@gmail.com>

---
+++ android/app/src/main/java/io/coursepick/coursepick/data/GetCoursesResponseItem.kt
@@ -0,0 +1,23 @@
+package io.coursepick.coursepick.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetCoursesResponseItem(
+    val type: String?,
+    val geometry: Geometry?,
+    val properties: Properties?,
+) {
+    @Serializable
+    data class Geometry(
+        val type: String?,
+        val coordinates: List<List<Double?>?>?,
+    )
+
+    @Serializable
+    data class Properties(
+        val name: String?,
+        val distance: Double?,
+        val length: Double?,
+    )
+}
