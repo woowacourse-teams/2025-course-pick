f738f356867856ebf76c9046339ba5037d14ef2a
doabletuple <161921046+doabletuple@users.noreply.github.com>
2025-07-18 19:09:15 +0900

    코스 목록 조회 API 연결 (#49)
    * feat: Retrofit 의존성 추가
    
    * feat: HTTPS 통신을 위한 설정
    
    * feat: kotlinx.serialization 의존성 추가
    
    * fix: kotlinx.serialization 버전 하향
    
    * feat: GetCoursesResponse 구현
    
    * feat: local.properties에 Base url 추가
    
    * feat: OkHttp 의존성 추가
    
    * fix: OkHttp 버전 하향
    
    * feat: 서버 통신을 위한 의존성 추가
    
    * feat: 코스 목록 DTO 추가
    
    * feat: 코스 목록 조회를 위한 서비스 구현
    
    * refactor: courseService 접근 제한자 변경
    
    * refactor: courseService 접근 제한자 변경
    
    * refactor: 코스 DTO 관련 클래스들을 하나로 통합
    
    * refactor: Http 로그가 Debug 빌드에서만 출력되도록 변경
    
    * refactor: 코스 DTO 이름 변경
    
    * refactor: CourseService 초기화 로직 개선
    
    ---------
    
    Co-authored-by: Giovanni Junseo Kim <kingjunseo@gmail.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,19 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.android.junit5)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.serialization)
 }
+
+private val localProperties: Properties =
+    Properties().apply {
+        val localFile = rootProject.file("local.properties")
+        if (localFile.exists()) {
+            load(localFile.inputStream())
+        }
+    }
 
 android {
     namespace = "io.coursepick.coursepick"
@@ -19,36 +30,54 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("boolean", "DEBUG", "true")
+            buildConfigField("String", "BASE_URL", localProperties["base.url.debug"].toString())
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            buildConfigField("boolean", "DEBUG", "false")
+            buildConfigField("String", "BASE_URL", localProperties["base.url.release"].toString())
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
+
     kotlinOptions {
         jvmTarget = "21"
     }
+
     buildFeatures {
+        buildConfig = true
         dataBinding = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.kotest.runner.junit5)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
