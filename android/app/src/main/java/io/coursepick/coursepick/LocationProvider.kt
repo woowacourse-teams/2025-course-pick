33a9ba5d4d43577789048229b235cc0794036b42
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-07-19 19:16:17 +0900

    style: 파라미터 개수 감소

--- android/app/src/main/java/io/coursepick/coursepick/LocationProvider.kt
+++ android/app/src/main/java/io/coursepick/coursepick/LocationProvider.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.annotation.RequiresPermission
-import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
@@ -37,15 +36,9 @@ class LocationProvider(
 
     private val hasCoarseLocationPermission: Boolean
         get() =
-            ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-            ) == PackageManager.PERMISSION_GRANTED
+            context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
     private val hasFineLocationPermission: Boolean
         get() =
-            ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-            ) == PackageManager.PERMISSION_GRANTED
+            context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 }
