b8ff5c9e9a8f348c85de6e098a699e2afaff562c
Dompoo <dlckdrms0517@gmail.com>
2025-07-19 23:55:24 +0900

    feat: openapi 애노테이션 추가

---
+++ backend/src/main/java/coursepick/coursepick/presentation/CourseWebApi.java
@@ -0,0 +1,35 @@
+package coursepick.coursepick.presentation;
+
+import coursepick.coursepick.presentation.dto.GeoJson;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+@Tag(name = "코스")
+public interface CourseWebApi {
+
+    @Operation(summary = "좌표 근처 1km 내 코스 전체 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(examples = {
+                    @ExampleObject(
+                            name = "위도가 범위 외인 경우",
+                            ref = "#/components/examples/INVALID_LATITUDE_RANGE"
+                    ),
+                    @ExampleObject(
+                            name = "경도가 범위 외인 경우",
+                            ref = "#/components/examples/INVALID_LONGITUDE_RANGE"
+                    )
+            })),
+    })
+    List<GeoJson> findNearbyCourses(
+            @Parameter(example = "37.5165004", required = true) double latitude,
+            @Parameter(example = "127.1040109", required = true) double longitude
+    );
+}
