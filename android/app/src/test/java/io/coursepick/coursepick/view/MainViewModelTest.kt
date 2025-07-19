fa82b8b455224dcbbba65194743221a40e612e18
doabletuple <161921046+doabletuple@users.noreply.github.com>
2025-07-19 17:47:46 +0900

    코스 Repository 구현 (#57)
    * refactor: Latitude, Longitude 가시성 변경
    
    * feat: 코스 목록 API 응답을 도메인 모델로 변환하는 함수 추가
    
    * refactor: 코스 목록을 API 서버에서 가져오도록 변경
    
    * feat: 코루틴 테스트를 위한 의존성 추가
    
    * test: ViewModel 테스트 시 Main Dispatcher 설정 추가
    
    * style: 스타일 컨벤션 준수
    
    * test: 코루틴 테스트용 Main Dispatcher 설정 로직 분리
    
    * refactor: GetCoursesResponseItem 변환 함수를 멤버 함수로 변경
    
    * refactor: 코스 목록 조회 시 null 처리 로직 변경
    
    * refactor: GetCoursesResponseItem 변환 함수명 변경

--- android/app/src/test/java/io/coursepick/coursepick/view/MainViewModelTest.kt
+++ android/app/src/test/java/io/coursepick/coursepick/view/MainViewModelTest.kt
@@ -4,19 +4,27 @@ import io.coursepick.coursepick.domain.Course
 import io.coursepick.coursepick.view.fixtures.COURSE_20
 import io.coursepick.coursepick.view.fixtures.FAKE_COURSES
 import io.coursepick.coursepick.view.fixtures.FakeRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
+@ExperimentalCoroutinesApi
+@ExtendWith(CoroutinesTestExtension::class)
 @ExtendWith(InstantTaskExecutorExtension::class)
 class MainViewModelTest {
-    private val fakeRepository = FakeRepository(FAKE_COURSES)
+    private val fakeRepository = FakeRepository()
     private lateinit var mainViewModel: MainViewModel
+
+    @BeforeEach
+    fun setUp() {
+        mainViewModel = MainViewModel(fakeRepository)
+    }
 
     @Test
     fun `초기 상태에서는 가장 가까운 코스가 선택된다`() {
         // given
-        mainViewModel = MainViewModel(fakeRepository)
         val expected =
             MainUiState(
                 FAKE_COURSES.mapIndexed { index: Int, course: Course ->
@@ -32,7 +40,6 @@ class MainViewModelTest {
     @Test
     fun `하나의 코스를 선택하면 나머지는 해제되고 해당 코스만 선택된다`() {
         // given
-        mainViewModel = MainViewModel(fakeRepository)
         val expected =
             MainUiState(
                 FAKE_COURSES.map { course: Course ->
@@ -50,7 +57,6 @@ class MainViewModelTest {
     @Test
     fun `이미 선택된 코스가 선택되면 해당 코스가 유지된다`() {
         // given
-        mainViewModel = MainViewModel(fakeRepository)
         mainViewModel.select(CourseItem(COURSE_20, selected = false))
 
         val expected =
