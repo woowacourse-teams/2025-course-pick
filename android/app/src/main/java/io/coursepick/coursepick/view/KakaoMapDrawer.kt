2c02c90c7b00e4136d63e65f8f53ca901b519626
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-07-21 21:23:18 +0900

    feat: 현 위치 표시

--- android/app/src/main/java/io/coursepick/coursepick/view/KakaoMapDrawer.kt
+++ android/app/src/main/java/io/coursepick/coursepick/view/KakaoMapDrawer.kt
@@ -1,8 +1,16 @@
 package io.coursepick.coursepick.view
 
+import android.Manifest
 import android.content.Context
+import android.location.Location
+import android.util.Log
+import androidx.annotation.RequiresPermission
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.label.LabelLayer
+import com.kakao.vectormap.label.LabelOptions
+import com.kakao.vectormap.label.LabelStyle
+import com.kakao.vectormap.label.LabelStyles
 import com.kakao.vectormap.route.RouteLineLayer
 import com.kakao.vectormap.route.RouteLineOptions
 import com.kakao.vectormap.route.RouteLineSegment
@@ -15,6 +23,8 @@ import io.coursepick.coursepick.domain.Coordinate
 class KakaoMapDrawer(
     private val context: Context,
 ) {
+    private val locationProvider: LocationProvider = LocationProvider(context)
+
     fun drawCourse(
         kakaoMap: KakaoMap,
         course: CourseItem,
@@ -31,6 +41,32 @@ class KakaoMapDrawer(
         val segment = RouteLineSegment.from(course.toLatLngs()).setStyles(styleSet.getStyles(0))
         val options = RouteLineOptions.from(segment).setStylesSet(styleSet)
         layer.addRouteLine(options)
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    fun drawCurrentLocation(map: KakaoMap) {
+        locationProvider.fetchCurrentLocation(
+            onSuccess = { location: Location ->
+                val styles: LabelStyles? =
+                    map.labelManager
+                        ?.addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.image_current_location)))
+                val options: LabelOptions =
+                    LabelOptions
+                        .from(
+                            LatLng.from(
+                                location.latitude,
+                                location.longitude,
+                            ),
+                        ).setStyles(styles)
+
+                val layer: LabelLayer? = map.labelManager?.layer
+
+                layer?.addLabel(options)
+            },
+            onFailure = { exception: Exception ->
+                Log.e("Location", "위치 조회 실패: ${exception.message}")
+            },
+        )
     }
 
     private fun CourseItem.toLatLngs() = coordinates.map { coordinate: Coordinate -> coordinate.toLatLng() }
