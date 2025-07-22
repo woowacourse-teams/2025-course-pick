59ea726589bf9d0cab2c3a9d1585ad1484e5bd1c
DoableTuple <doabletuple@gmail.com>
2025-07-22 13:54:16 +0900

    feat: CourseItem에 남서, 북동, 남동, 북서 좌표 프로퍼티 추가

--- android/app/src/main/java/io/coursepick/coursepick/view/CourseItem.kt
+++ android/app/src/main/java/io/coursepick/coursepick/view/CourseItem.kt
@@ -2,6 +2,8 @@ package io.coursepick.coursepick.view
 
 import io.coursepick.coursepick.domain.Coordinate
 import io.coursepick.coursepick.domain.Course
+import io.coursepick.coursepick.domain.Latitude
+import io.coursepick.coursepick.domain.Longitude
 
 data class CourseItem(
     private val course: Course,
@@ -12,4 +14,18 @@ data class CourseItem(
     val distance: Int = course.distance.meter
     val length: Int = course.length.meter
     val coordinates: List<Coordinate> = course.coordinates
+
+    private val westernmost: Latitude =
+        coordinates.minBy { coordinate: Coordinate -> coordinate.latitude.value }.latitude
+    private val easternmost: Latitude =
+        coordinates.maxBy { coordinate: Coordinate -> coordinate.latitude.value }.latitude
+    private val southernmost: Longitude =
+        coordinates.minBy { coordinate: Coordinate -> coordinate.longitude.value }.longitude
+    private val northernmost: Longitude =
+        coordinates.maxBy { coordinate: Coordinate -> coordinate.longitude.value }.longitude
+
+    val northwest: Coordinate = Coordinate(westernmost, northernmost)
+    val northeast: Coordinate = Coordinate(easternmost, northernmost)
+    val southeast: Coordinate = Coordinate(easternmost, southernmost)
+    val southwest: Coordinate = Coordinate(westernmost, southernmost)
 }
