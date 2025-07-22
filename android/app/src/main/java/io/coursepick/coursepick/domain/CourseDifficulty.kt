bf94eb9b322f25c501ca3320c8518d6583f0df43
지원 <cheerfulboys@naver.com>
2025-07-22 15:36:54 +0900

    refactor: 난이도 계산 로직을 CourseScore로 이동

--- android/app/src/main/java/io/coursepick/coursepick/domain/CourseDifficulty.kt
+++ android/app/src/main/java/io/coursepick/coursepick/domain/CourseDifficulty.kt
@@ -4,17 +4,4 @@ enum class CourseDifficulty {
     EASY,
     NORMAL,
     HARD,
-    ;
-
-    companion object {
-        operator fun invoke(courseScore: CourseScore): CourseDifficulty {
-            val score: Double = courseScore.value
-
-            return when {
-                score < 3.0 -> EASY
-                score < 7.0 -> NORMAL
-                else -> HARD
-            }
-        }
-    }
 }
