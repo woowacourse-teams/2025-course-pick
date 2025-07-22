a7e7519a40a8679a9c9a142147b37b7781345f9d
지원 <cheerfulboys@naver.com>
2025-07-22 10:30:22 +0900

    refactor: 에러 메세지 추가

--- android/app/src/main/java/io/coursepick/coursepick/domain/CourseScore.kt
+++ android/app/src/main/java/io/coursepick/coursepick/domain/CourseScore.kt
@@ -5,7 +5,7 @@ value class CourseScore(
     val value: Double,
 ) {
     init {
-        require(value > MIN_SCORE && value < MAX_SCORE) { }
+        require(value > MIN_SCORE && value < MAX_SCORE) { "점수는 $MIN_SCORE 초과 $MAX_SCORE 미만이어야 합니다. 입력값: $value" }
     }
 
     companion object {
