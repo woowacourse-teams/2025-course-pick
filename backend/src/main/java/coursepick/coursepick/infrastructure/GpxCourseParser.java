660f522816608b38a794d7011b637d872e451959
GiSeok Kang <ime.rokz@gmail.com>
2025-07-23 17:15:21 +0900

    feat: GPX -> Course 파싱 기능 구현

---
+++ backend/src/main/java/coursepick/coursepick/infrastructure/GpxCourseParser.java
@@ -0,0 +1,39 @@
+package coursepick.coursepick.infrastructure;
+
+import coursepick.coursepick.domain.Coordinate;
+import coursepick.coursepick.domain.Course;
+import coursepick.coursepick.domain.CourseParser;
+import io.jenetics.jpx.GPX;
+import io.jenetics.jpx.Length;
+import io.jenetics.jpx.Track;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Component
+public class GpxCourseParser implements CourseParser {
+    public List<Course> parse(InputStream fileStream) {
+        try {
+            GPX gpx = GPX.Reader.of(GPX.Reader.Mode.LENIENT).read(fileStream);
+
+            return gpx.tracks()
+                    .map(track -> new Course(track.getName().orElse("Default"), getCoordinates(track)))
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<Coordinate> getCoordinates(Track track) {
+        return track.getSegments().stream()
+                .flatMap(segment -> segment.getPoints().stream()
+                        .map(point -> new Coordinate(
+                                point.getLatitude().doubleValue(),
+                                point.getLongitude().doubleValue(),
+                                point.getElevation().orElse(Length.of(0, Length.Unit.METER)).doubleValue())
+                        )
+                ).toList();
+    }
+}
