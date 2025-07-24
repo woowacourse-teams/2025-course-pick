61a5965e4ee3a27f491b28e147925d8218e1c181
Giovanni Junseo Kim <giovannijunseokim@gmail.com>
2025-07-24 10:25:29 +0900

    현 위치 실시간 반영 (#112)
    * refactor: 좌표에 그릴 아이콘을 파라미터로 주입받을 수 있도록 수정
    
    * feat: 실시간 위치 받아오는 기능 구현
    
    * feat: 실시간으로 위치를 받아왔을 때, 현 위치를 그리는 기능 구현
    
    * feat: 지도 위의 Label을 모두 지우는 기능 구현
    
    * feat: 실시간 위치를 그리는 기능 Activity Lifecycle 반영해서 구현
    
    * feat: 현위치 이미지 변경
    
    * refactor: 실시간 위치 업데이트 중복 요청 방지
    
    * refactor: Exception 메시지 변경
    
    * refactor: 권한 체크 어노테이션 추가

--- android/app/src/main/java/io/coursepick/coursepick/view/KakaoMapDrawer.kt
+++ android/app/src/main/java/io/coursepick/coursepick/view/KakaoMapDrawer.kt
@@ -2,6 +2,7 @@ package io.coursepick.coursepick.view
 
 import android.content.Context
 import android.location.Location
+import androidx.annotation.DrawableRes
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.label.LabelLayer
@@ -41,10 +42,13 @@ class KakaoMapDrawer(
 
     fun draw(
         map: KakaoMap,
+        @DrawableRes
+        iconResourceId: Int,
         coordinate: Coordinate,
     ) {
         draw(
             map,
+            iconResourceId,
             coordinate.latitude.value,
             coordinate.longitude.value,
         )
@@ -52,23 +56,33 @@ class KakaoMapDrawer(
 
     fun draw(
         map: KakaoMap,
+        @DrawableRes
+        iconResourceId: Int,
         location: Location,
     ) {
         draw(
             map,
+            iconResourceId,
             location.latitude,
             location.longitude,
         )
     }
 
+    fun removeAllLabels(map: KakaoMap) {
+        val layer: LabelLayer = map.labelManager?.layer ?: return
+        layer.removeAll()
+    }
+
     private fun draw(
         map: KakaoMap,
+        @DrawableRes
+        iconResourceId: Int,
         latitude: Double,
         longitude: Double,
     ) {
         val labelManager: LabelManager = map.labelManager ?: return
         val styles: LabelStyles =
-            labelManager.addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.image_current_location)))
+            labelManager.addLabelStyles(LabelStyles.from(LabelStyle.from(iconResourceId)))
                 ?: return
         val options: LabelOptions =
             LabelOptions
