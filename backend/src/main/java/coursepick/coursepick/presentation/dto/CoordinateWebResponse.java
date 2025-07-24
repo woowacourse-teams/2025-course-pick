8a1cf70389b09ca5f942d9420896255892600a8e
Dompoo <dlckdrms0517@gmail.com>
2025-07-24 16:38:06 +0900

    docs: 응답에 API 문서 예시 추가

--- backend/src/main/java/coursepick/coursepick/presentation/dto/CoordinateWebResponse.java
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/CoordinateWebResponse.java
@@ -1,11 +1,14 @@
 package coursepick.coursepick.presentation.dto;
 
 import coursepick.coursepick.domain.Coordinate;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record CoordinateWebResponse(
+        @Schema(example = "37.514167")
         double latitude,
+        @Schema(example = "127.103611")
         double longitude
 ) {
     public static List<CoordinateWebResponse> from(List<Coordinate> coordinates) {
--- backend/src/main/java/coursepick/coursepick/presentation/dto/CourseWebResponse.java
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/CourseWebResponse.java
@@ -2,15 +2,22 @@ package coursepick.coursepick.presentation.dto;
 
 import coursepick.coursepick.application.dto.CourseResponse;
 import coursepick.coursepick.domain.RoadType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record CourseWebResponse(
+        @Schema(example = "1")
         long id,
+        @Schema(example = "석촌호수")
         String name,
+        @Schema(example = "200.123")
         double distance,
+        @Schema(example = "2146.123")
         double length,
+        @Schema(example = "트랙")
         RoadType roadType,
+        @Schema(example = "1.235")
         double difficulty,
         List<CoordinateWebResponse> coordinates
 ) {
