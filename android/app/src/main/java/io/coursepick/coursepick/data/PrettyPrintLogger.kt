ec83f158db47bf44b13d4d635d98b786d2e22a62
Giovanni Junseo Kim <giovannijunseokim@gmail.com>
2025-07-24 19:22:08 +0900

    feat: PrettyPrintLogger 구현 (#126)

---
+++ android/app/src/main/java/io/coursepick/coursepick/data/PrettyPrintLogger.kt
@@ -0,0 +1,21 @@
+package io.coursepick.coursepick.data
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import okhttp3.internal.platform.Platform
+import okhttp3.logging.HttpLoggingInterceptor
+
+class PrettyPrintLogger : HttpLoggingInterceptor.Logger {
+    private val json = Json { prettyPrint = true }
+
+    override fun log(message: String) {
+        val formattedMessage: String = message.toPrettyJsonStringOrThis()
+        Platform.get().log(formattedMessage)
+    }
+
+    private fun String.toPrettyJsonStringOrThis(): String =
+        runCatching {
+            val element: JsonElement = json.parseToJsonElement(this)
+            json.encodeToString(JsonElement.serializer(), element)
+        }.getOrElse { this }
+}
