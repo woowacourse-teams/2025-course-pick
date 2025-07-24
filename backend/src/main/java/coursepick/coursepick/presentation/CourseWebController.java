6d3200764e9f61422ca2cf4aab98a26e48d26702
GiSeok Kang <ime.rokz@gmail.com>
2025-07-24 16:56:57 +0900

    feat: GPX 파일 파싱 로직 추가
    - 파일 확장자 별로 다른 파싱 메서드 호출하도록 다형성 적용

--- backend/src/main/java/coursepick/coursepick/presentation/CourseWebController.java
+++ backend/src/main/java/coursepick/coursepick/presentation/CourseWebController.java
@@ -7,12 +7,15 @@ import coursepick.coursepick.presentation.dto.GeoJson;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -27,13 +30,18 @@ public class CourseWebController implements CourseWebApi {
     @PostMapping("/admin/courses/import")
     public void importCourses(
             @RequestParam("adminToken") String token,
-            @RequestParam("filePath") String filePath
+            @RequestParam("file") MultipartFile file
     ) {
         if (adminToken.isEmpty() || !adminToken.equals(token)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "올바르지 않은 어드민 토큰값 입니다.");
         }
 
-        courseApplicationService.parseAndSaveCourses(filePath);
+        try {
+            String fileExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+            courseApplicationService.parseInputStreamAndSave(file.getInputStream(), fileExtension);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
