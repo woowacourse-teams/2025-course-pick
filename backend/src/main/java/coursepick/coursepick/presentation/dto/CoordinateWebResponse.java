6c287180e768bc4dd2aec257225d9be4f27b865d
이창근 <dlckdrms0517@gmail.com>
2025-07-28 14:37:00 +0900

    주변 코스 조회 응답 수정 (#124)
    * refactor: 코스 응답을 협의된 형식으로 수정
    
    # Conflicts:
    #	backend/src/main/java/coursepick/coursepick/presentation/dto/GeoJson.java
    
    * docs: 응답에 API 문서 예시 추가
    
    * package: v2 API들을 패키지 분리
    
    * rename: v2 API 들을 명시적으로 표시
    
    * feat: 버저닝 추가
    
    * docs: API 문서를 버전별로 그루핑
    
    * Revert "docs: API 문서를 버전별로 그루핑"
    
    This reverts commit 3c022ea893f52870fd21b5fc9c259a9035296cb0.
    
    * Revert "feat: 버저닝 추가"
    
    This reverts commit a45d3f15fce4be8de5dbd8da8a7511f1803fa6f0.
    
    * Revert "rename: v2 API 들을 명시적으로 표시"
    
    This reverts commit 66b27f26b0cdf5b1e85965d09722fa6db097d7b6.
    
    * Revert "package: v2 API들을 패키지 분리"
    
    This reverts commit 2e22072223a7a5b97eacdb3168a523008c655d46.
    
    * fix: 새로운 응답을 사용하도록 수정

---
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/CoordinateWebResponse.java
@@ -0,0 +1,23 @@
+package coursepick.coursepick.presentation.dto;
+
+import coursepick.coursepick.domain.Coordinate;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public record CoordinateWebResponse(
+        @Schema(example = "37.514167")
+        double latitude,
+        @Schema(example = "127.103611")
+        double longitude
+) {
+    public static CoordinateWebResponse from(Coordinate coordinate) {
+        return new CoordinateWebResponse(coordinate.latitude(), coordinate.longitude());
+    }
+
+    public static List<CoordinateWebResponse> from(List<Coordinate> coordinates) {
+        return coordinates.stream()
+                .map(CoordinateWebResponse::from)
+                .toList();
+    }
+}
---
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/CourseWebResponse.java
@@ -0,0 +1,36 @@
+package coursepick.coursepick.presentation.dto;
+
+import coursepick.coursepick.application.dto.CourseResponse;
+import coursepick.coursepick.domain.RoadType;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public record CourseWebResponse(
+        @Schema(example = "1")
+        long id,
+        @Schema(example = "석촌호수")
+        String name,
+        @Schema(example = "200.123")
+        double distance,
+        @Schema(example = "2146.123")
+        double length,
+        @Schema(example = "트랙")
+        RoadType roadType,
+        @Schema(example = "1.235")
+        double difficulty,
+        List<CoordinateWebResponse> coordinates
+) {
+    public static List<CourseWebResponse> from(List<CourseResponse> courseResponses) {
+        return courseResponses.stream()
+                .map(courseResponse -> new CourseWebResponse(
+                        courseResponse.id(),
+                        courseResponse.name(),
+                        courseResponse.distance().value(),
+                        courseResponse.length().value(),
+                        courseResponse.roadType(),
+                        courseResponse.difficulty(),
+                        CoordinateWebResponse.from(courseResponse.coordinates())
+                )).toList();
+    }
+}
