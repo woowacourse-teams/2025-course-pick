a7aaea37cdb2f066646b993d3a065dc4e497ca94
doabletuple <161921046+doabletuple@users.noreply.github.com>
2025-07-29 22:40:05 +0900

    현 위치 표시 방식 변경 (#137)
    * refactor: 현 위치를 표시하는 레이블에 ID 추가
    
    * refactor: 레이블을 추가하기 전에 기존 레이블을 제거하도록 변경
    
    * refactor: 현재 위치 이동 애니메이션 추가

--- android/app/src/main/java/io/coursepick/coursepick/view/KakaoMapDrawer.kt
+++ android/app/src/main/java/io/coursepick/coursepick/view/KakaoMapDrawer.kt
@@ -5,6 +5,7 @@ import android.location.Location
 import androidx.annotation.DrawableRes
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.label.Label
 import com.kakao.vectormap.label.LabelLayer
 import com.kakao.vectormap.label.LabelManager
 import com.kakao.vectormap.label.LabelOptions
@@ -80,21 +81,19 @@ class KakaoMapDrawer(
         latitude: Double,
         longitude: Double,
     ) {
-        val labelManager: LabelManager = map.labelManager ?: return
-        val styles: LabelStyles =
-            labelManager.addLabelStyles(LabelStyles.from(LabelStyle.from(iconResourceId)))
-                ?: return
-        val options: LabelOptions =
-            LabelOptions
-                .from(
-                    LatLng.from(
-                        latitude,
-                        longitude,
-                    ),
-                ).setStyles(styles)
-        val layer: LabelLayer = map.labelManager?.layer ?: return
-
-        layer.addLabel(options)
+        val manager: LabelManager = map.labelManager ?: return
+        val layer: LabelLayer = manager.layer ?: return
+        val label: Label? = layer.getLabel(CURRENT_LOCATION_LABEL_ID)
+        if (label == null) {
+            val styles: LabelStyles =
+                manager.addLabelStyles(LabelStyles.from(LabelStyle.from(iconResourceId))) ?: return
+            val options: LabelOptions =
+                LabelOptions.from(LatLng.from(latitude, longitude)).setStyles(styles)
+            options.labelId = CURRENT_LOCATION_LABEL_ID
+            layer.addLabel(options)
+            return
+        }
+        label.moveTo(LatLng.from(latitude, longitude), LABEL_MOVE_ANIMATION_DURATION)
     }
 
     private fun CourseItem.toLatLngs() = coordinates.map { coordinate: Coordinate -> coordinate.toLatLng() }
@@ -103,6 +102,8 @@ class KakaoMapDrawer(
 
     companion object {
         private const val STYLE_ID = "CoursePickRouteLineStyle"
+        private const val CURRENT_LOCATION_LABEL_ID = "CurrentLocationLabel"
         private const val LINE_COLOR = 0xFF0000FF.toInt()
+        private const val LABEL_MOVE_ANIMATION_DURATION = 500
     }
 }
