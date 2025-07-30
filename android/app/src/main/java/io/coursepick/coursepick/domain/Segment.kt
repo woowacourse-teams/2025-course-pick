b643732188cd74ed80ce8eadde2b0426563cbd51
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-07-30 19:05:56 +0900

    refactor: Segment 검증 로직 변경

--- android/app/src/main/java/io/coursepick/coursepick/domain/Segment.kt
+++ android/app/src/main/java/io/coursepick/coursepick/domain/Segment.kt
@@ -5,6 +5,10 @@ data class Segment(
     val coordinates: List<Coordinate>,
 ) {
     init {
-        require(coordinates.isNotEmpty())
+        require(coordinates.size >= MIN_COORDINATES_SIZE)
+    }
+
+    companion object {
+        const val MIN_COORDINATES_SIZE = 2
     }
 }
