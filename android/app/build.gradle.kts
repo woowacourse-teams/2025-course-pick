fde2d6a239ce14bf5743c98a44fa8df26a43c269
박지원 <cheerfulboys12@gmail.com>
2025-07-30 12:40:32 +0900

    LeakCanary 도입 (#160)
    * feat: LeakCanary 의존성 추가
    
    * refactor: 코스 목록 조회 API 변경사항 대응 (#168)
    
    * 현 위치 표시 방식 변경 (#137)
    
    * refactor: 현 위치를 표시하는 레이블에 ID 추가
    
    * refactor: 레이블을 추가하기 전에 기존 레이블을 제거하도록 변경
    
    * refactor: 현재 위치 이동 애니메이션 추가
    
    * Timber 도입 (#165)
    
    * feat: Timber 의존성 추가
    
    * feat: Timber 초기화 코드 추가
    
    * feat: LeakCanary 의존성 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -75,6 +75,7 @@ android {
 }
 
 dependencies {
+    debugImplementation(libs.leakcanary.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
