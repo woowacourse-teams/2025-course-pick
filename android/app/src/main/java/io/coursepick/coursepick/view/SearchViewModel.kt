200fe7d3330c02b89e73cce5e27caefc3fb810f9
지원 <cheerfulboys@naver.com>
2025-08-01 01:37:42 +0900

    feat: 장소 검색 기능 추가

---
+++ android/app/src/main/java/io/coursepick/coursepick/view/SearchViewModel.kt
@@ -0,0 +1,38 @@
+package io.coursepick.coursepick.view
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.coursepick.coursepick.data.DefaultSearchKeywordRepository
+import io.coursepick.coursepick.domain.SearchKeywordRepository
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SearchViewModel(
+    private val searchRepository: SearchKeywordRepository = DefaultSearchKeywordRepository(),
+) : ViewModel() {
+    private var searchJob: Job? = null
+
+    private val _state: MutableLiveData<List<SearchKeywordItem>> =
+        MutableLiveData<List<SearchKeywordItem>>()
+    val state: LiveData<List<SearchKeywordItem>> get() = _state
+
+    fun searchWithDebounce(query: String) {
+        searchJob?.cancel()
+
+        searchJob =
+            viewModelScope.launch {
+                delay(2000)
+
+                if (query.isNotBlank()) {
+                    val results = searchRepository.searchKeywords(query)
+                    val keywordsItem = results.keywords.map { SearchKeywordItem(it.placeName) }
+                    _state.value = keywordsItem
+                } else {
+                    _state.value = emptyList()
+                }
+            }
+    }
+}
