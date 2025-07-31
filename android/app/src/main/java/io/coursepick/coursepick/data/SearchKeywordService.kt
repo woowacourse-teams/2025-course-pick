6d5f5be6500f5af387ca48656a902f9240b0e650
지원 <cheerfulboys@naver.com>
2025-08-01 01:35:59 +0900

    feat: 카카오 로컬 API 요청 및 응답 구현

---
+++ android/app/src/main/java/io/coursepick/coursepick/data/SearchKeywordService.kt
@@ -0,0 +1,14 @@
+package io.coursepick.coursepick.data
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface SearchKeywordService {
+    @GET("/v2/local/search/keyword.json")
+    suspend fun searchKeywords(
+        @Query("query") query: String,
+        @Query("page") page: Int? = null,
+        @Query("size") size: Int? = null,
+        @Query("sort") sort: String? = null,
+    ): SearchKeywordsResponseItem
+}
---
+++ android/app/src/main/java/io/coursepick/coursepick/domain/SearchKeywords.kt
@@ -0,0 +1,5 @@
+package io.coursepick.coursepick.domain
+
+data class SearchKeywords(
+    val keywords: List<SearchKeyword>,
+)
