2920279caa417853e755ecd6b5c0ff78b0d6e8b5
Dompoo <dlckdrms0517@gmail.com>
2025-08-03 02:31:50 +0900

    chore: 사용하지 않는 코드 제거

--- backend/src/main/java/coursepick/coursepick/application/dto/CourseFile.java
+++ backend/src/main/java/coursepick/coursepick/application/dto/CourseFile.java
@@ -1,23 +1,10 @@
 package coursepick.coursepick.application.dto;
 
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.io.InputStream;
-import java.text.Normalizer;
 
 public record CourseFile(
         String name,
         CourseFileExtension extension,
         InputStream inputStream
 ) {
-    public static CourseFile fromMultipartFile(MultipartFile file) throws IOException {
-        String filename = Normalizer.normalize(file.getOriginalFilename(), Normalizer.Form.NFC);
-        String extensionStr = StringUtils.getFilenameExtension(file.getOriginalFilename());
-        CourseFileExtension extension = CourseFileExtension.findByName(extensionStr);
-        return new CourseFile(
-                filename, extension, file.getInputStream()
-        );
-    }
 }
