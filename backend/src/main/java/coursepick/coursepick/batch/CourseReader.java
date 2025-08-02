a496d87154b1481d53c14184f395274b7d4cb123
Dompoo <dlckdrms0517@gmail.com>
2025-08-02 23:41:21 +0900

    refactor: CourseReader의 구조를 읽기 좋게 개선

--- backend/src/main/java/coursepick/coursepick/batch/CourseReader.java
+++ backend/src/main/java/coursepick/coursepick/batch/CourseReader.java
@@ -4,6 +4,7 @@ import coursepick.coursepick.application.CourseParserService;
 import coursepick.coursepick.application.dto.CourseFile;
 import coursepick.coursepick.domain.Course;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
@@ -11,6 +12,7 @@ import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -25,23 +27,30 @@ public class CourseReader implements ItemReader<Course> {
     private Iterator<CourseFile> fileIterator;
     private Iterator<Course> courseIterator;
 
+    @BeforeStep
+    public void open() {
+        this.fileIterator = courseFileFetcher.fetchAll().iterator();
+        this.courseIterator = Collections.emptyIterator();
+    }
+
     @Override
     public Course read() throws UnexpectedInputException, ParseException, NonTransientResourceException {
-        if (fileIterator == null) {
-            fileIterator = courseFileFetcher.fetchAll().iterator();
+        if (courseIterator.hasNext()) {
+            return courseIterator.next();
         }
 
-        // 현재 파일의 Course 목록을 다 읽었으면 다음 파일 처리
-        if (courseIterator == null || !courseIterator.hasNext()) {
-            if (fileIterator.hasNext()) {
-                CourseFile nextFile = fileIterator.next();
-                List<Course> coursesFromFile = courseParserService.parse(nextFile);
-                this.courseIterator = coursesFromFile.iterator();
-            } else {
-                return null; // 모든 파일 처리 완료
-            }
+        if (!fileIterator.hasNext()) {
+            return null;
         }
 
-        return courseIterator.hasNext() ? courseIterator.next() : read();
+        moveToNextFile();
+
+        return read();
+    }
+
+    private void moveToNextFile() {
+        CourseFile nextFile = fileIterator.next();
+        List<Course> coursesFromFile = courseParserService.parse(nextFile);
+        this.courseIterator = coursesFromFile.iterator();
     }
 }
