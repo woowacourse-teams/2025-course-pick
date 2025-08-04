aae1808a8a9118bfc38ce4353799660876d73697
Dompoo <dlckdrms0517@gmail.com>
2025-08-04 13:41:08 +0900

    chore: 사용하지 않는 기능 제거

--- backend/src/main/java/coursepick/coursepick/application/dto/CourseFileExtension.java
+++ backend/src/main/java/coursepick/coursepick/application/dto/CourseFileExtension.java
@@ -1,22 +1,6 @@
 package coursepick.coursepick.application.dto;
 
-import java.util.Arrays;
-
 public enum CourseFileExtension {
-    GPX("gpx"),
-    KML("kml"),
-    ;
-
-    private final String strName;
-
-    CourseFileExtension(String strName) {
-        this.strName = strName;
-    }
-
-    public static CourseFileExtension findByName(String strName) {
-        return Arrays.stream(CourseFileExtension.values())
-                .filter(extension -> strName.equalsIgnoreCase(extension.strName))
-                .findFirst()
-                .orElseThrow();
-    }
+    GPX,
+    KML,
 }
