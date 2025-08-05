3d014185f6354ee59e83f14b6d12688f147841b0
DoableTuple <doabletuple@gmail.com>
2025-08-05 14:04:49 +0900

    refactor: 레이블 표시 관련 중복 로직 분리

--- android/app/src/main/java/io/coursepick/coursepick/view/KakaoMapDrawer.kt
+++ android/app/src/main/java/io/coursepick/coursepick/view/KakaoMapDrawer.kt
@@ -2,10 +2,11 @@ package io.coursepick.coursepick.view
 
 import android.content.Context
 import android.location.Location
+import androidx.annotation.DrawableRes
 import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.label.Label
 import com.kakao.vectormap.label.LabelLayer
-import com.kakao.vectormap.label.LabelManager
 import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelStyle
 import com.kakao.vectormap.label.LabelStyles
@@ -37,49 +38,59 @@ class KakaoMapDrawer(
         map: KakaoMap,
         location: Location,
     ) {
-        val manager: LabelManager = map.labelManager ?: return
-        val layer: LabelLayer = manager.layer ?: return
         val labelId: Int = R.drawable.image_current_location
-        val label: Label? = layer.getLabel(labelId.toString())
-        if (label != null) {
-            label.moveTo(location.toLatLng(), LABEL_MOVE_ANIMATION_DURATION)
-            return
+        val styles = LabelStyles.from(LabelStyle.from(labelId))
+        val latLng = location.toLatLng()
+        updateLabel(map, latLng, labelId, styles) { _, label: Label ->
+            label.moveTo(latLng, LABEL_MOVE_ANIMATION_DURATION)
         }
-        val styles: LabelStyles =
-            manager.addLabelStyles(LabelStyles.from(LabelStyle.from(labelId))) ?: return
-        val options: LabelOptions = LabelOptions.from(location.toLatLng()).setStyles(styles)
-        options.labelId = labelId.toString()
-        layer.addLabel(options)
     }
 
     fun showSearchPosition(
         map: KakaoMap,
         coordinate: Coordinate,
     ) {
-        val manager: LabelManager = map.labelManager ?: return
-        val layer: LabelLayer = manager.layer ?: return
         val labelId: Int = R.drawable.image_search_location
-        val label: Label? = layer.getLabel(labelId.toString())
-        val styles: LabelStyles =
-            manager.addLabelStyles(
-                LabelStyles.from(
-                    LabelStyle
-                        .from(labelId)
-                        .setAnchorPoint(0.5F, 0.5F)
-                        .setIconTransition(
-                            LabelTransition.from(Transition.None, Transition.None),
-                        ),
-                ),
-            ) ?: return
-        val options: LabelOptions = LabelOptions.from(coordinate.toLatLng()).setStyles(styles)
-        options.labelId = labelId.toString()
-        layer.remove(label)
-        layer.addLabel(options)
+        val styles =
+            LabelStyles.from(
+                LabelStyle
+                    .from(labelId)
+                    .setAnchorPoint(0.5F, 0.5F)
+                    .setIconTransition(
+                        LabelTransition.from(Transition.None, Transition.None),
+                    ),
+            )
+        updateLabel(map, coordinate.toLatLng(), labelId, styles) {
+            layer: LabelLayer,
+            label: Label,
+            ->
+            layer.remove(label)
+        }
     }
 
     fun removeAllLabels(map: KakaoMap) {
         val layer: LabelLayer = map.labelManager?.layer ?: return
         layer.removeAll()
+    }
+
+    private fun updateLabel(
+        map: KakaoMap,
+        position: LatLng,
+        @DrawableRes labelIdRes: Int,
+        styles: LabelStyles,
+        handleOldLabel: (LabelLayer, Label) -> Unit,
+    ) {
+        val manager = map.labelManager ?: return
+        val layer = manager.layer ?: return
+
+        layer.getLabel(labelIdRes.toString())?.let { existingLabel ->
+            handleOldLabel(layer, existingLabel)
+            return
+        }
+
+        val options: LabelOptions = LabelOptions.from(position).setStyles(styles)
+        options.labelId = labelIdRes.toString()
+        layer.addLabel(options)
     }
 
     companion object {
