736c25e8959b62a42b641798787fed9f39c0948b
Giovanni Junseo Kim <giovannijunseokim@gmail.com>
2025-08-06 22:58:37 +0900

    길찾기 지도 연결 다이얼로그 구현 (#218)
    * feat: KAKAO_MAP의 navigationUrl 구현
    
    * feat: NAVER_MAP의 navigationUrl 구현
    
    * feat: 지도 선택 다이얼로그 구현
    
    * refactor: navigate는 앱 내부 이동으로 혼동될 수 있기에 이름 수정
    
    * feat: 코스의 가장 가까운 좌표 API 연결
    
    * test: Fixtures 추가
    
    * test: CourseFixture 정리
    
    * test: FakeRepository에 함수 구현 추가
    
    * refactor: 사용할 때만 MapChoiceDialog 생성
    
    * refactor: ACTION_VIEW를 처리할 수 있는 앱이 없을 때의 예외처리
    
    * refactor: 불필요한 인코딩 제거
    
    * refactor: 상수화
    
    * refactor: URL에서 불필요한 부분 제거
    
    * refactor: 수식 수행 로직 개선
    
    * refactor: 타입에 맞게 변수명 설정
    
    * refactor: 함수명 변경

--- android/app/src/main/java/io/coursepick/coursepick/data/CourseService.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/CourseService.kt
@@ -1,6 +1,7 @@
 package io.coursepick.coursepick.data
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CourseService {
@@ -11,4 +12,11 @@ interface CourseService {
         @Query("userLat") userLatitude: Double?,
         @Query("userLng") userLongitude: Double?,
     ): List<CourseDto>
+
+    @GET("/courses/{id}/closest-coordinate")
+    suspend fun nearestCoordinate(
+        @Path("id") courseId: Long,
+        @Query("lat") currentLatitude: Double,
+        @Query("lng") currentLongitude: Double,
+    ): CoordinateDto
 }
---
+++ android/app/src/test/java/io/coursepick/coursepick/domain/fixture/LongitudeFixture.kt
@@ -0,0 +1,7 @@
+package io.coursepick.coursepick.domain.fixture
+
+import io.coursepick.coursepick.domain.Longitude
+
+val LONGITUDE_FIXTURE = Longitude(DEFAULT_LONGITUDE_VALUE)
+
+private const val DEFAULT_LONGITUDE_VALUE = 127.102987
