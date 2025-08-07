a10e9bf9b7dac6385403956d8c7fb234864fe6ed
Giovanni Junseo Kim <giovannijunseokim@gmail.com>
2025-08-07 15:50:26 +0900

    기본 지도 앱 설정 (#223)
    refactor: 리스너 이름 변경
    
    refactor: binding 객체의 배킹 프로퍼티 ktlint 무시
    
    refactor: Binding 객체 null로 해제 로직 추가
    
    refactor: Fragment의 빈 생성자를 통해 재생성되도록 구현
    
    refactor: IO 작업 코루틴 통해 처리
    
    refactor: 람다 인자 타입 명시
    
    refactor: 타입 명시
    
    refactor: 로그 제거
    
    refactor: 함수화
    
    refactor: 함수명 수정
    
    feat: MapChoiceDialogFragment로 MapChoiceDialog 대체
    
    feat: MapChoiceDialogFragment 구현
    
    refactor: 저장되는 값과 얻는 값 통일
    
    refactor: String 리소스 이름 변경
    
    feat: 설정에 따른 분기처리 구현
    
    feat: 설정 get, set 기능 구현
    
    feat: 설정 화면 구현
    
    feat: androidx.preference-ktx 라이브러리 추가
    
    refactor: URL에서 불필요한 부분 제거
    
    refactor: 상수화
    
    refactor: 불필요한 인코딩 제거
    
    refactor: ACTION_VIEW를 처리할 수 있는 앱이 없을 때의 예외처리
    
    refactor: 사용할 때만 MapChoiceDialog 생성
    
    test: FakeRepository에 함수 구현 추가
    
    test: CourseFixture 정리
    
    test: Fixtures 추가
    
    feat: 코스의 가장 가까운 좌표 API 연결
    
    refactor: navigate는 앱 내부 이동으로 혼동될 수 있기에 이름 수정
    
    feat: 지도 선택 다이얼로그 구현
    
    feat: NAVER_MAP의 navigationUrl 구현
    
    feat: KAKAO_MAP의 navigationUrl 구현

---
+++ android/app/src/main/java/io/coursepick/coursepick/view/CoursePickPreferences.kt
@@ -0,0 +1,47 @@
+package io.coursepick.coursepick.view
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
+import io.coursepick.coursepick.R
+
+object CoursePickPreferences {
+    private lateinit var preferences: SharedPreferences
+    private lateinit var selectedMapApplicationKey: String
+    private lateinit var kakaoMap: String
+    private lateinit var naverMap: String
+    private lateinit var none: String
+
+    fun init(context: Context) {
+        preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        selectedMapApplicationKey = context.getString(R.string.selected_map_application_key)
+        kakaoMap = context.getString(R.string.selected_map_application_value_kakao)
+        naverMap = context.getString(R.string.selected_map_application_value_naver)
+        none = context.getString(R.string.selected_map_application_value_none)
+    }
+
+    var selectedMapApplication: MapApplication?
+        get() =
+            when (preferences.getString(selectedMapApplicationKey, null)) {
+                kakaoMap -> MapApplication.KAKAO_MAP
+                naverMap -> MapApplication.NAVER_MAP
+                else -> null
+            }
+        set(mapApplication) {
+            preferences.edit {
+                putString(
+                    selectedMapApplicationKey,
+                    mapApplication.serialized,
+                )
+            }
+        }
+
+    private val MapApplication?.serialized: String
+        get() =
+            when (this) {
+                MapApplication.KAKAO_MAP -> kakaoMap
+                MapApplication.NAVER_MAP -> naverMap
+                null -> none
+            }
+}
