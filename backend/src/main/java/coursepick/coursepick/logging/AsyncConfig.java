12bf32d8e1907ba512145d49f4469aa432fa13fd
Dompoo <dlckdrms0517@gmail.com>
2025-08-11 22:38:11 +0900

    chore: 비동기 설정 관련 주석 수정

--- backend/src/main/java/coursepick/coursepick/logging/AsyncConfig.java
+++ backend/src/main/java/coursepick/coursepick/logging/AsyncConfig.java
@@ -14,9 +14,9 @@ public class AsyncConfig {
 
     /**
      * MDC를 전파하는 TaskDecorator가 등록된 Executor를 반환합니다.
-     * <p>
+     * <br>
      * MDC를 전파받아야 하는 비동기 작업에서 주입받아 사용하면 됩니다.
-     * <p>
+     * <br>
      * 자세한 Executor 설정은 <a href="https://docs.spring.io/spring-framework/reference/integration/scheduling.html">스프링 공식문서</a>를 참고했습니다.
      */
     @Bean(name = "asyncExecutor")
@@ -30,6 +30,14 @@ public class AsyncConfig {
         return executor;
     }
 
+    /**
+     * 작업 스레드의 MDC를 백업한 후에,
+     * 현재 스레드의 MDC를 작업 스레드에 전파하고,
+     * 작업 스레드의 작업을 실행합니다.
+     * 작업이 끝난 후에는 작업 스레드의 MDC를 기존으로 복구시킵니다.
+     * <br>
+     * 위 로직을 통해서 작업 스레드가 스레드풀에 반환되더라도 문제가 발생하지 않습니다.
+     */
     private static class MdcTaskDecorator implements TaskDecorator {
 
         @Override
