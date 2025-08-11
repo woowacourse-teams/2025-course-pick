a086011a9df37e35c2afc8736ee194d6491d9b79
Dompoo <dlckdrms0517@gmail.com>
2025-08-11 21:35:15 +0900

    feat: MDC를 활용하여 ClientId 로깅 기능 추가

---
+++ backend/src/main/java/coursepick/coursepick/logging/ClientIdLoggingFilter.java
@@ -0,0 +1,38 @@
+package coursepick.coursepick.logging;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
+@Component
+@Order(HIGHEST_PRECEDENCE)
+public class ClientIdLoggingFilter extends OncePerRequestFilter {
+
+    private static final String HEADER_NAME = "X-Client-Id";
+    private static final String MDC_KEY = "client_id";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String clientId = request.getHeader(HEADER_NAME);
+
+        try {
+            if (clientId != null && !clientId.isBlank()) {
+                MDC.put(MDC_KEY, clientId);
+            } else {
+                MDC.put(MDC_KEY, "Unknown");
+            }
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(MDC_KEY);
+        }
+    }
+}
