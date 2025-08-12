593341ecd7180cd4f6244c38d5a9bf54e42027c9
Dompoo <dlckdrms0517@gmail.com>
2025-08-12 09:53:30 +0900

    fix: ClientId 추출 필터 논리 오류 수정

--- backend/src/main/java/coursepick/coursepick/logging/ClientIdLoggingFilter.java
+++ backend/src/main/java/coursepick/coursepick/logging/ClientIdLoggingFilter.java
@@ -23,12 +23,13 @@ public class ClientIdLoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String clientId = request.getHeader(HEADER_NAME);
-        if (clientId != null && !clientId.isBlank()) {
+        if (clientId == null || clientId.isBlank()) {
             clientId = "Unknown";
         }
 
         try {
             MDC.put(MDC_KEY, clientId);
+            filterChain.doFilter(request, response);
         } finally {
             MDC.remove(MDC_KEY);
         }
