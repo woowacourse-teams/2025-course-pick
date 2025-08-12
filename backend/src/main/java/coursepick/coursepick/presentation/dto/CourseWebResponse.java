8ad7d4378bf2cd56f8a4fb227396b4088a00da1c
jhpark1227 <junhyeok0246@naver.com>
2025-08-12 10:47:11 +0900

    feat: 코스 조회 응답에 코스 경사도 요약 추가

--- backend/src/main/java/coursepick/coursepick/presentation/dto/CourseWebResponse.java
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/CourseWebResponse.java
@@ -5,6 +5,7 @@ import coursepick.coursepick.domain.Meter;
 import coursepick.coursepick.domain.RoadType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record CourseWebResponse(
@@ -20,20 +21,30 @@ public record CourseWebResponse(
         RoadType roadType,
         @Schema(example = "쉬움")
         String difficulty,
+        String summary,
         List<SegmentWebResponse> segments
 ) {
     public static List<CourseWebResponse> from(List<CourseResponse> courseResponses) {
-        return courseResponses.stream()
-                .map(courseResponse -> new CourseWebResponse(
-                        courseResponse.id(),
-                        courseResponse.name(),
-                        courseResponse.distance()
-                                .map(Meter::value)
-                                .orElse(null),
-                        courseResponse.length().value(),
-                        courseResponse.roadType(),
-                        courseResponse.difficulty().name(),
-                        SegmentWebResponse.from(courseResponse.segments())
-                )).toList();
+        List<String> courseSummary = List.of(
+                "초반에 오르막이 집중된 코스입니다.",
+                "전반적으로 평탄한 코스입니다."
+        );
+        List<CourseWebResponse> responses = new ArrayList<>();
+        for(int i=0;i<courseResponses.size();i++) {
+            CourseResponse courseResponse = courseResponses.get(i);
+            responses.add(
+                    new CourseWebResponse(
+                            courseResponse.id(),
+                            courseResponse.name(),
+                            courseResponse.distance().map(Meter::value).orElse(null),
+                            courseResponse.length().value(),
+                            courseResponse.roadType(),
+                            courseResponse.difficulty().name(),
+                            courseSummary.get(i % 2),
+                            SegmentWebResponse.from(courseResponse.segments())
+                    )
+            );
+        }
+        return responses;
     }
 }
