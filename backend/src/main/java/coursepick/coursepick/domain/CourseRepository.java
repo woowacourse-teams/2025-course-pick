106faa12e19fc524f13bd5596b08d893e8e8a18c
Dompoo <dlckdrms0517@gmail.com>
2025-08-13 20:33:42 +0900

    refactor: findAll 후에 필터링 하던 로직을 DB 수준에서 필터링

--- backend/src/main/java/coursepick/coursepick/domain/CourseRepository.java
+++ backend/src/main/java/coursepick/coursepick/domain/CourseRepository.java
@@ -1,25 +1,36 @@
 package coursepick.coursepick.domain;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CourseRepository extends Repository<Course, String> {
-
-    void save(Course course);
-
-    void saveAll(Iterable<Course> courses);
-
-    List<Course> findAll();
+public interface CourseRepository extends MongoRepository<Course, String> {
 
     Optional<Course> findById(String id);
 
-    default List<Course> findAllHasDistanceWithin(Coordinate target, Meter meter) {
-        return findAll().stream()
-                .filter(c -> c.distanceFrom(target).isWithin(meter))
-                .toList();
-    }
+    @Query("""
+            db.course.find ({
+                segments: {
+                    $geoWithin: {
+                        $centerSphere: [
+                            [
+                                ?#{#target.longitude()},
+                                ?#{#target.latitude()}
+                            ],
+                            ?#{#meter.value() / 6378100}
+                        ]
+                    }
+                }
+            })
+            """)
+    List<Course> findAllHasDistanceWithin(
+            @Param("target") Coordinate target,
+            @Param("meter") Meter meter
+    );
+
 
     boolean existsByName(CourseName courseName);
 }
