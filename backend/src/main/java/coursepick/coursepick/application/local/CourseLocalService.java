67d2df393a146280f16aac2e6f3b4cd24f8aa0e5
GiSeok Kang <ime.rokz@gmail.com>
2025-08-14 10:57:40 +0900

    fix: 서비스 메서드 호출 오류 수정

--- backend/src/main/java/coursepick/coursepick/application/local/CourseLocalService.java
+++ backend/src/main/java/coursepick/coursepick/application/local/CourseLocalService.java
@@ -1,22 +1,17 @@
 package coursepick.coursepick.application.local;
 
 import coursepick.coursepick.application.CourseParserService;
-
 import coursepick.coursepick.application.dto.CourseFile;
-import coursepick.coursepick.application.dto.CourseFileExtension;
-
 import coursepick.coursepick.domain.Course;
-
 import coursepick.coursepick.domain.CourseRepository;
-
-import java.io.IOException;
-import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 @Profile("local")
@@ -29,7 +24,7 @@ public class CourseLocalService {
     @Transactional
     public void parse(List<MultipartFile> files) throws IOException {
         for (MultipartFile file : files) {
-            List<Course> courses = courseParserService.parse(CourseFile.from(file));
+            List<Course> courses = courseParserService.parseAndCloseInputStream(CourseFile.from(file));
             courseRepository.saveAll(courses);
         }
     }
