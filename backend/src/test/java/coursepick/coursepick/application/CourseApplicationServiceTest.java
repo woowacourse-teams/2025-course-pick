0561864afd74efef14858fce8eb43b64e45b27e0
jhpark1227 <junhyeok0246@naver.com>
2025-08-14 13:35:14 +0900

    fix: 실패하는 테스트 수정

--- backend/src/test/java/coursepick/coursepick/application/CourseApplicationServiceTest.java
+++ backend/src/test/java/coursepick/coursepick/application/CourseApplicationServiceTest.java
@@ -1,9 +1,7 @@
 package coursepick.coursepick.application;
 
 import coursepick.coursepick.application.dto.CourseResponse;
-import coursepick.coursepick.domain.Coordinate;
-import coursepick.coursepick.domain.Course;
-import coursepick.coursepick.domain.RoadType;
+import coursepick.coursepick.domain.*;
 import coursepick.coursepick.test_util.IntegrationTest;
 import jakarta.persistence.EntityNotFoundException;
 import org.assertj.core.api.Assertions;
@@ -23,19 +21,19 @@ class CourseApplicationServiceTest extends IntegrationTest {
 
     @Test
     void 코스는_최소_1KM부터_탐색할_수_있다() {
-        Course course1 = new Course("한강 러닝 코스", RoadType.트랙, List.of(
+        Course course1 = new Course("한강 러닝 코스", RoadType.트랙, InclineSummary.UNKNOWN, List.of(
                 new Coordinate(37.5180, 127.0280),
                 new Coordinate(37.5175, 127.0270),
                 new Coordinate(37.5170, 127.0265),
                 new Coordinate(37.5180, 127.0280)
         ));
-        Course course2 = new Course("양재천 산책길", RoadType.트랙, List.of(
+        Course course2 = new Course("양재천 산책길", RoadType.트랙, InclineSummary.UNKNOWN, List.of(
                 new Coordinate(37.5165, 127.0285),
                 new Coordinate(37.5160, 127.0278),
                 new Coordinate(37.5155, 127.0265),
                 new Coordinate(37.5165, 127.0285)
         ));
-        Course course3 = new Course("북악산 둘레길", RoadType.트레일, List.of(
+        Course course3 = new Course("북악산 둘레길", RoadType.트레일, InclineSummary.UNKNOWN, List.of(
                 new Coordinate(37.602500, 126.967000),
                 new Coordinate(37.603000, 126.968000),
                 new Coordinate(37.603500, 126.969000),
@@ -60,13 +58,13 @@ class CourseApplicationServiceTest extends IntegrationTest {
 
     @Test
     void 코스는_최대_3KM까지_탐색할_수_있다() {
-        Course course1 = new Course("한강 러닝 코스", RoadType.트랙, List.of(
+        Course course1 = new Course("한강 러닝 코스", RoadType.트랙, InclineSummary.UNKNOWN, List.of(
                 new Coordinate(37.5180, 127.0280),
                 new Coordinate(37.5175, 127.0270),
                 new Coordinate(37.5170, 127.0265),
                 new Coordinate(37.5180, 127.0280)
         ));
-        Course course2 = new Course("북악산 둘레길", RoadType.트레일, List.of(
+        Course course2 = new Course("북악산 둘레길", RoadType.트레일, InclineSummary.UNKNOWN, List.of(
                 new Coordinate(38.602500, 126.967000),
                 new Coordinate(38.603000, 126.968000),
                 new Coordinate(38.603500, 126.969000),
@@ -89,19 +87,19 @@ class CourseApplicationServiceTest extends IntegrationTest {
 
     @Test
     void 가까운_코스들을_조회한다() {
-        Course course1 = new Course("한강 러닝 코스", RoadType.트랙, List.of(
+        Course course1 = new Course("한강 러닝 코스", RoadType.트랙, InclineSummary.UNKNOWN, List.of(
                 new Coordinate(37.5180, 127.0280),
                 new Coordinate(37.5175, 127.0270),
                 new Coordinate(37.5170, 127.0265),
                 new Coordinate(37.5180, 127.0280)
         ));
-        Course course2 = new Course("양재천 산책길", RoadType.트랙, List.of(
+        Course course2 = new Course("양재천 산책길", RoadType.트랙, InclineSummary.UNKNOWN, List.of(
                 new Coordinate(37.5165, 127.0285),
                 new Coordinate(37.5160, 127.0278),
                 new Coordinate(37.5155, 127.0265),
                 new Coordinate(37.5165, 127.0285)
         ));
-        Course course3 = new Course("북악산 둘레길", RoadType.트레일, List.of(
+        Course course3 = new Course("북악산 둘레길", RoadType.트레일, InclineSummary.UNKNOWN, List.of(
                 new Coordinate(37.602500, 126.967000),
                 new Coordinate(37.603000, 126.968000),
                 new Coordinate(37.603500, 126.969000),
