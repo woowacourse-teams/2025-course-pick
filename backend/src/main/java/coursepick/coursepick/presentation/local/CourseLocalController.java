9aeaace30ff20963697b412ab6e551bcc46fbb24
GiSeok Kang <ime.rokz@gmail.com>
2025-08-14 10:53:21 +0900

    refactor: yml 설정 대신 컨트롤러에서 static 페이지 접근하도록 변경

--- backend/src/main/java/coursepick/coursepick/presentation/local/CourseLocalController.java
+++ backend/src/main/java/coursepick/coursepick/presentation/local/CourseLocalController.java
@@ -1,17 +1,19 @@
 package coursepick.coursepick.presentation.local;
 
 import coursepick.coursepick.application.local.CourseLocalService;
-
-import java.io.IOException;
-
-import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @Profile("local")
@@ -24,5 +26,11 @@ public class CourseLocalController {
     public String importGpxFileToCourse(@RequestParam("files") List<MultipartFile> files) throws IOException {
         courseLocalService.parse(files);
         return "done";
+    }
+
+    @GetMapping("/import.html")
+    public ResponseEntity<Resource> importPage() {
+        Resource resource = new ClassPathResource("local/import.html");
+        return ResponseEntity.ok().body(resource);
     }
 }
