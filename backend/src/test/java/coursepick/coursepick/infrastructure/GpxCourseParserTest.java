751fa9281f9d47ec9189be720e25b9ab1fea2ea0
Dompoo <dlckdrms0517@gmail.com>
2025-08-16 13:58:25 +0900

    test: 기본 테스트 제거

--- backend/src/test/java/coursepick/coursepick/infrastructure/GpxCourseParserTest.java
+++ backend/src/test/java/coursepick/coursepick/infrastructure/GpxCourseParserTest.java
@@ -3,19 +3,18 @@ package coursepick.coursepick.infrastructure;
 import coursepick.coursepick.application.dto.CourseFile;
 import coursepick.coursepick.application.dto.CourseFileExtension;
 import coursepick.coursepick.domain.Coordinate;
-import coursepick.coursepick.test_util.GpxTestUtil;
 import org.junit.jupiter.api.Test;
 
+import static coursepick.coursepick.test_util.GpxTestUtil.createGpxInputStreamOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GpxCourseParserTest {
 
     GpxCourseParser sut = new GpxCourseParser();
-    GpxTestUtil gpxUtil = new GpxTestUtil();
 
     @Test
     void GPX_파일을_파싱하여_코스정보를_가져온다() {
-        var inputStream = GpxTestUtil.createGpxInputStreamOf(
+        var inputStream = createGpxInputStreamOf(
                 new Coordinate(37.4869510, 126.9230870, 27.8),
                 new Coordinate(37.4869515, 126.9230875, 27.8),
                 new Coordinate(37.4845100, 126.9255380, 29.2)
