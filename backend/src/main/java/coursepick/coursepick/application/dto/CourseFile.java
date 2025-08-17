393472855bebd87370340dd2022640845e84395b
Dompoo <dlckdrms0517@gmail.com>
2025-08-17 20:36:11 +0900

    refactor: 정규화 로직을 생성자로 이동

--- backend/src/main/java/coursepick/coursepick/application/dto/CourseFile.java
+++ backend/src/main/java/coursepick/coursepick/application/dto/CourseFile.java
@@ -4,8 +4,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.Normalizer;
 
-import static java.text.Normalizer.Form;
 import static java.text.Normalizer.normalize;
 
 public record CourseFile(
@@ -13,18 +13,19 @@ public record CourseFile(
         CourseFileExtension extension,
         InputStream inputStream
 ) {
-    public static CourseFile from(MultipartFile file) throws IOException {
-        String[] fileFullName = file.getOriginalFilename().split("\\.");
-        String fileName = normalize(fileFullName[0], Form.NFC);
-        String extension = fileFullName[1];
-
-        return new CourseFile(fileName, CourseFileExtension.from(extension), file.getInputStream());
-    }
-
     public CourseFile {
         String extensionSuffix = "." + extension.name().toLowerCase();
         if (name.toLowerCase().endsWith(extensionSuffix)) {
             name = name.substring(0, name.length() - extensionSuffix.length());
         }
+        name = normalize(name, Normalizer.Form.NFC);
+    }
+
+    public static CourseFile from(MultipartFile file) throws IOException {
+        String[] fileFullName = file.getOriginalFilename().split("\\.");
+        String fileName = fileFullName[0];
+        String extension = fileFullName[1];
+
+        return new CourseFile(fileName, CourseFileExtension.from(extension), file.getInputStream());
     }
 }
