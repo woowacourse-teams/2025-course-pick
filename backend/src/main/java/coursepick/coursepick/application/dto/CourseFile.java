dfc3e6550ded6cfac266bba8f443638a1ba0c571
Dompoo <dlckdrms0517@gmail.com>
2025-08-17 20:23:04 +0900

    fix: NFC 방식으로 정규화하여 자모 분리 현상을 해결

--- backend/src/main/java/coursepick/coursepick/application/dto/CourseFile.java
+++ backend/src/main/java/coursepick/coursepick/application/dto/CourseFile.java
@@ -1,9 +1,12 @@
 package coursepick.coursepick.application.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.springframework.web.multipart.MultipartFile;
+import static java.text.Normalizer.Form;
+import static java.text.Normalizer.normalize;
 
 public record CourseFile(
         String name,
@@ -12,12 +15,12 @@ public record CourseFile(
 ) {
     public static CourseFile from(MultipartFile file) throws IOException {
         String[] fileFullName = file.getOriginalFilename().split("\\.");
-        String fileName = fileFullName[0];
+        String fileName = normalize(fileFullName[0], Form.NFC);
         String extension = fileFullName[1];
 
         return new CourseFile(fileName, CourseFileExtension.from(extension), file.getInputStream());
     }
-    
+
     public CourseFile {
         String extensionSuffix = "." + extension.name().toLowerCase();
         if (name.toLowerCase().endsWith(extensionSuffix)) {
