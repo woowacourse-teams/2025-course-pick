4ca60a242a79dec134333310b7b340ffbf7525b0
이창근 <dlckdrms0517@gmail.com>
2025-08-19 13:27:20 +0900

    파일명 길이가 잘못 집계되던 문제 해결 (#305)
    * fix: course import 하는 화이트리스트 추가
    
    * chore: 중복된 IOException import 제거
    
    * fix: NFC 방식으로 정규화하여 자모 분리 현상을 해결
    
    * refactor: 정규화 로직을 생성자로 이동
    
    * test: 정규화 로직 단위테스트 추가

--- backend/src/test/java/coursepick/coursepick/application/dto/CourseFileTest.java
+++ backend/src/test/java/coursepick/coursepick/application/dto/CourseFileTest.java
@@ -1,7 +1,10 @@
 package coursepick.coursepick.application.dto;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.text.Normalizer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +16,19 @@ class CourseFileTest {
         var result = new CourseFile(filename, CourseFileExtension.GPX, null);
 
         assertThat(result.name()).isEqualTo("File");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "코스.gpx, 2",
+            "코스이.gpx, 3",
+            "코스이름.gpx, 4"
+    })
+    void 자모가_분리되어_있어도_파일이름_길이가_정상적으로_집계된다(String filename, int expectedLength) {
+        var macFileNameInput = Normalizer.normalize(filename, Normalizer.Form.NFD);
+
+        var result = new CourseFile(macFileNameInput, CourseFileExtension.GPX, null);
+
+        assertThat(result.name().length()).isEqualTo(expectedLength);
     }
 }
