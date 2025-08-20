cd51e32945dce2b9bd1f353191d6ce61d6d58c73
doabletuple <161921046+doabletuple@users.noreply.github.com>
2025-08-20 20:41:29 +0900

    feat: 검색 결과가 없을 시 EmptyView를 표시하는 기능 추가 (#358)

--- android/app/src/main/java/io/coursepick/coursepick/presentation/search/SearchUiState.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/search/SearchUiState.kt
@@ -3,6 +3,9 @@ package io.coursepick.coursepick.presentation.search
 import io.coursepick.coursepick.domain.search.Place
 
 data class SearchUiState(
+    val isQueryBlank: Boolean,
     val places: List<Place>,
     val isLoading: Boolean,
-)
+) {
+    val arePlacesEmpty: Boolean get() = places.isEmpty()
+}
