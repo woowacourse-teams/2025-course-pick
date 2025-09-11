a49af2b6ad31f352790f0d00020d6368efbd45ec
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-11 15:38:03 +0900

    feat: 안드로이드 CD 파일 생성

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,6 +34,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         getByName("debug") {
             buildConfigField("boolean", "DEBUG", "true")
