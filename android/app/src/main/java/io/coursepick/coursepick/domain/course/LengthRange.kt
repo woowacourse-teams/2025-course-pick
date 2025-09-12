9534926930f3295d5c44abbd80506486dba57d54
지원 <cheerfulboys12@gmail.com>
2025-09-12 12:12:43 +0900

    feat: 코스 필터링 조건 모델 추가

---
+++ android/app/src/main/java/io/coursepick/coursepick/domain/course/LengthRange.kt
@@ -0,0 +1,6 @@
+package io.coursepick.coursepick.domain.course
+
+data class LengthRange(
+    val minimum: Int,
+    val maximum: Int,
+)
---
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/course/FilterCondition.kt
@@ -0,0 +1,9 @@
+package io.coursepick.coursepick.presentation.course
+
+import io.coursepick.coursepick.domain.course.Difficulty
+import io.coursepick.coursepick.domain.course.LengthRange
+
+data class FilterCondition(
+    val lengthRange: LengthRange = LengthRange(0, Int.MAX_VALUE),
+    val difficulties: Set<Difficulty> = setOf(),
+)
