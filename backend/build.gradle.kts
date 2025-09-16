cba24a6ac9694f48a3e9588745dc7a487b8a3100
jhpark1227 <junhyeok0246@naver.com>
2025-09-16 14:36:23 +0900

    build: JWT 및 암호화 관련 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -63,6 +63,11 @@ dependencies {
     testImplementation("org.springframework.batch:spring-batch-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    implementation("org.springframework.security:spring-security-crypto")
 }
 
 tasks.withType<Test> {
