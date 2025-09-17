1f5ec1dad45c5823043e1efd69b4ec8dc57fdcdc
jhpark1227 <junhyeok0246@naver.com>
2025-09-18 02:46:36 +0900

    feat: 프로필에 따라 AuthenticationInterceptor를 등록하는 설정 추가

--- backend/src/main/java/coursepick/coursepick/auth/config/WebMvcConfig.java
+++ backend/src/main/java/coursepick/coursepick/auth/config/WebMvcConfig.java
@@ -4,11 +4,13 @@ import coursepick.coursepick.application.JwtProvider;
 import coursepick.coursepick.auth.interceptor.AuthenticationInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
+@Profile({"dev", "prod"})
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final JwtProvider jwtProvider;
