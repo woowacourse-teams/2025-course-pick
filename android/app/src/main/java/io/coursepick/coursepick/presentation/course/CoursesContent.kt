6c1ad8a6bc37c717e1f5a14369384faa9e2cb22f
DoableTuple <doabletuple@gmail.com>
2025-09-18 17:09:50 +0900

    refactor: CoursesContent 분기 처리 로직 개선

--- android/app/src/main/java/io/coursepick/coursepick/presentation/course/CoursesContent.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/course/CoursesContent.kt
@@ -1,6 +1,14 @@
 package io.coursepick.coursepick.presentation.course
 
-enum class CoursesContent {
-    EXPLORE,
-    FAVORITES,
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import io.coursepick.coursepick.R
+import io.coursepick.coursepick.presentation.favorites.FavoriteCoursesFragment
+
+enum class CoursesContent(
+    @StringRes val headerId: Int,
+    val fragmentClass: Class<out Fragment>,
+) {
+    EXPLORE(R.string.main_courses_header, ExploreCoursesFragment::class.java),
+    FAVORITES(R.string.favorites_header, FavoriteCoursesFragment::class.java),
 }
