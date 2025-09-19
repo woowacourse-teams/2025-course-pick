10f4fffacfd9555ce154147befce668dcbc60adb
jhpark1227 <junhyeok0246@naver.com>
2025-09-19 11:20:24 +0900

    test: 테스트 메서드 순서 변경

--- backend/src/test/java/coursepick/coursepick/infrastructure/JJwtProviderTest.java
+++ backend/src/test/java/coursepick/coursepick/infrastructure/JJwtProviderTest.java
@@ -1,14 +1,24 @@
 package coursepick.coursepick.infrastructure;
 
-import coursepick.coursepick.domain.Admin;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import coursepick.coursepick.domain.Admin;
+import org.junit.jupiter.api.Test;
 
 class JJwtProviderTest {
 
     JJwtProvider sut = new JJwtProvider("testSecretKeyTestSecretKeyTestSecretKey", 1800000L);
+
+    @Test
+    void 토큰을_생성한다() {
+        var admin = new Admin("adminId1", "password");
+
+        var token = sut.createToken(admin);
+
+        assertThatCode(() -> sut.validateToken(token))
+                .doesNotThrowAnyException();
+    }
 
     @Test
     void 조작된_토큰이면_예외가_발생한다() {
@@ -29,15 +39,5 @@ class JJwtProviderTest {
         assertThatThrownBy(() -> sut.validateToken(expiredToken))
                 .isInstanceOf(SecurityException.class)
                 .hasMessage("만료된 토큰입니다.");
-    }
-
-    @Test
-    void 토큰을_생성한다() {
-        var admin = new Admin("adminId1", "password");
-
-        var token = sut.createToken(admin);
-
-        assertThatCode(() -> sut.validateToken(token))
-                .doesNotThrowAnyException();
     }
 }
