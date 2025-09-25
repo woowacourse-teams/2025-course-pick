788f0b19ce9d55501433c4f62ff3b0678aed68d8
지원 <cheerfulboys12@gmail.com>
2025-09-25 15:17:41 +0900

    feat: 장소 검색 화면 UI 구현

--- android/app/src/main/java/io/coursepick/coursepick/presentation/search/NewSearchActivity.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/search/NewSearchActivity.kt
@@ -4,19 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.coursepick.coursepick.R
+import io.coursepick.coursepick.domain.course.Coordinate
+import io.coursepick.coursepick.domain.course.Latitude
+import io.coursepick.coursepick.domain.course.Longitude
+import io.coursepick.coursepick.domain.search.Place
 import io.coursepick.coursepick.presentation.search.ui.theme.CoursePickTheme
 
 class NewSearchActivity : ComponentActivity() {
@@ -26,7 +39,6 @@ class NewSearchActivity : ComponentActivity() {
         setContent {
             CoursePickTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding: PaddingValues ->
-                    SearchScreen(modifier = Modifier.padding(innerPadding))
 //                    <androidx.constraintlayout.widget.ConstraintLayout
 //
 //
@@ -46,36 +58,115 @@ class NewSearchActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchScreen(modifier: Modifier = Modifier) {
+fun SearchScreen(
+    query: String,
+    onQueryChange: (query: String) -> Unit,
+    places: List<Place>,
+    modifier: Modifier = Modifier,
+) {
     Column(modifier = modifier) {
-        SearchBar(
-            inputField = {
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                )
+        TextField(
+            value = query,
+            onValueChange = onQueryChange,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(text = stringResource(R.string.search_query_hint))
             },
-            expanded = false,
-            onExpandedChange = { },
-        ) {
-        }
-
-        LazyColumn {
+        )
+        if (places.isEmpty()) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = stringResource(R.string.search_no_result),
+                    fontSize = 20.sp,
+                )
+            }
+        } else {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(places) { place: Place ->
+                    SearchResult(place = place, modifier = modifier.fillMaxWidth())
+                }
+            }
         }
     }
 }
 
 @Composable
-private fun SearchResult(modifier: Modifier = Modifier) {
-    Column {
-        Text("")
+private fun SearchResult(
+    place: Place,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier.padding(horizontal = 20.dp)) {
+        Text(
+            text = place.placeName,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.item_primary),
+            modifier =
+                Modifier
+                    .padding(top = 10.dp)
+                    .fillMaxWidth(),
+        )
 
-        Text("")
+        Text(
+            text = place.addressName,
+            color = colorResource(R.color.item_secondary),
+            modifier =
+                Modifier
+                    .padding(vertical = 10.dp)
+                    .fillMaxWidth(),
+        )
     }
 }
 
 @Preview
 @Composable
+private fun SearchScreenPreview2() {
+    SearchScreen(
+        query = "화성 회의실",
+        onQueryChange = { query: String -> },
+        places = emptyList(),
+        modifier = Modifier.fillMaxSize(),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
 private fun SearchScreenPreview() {
-    SearchScreen()
+    SearchScreen(
+        query = "",
+        onQueryChange = { query: String -> },
+        places =
+            List(10) {
+                Place(
+                    id = 0,
+                    addressName = "서울 송파구 신천동 7-20",
+                    placeName = "검색어",
+                    coordinate =
+                        Coordinate(
+                            latitude = Latitude(0.0),
+                            longitude = Longitude(0.0),
+                        ),
+                )
+            },
+        modifier = Modifier.fillMaxSize(),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchResultPreview() {
+    SearchResult(
+        Place(
+            id = 0,
+            addressName = "서울 송파구 신천동 7-20",
+            placeName = "검색어",
+            coordinate =
+                Coordinate(
+                    latitude = Latitude(0.0),
+                    longitude = Longitude(0.0),
+                ),
+        ),
+    )
 }
