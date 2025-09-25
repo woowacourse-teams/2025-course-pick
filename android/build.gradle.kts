fbccb11d9611db80dbe85c04eabce84aadebcb9d
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-25 14:55:14 +0900

    refactor: 컴포즈 의존성 추가

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
 
 allprojects {
