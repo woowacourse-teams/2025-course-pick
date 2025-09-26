b6c8555e625629960e2eb0535edb43c066bf0cd8
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-26 20:48:37 +0900

    refactor: 반환하는 함수는 반환되는 값을 설명합니다.

--- android/app/src/main/java/io/coursepick/coursepick/domain/search/SearchRepository.kt
+++ android/app/src/main/java/io/coursepick/coursepick/domain/search/SearchRepository.kt
@@ -1,7 +1,7 @@
 package io.coursepick.coursepick.domain.search
 
 interface SearchRepository {
-    suspend fun searchPlaces(
+    suspend fun places(
         query: String,
         page: Int? = null,
         size: Int? = null,
