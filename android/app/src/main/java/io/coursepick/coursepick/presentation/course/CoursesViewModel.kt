ff83929015f39ae9ee8d2097811eb3f3ca158657
지원 <cheerfulboys12@gmail.com>
2025-09-27 17:25:35 +0900

    refactor: `fetchedCourses` -> `originalCourses`로 변수명 변경

--- android/app/src/main/java/io/coursepick/coursepick/presentation/course/CoursesViewModel.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/course/CoursesViewModel.kt
@@ -29,8 +29,8 @@ class CoursesViewModel(
     private val favoritesRepository: FavoritesRepository,
     private val networkMonitor: NetworkMonitor,
 ) : ViewModel() {
-    private var _fetchedCourses: List<CourseItem> = emptyList()
-    val fetchedCourses: List<CourseItem> get() = _fetchedCourses
+    var originalCourses: List<CourseItem> = emptyList()
+        private set
 
     private val _state: MutableLiveData<CoursesUiState> =
         MutableLiveData(
@@ -140,7 +140,7 @@ class CoursesViewModel(
                     }
             }.onSuccess { courses: List<CourseItem> ->
                 Logger.log(Logger.Event.Success("fetch_courses"))
-                _fetchedCourses = courses
+                originalCourses = courses
                 _state
                     .value =
                     state.value?.copy(
@@ -306,7 +306,7 @@ class CoursesViewModel(
 
     fun applyFilter(courseFilter: CourseFilter) {
         val filtered =
-            fetchedCourses
+            originalCourses
                 .filter { courseItem ->
                     (courseFilter.difficulties.isEmpty() || courseItem.difficulty in courseFilter.difficulties) &&
                         (courseItem.length in courseFilter.lengthRange.first..courseFilter.lengthRange.last)
