be2e44556f6b9a71b5461d0ae3e2ab2e5fdfd8bb
Dompoo <dlckdrms0517@gmail.com>
2025-10-01 10:54:31 +0900

    fix: 누락된 의존성 추가

--- backend/src/main/java/coursepick/coursepick/infrastructure/GoogleDriveCourseFileFetcher.java
+++ backend/src/main/java/coursepick/coursepick/infrastructure/GoogleDriveCourseFileFetcher.java
@@ -1,6 +1,7 @@
 package coursepick.coursepick.infrastructure;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
