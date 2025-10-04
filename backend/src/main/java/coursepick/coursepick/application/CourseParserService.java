1e24d9c8b08e7ce215323509ca07ef5e976c146f
슥 <ime.rokz@gmail.com>
2025-10-04 15:51:06 +0900

    기존 예외 발생 시 InputStream 닫지 않는 문제 해결 (#457)
    * refactor: 예외 발생 시 inputStream 닫지 않는 문제 해결
    
    * refactor: CourseFile에서 AutoCloseable 구현하도록 변경
    
    * refactor: CourseFile 사용하는 쪽에서 try-with-resources 사용하도록 변경

--- backend/src/main/java/coursepick/coursepick/application/CourseParserService.java
+++ backend/src/main/java/coursepick/coursepick/application/CourseParserService.java
@@ -19,12 +19,11 @@ public class CourseParserService {
 
     private final List<CourseParser> parsers;
 
-    public List<Course> parseAndCloseInputStream(CourseFile file) throws IOException {
+    public List<Course> parse(CourseFile file) {
         CourseParser parser = findParser(file);
         log.debug("코스 파싱을 시작합니다. 선택된 구현체={}", parser.getClass().getSimpleName());
         List<Course> result = parser.parse(file);
         log.debug("{}개의 코스를 파싱했습니다.", result.size());
-        file.inputStream().close();
         return result;
     }
 
