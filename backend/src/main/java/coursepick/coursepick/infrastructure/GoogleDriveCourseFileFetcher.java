21e2fb1fecbd7bb7327d6dbbde186e71eaed3eb1
GiSeok Kang <ime.rokz@gmail.com>
2025-10-19 19:14:34 +0900

    feat: 파일 리스트 가져올 때 10개 파일씩 가져오도록 변경

--- backend/src/main/java/coursepick/coursepick/infrastructure/GoogleDriveCourseFileFetcher.java
+++ backend/src/main/java/coursepick/coursepick/infrastructure/GoogleDriveCourseFileFetcher.java
@@ -89,6 +89,7 @@ public class GoogleDriveCourseFileFetcher implements CourseFileFetcher {
                 .setSpaces("drive")
                 .setFields("nextPageToken, files(id, name)")
                 .setPageToken(pageToken)
+                .setPageSize(10)
                 .execute();
     }
 
