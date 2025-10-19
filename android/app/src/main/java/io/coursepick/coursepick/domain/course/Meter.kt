d4f7b4f710281d9328e7a65c478491bde27db8ac
지원 <cheerfulboys12@gmail.com>
2025-10-19 23:08:59 +0900

    refactor: Meter 활용 및 리팩토링

--- android/app/src/main/java/io/coursepick/coursepick/domain/course/Meter.kt
+++ android/app/src/main/java/io/coursepick/coursepick/domain/course/Meter.kt
@@ -11,4 +11,12 @@ value class Meter(
     operator fun compareTo(other: Int): Int = this.value.compareTo(other)
 
     operator fun compareTo(other: Double): Int = this.value.compareTo(other)
+
+    companion object {
+        private const val KM_TO_METER = 1000
+
+        fun kmToMeter(km: Float): Meter = Meter((km * KM_TO_METER).toInt())
+
+        val Infinite = Meter(Int.MAX_VALUE)
+    }
 }
--- android/app/src/main/java/io/coursepick/coursepick/presentation/filter/CourseFilter.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/filter/CourseFilter.kt
@@ -1,6 +1,7 @@
 package io.coursepick.coursepick.presentation.filter
 
 import android.os.Parcelable
+import io.coursepick.coursepick.domain.course.Meter
 import io.coursepick.coursepick.presentation.course.CourseItem
 import io.coursepick.coursepick.presentation.model.Difficulty
 import kotlinx.parcelize.Parcelize
@@ -10,18 +11,21 @@ data class CourseFilter(
     val lengthRange: FloatRange = FloatRange(MINIMUM_LENGTH_RANGE, MAXIMUM_LENGTH_RANGE),
     val difficulties: Set<Difficulty> = setOf(Difficulty.EASY, Difficulty.NORMAL, Difficulty.HARD),
 ) : Parcelable {
-    val minimumLengthMeter: Int get() = (lengthRange.first * KM_TO_METER).toInt()
-    val maximumLengthMeter: Int
+    private val minimumLength: Meter get() = Meter.kmToMeter(lengthRange.first)
+    private val maximumLength: Meter
         get() =
-            if (lengthRange.last == MAXIMUM_LENGTH_RANGE) Float.MAX_VALUE.toInt() else (lengthRange.last * KM_TO_METER).toInt()
+            if (lengthRange.last == MAXIMUM_LENGTH_RANGE) {
+                Meter(Int.MAX_VALUE)
+            } else {
+                Meter.kmToMeter(lengthRange.last)
+            }
 
     fun match(courseItem: CourseItem): Boolean =
         (difficulties.isEmpty() || courseItem.difficulty in difficulties) &&
-            (courseItem.length in minimumLengthMeter..maximumLengthMeter)
+            (Meter(courseItem.length) in minimumLength..maximumLength)
 
     companion object {
         const val MINIMUM_LENGTH_RANGE = 0F
         const val MAXIMUM_LENGTH_RANGE = 21F
-        private const val KM_TO_METER = 1000
     }
 }
