8524571ec6a0981115f6439bb5b41b83c6858329
doabletuple <161921046+doabletuple@users.noreply.github.com>
2025-10-21 22:15:05 +0900

    Kilometer 객체 도입 (#509)
    * feat: Kilometer 클래스 구현
    
    * refactor: CourseFilter가 Kilometer를 사용하도록 변경
    
    * refactor: 사용되지 않는 함수 제거
    
    * style: 스타일 가이드 준수
    
    * feat: Meter -> Kilometer 변환 로직 구현
    
    * refactor: valueFrom, valueTo가 Float 값을 받도록 수정

---
+++ android/app/src/main/java/io/coursepick/coursepick/domain/course/Kilometer.kt
@@ -0,0 +1,14 @@
+package io.coursepick.coursepick.domain.course
+
+@JvmInline
+value class Kilometer(
+    val value: Double,
+) : Comparable<Kilometer> {
+    override fun compareTo(other: Kilometer): Int = this.value.compareTo(other.value)
+
+    fun toMeter(): Meter = Meter(value * METRIC_MULTIPLIER / Meter.METRIC_MULTIPLIER)
+
+    companion object {
+        const val METRIC_MULTIPLIER = 1_000
+    }
+}
