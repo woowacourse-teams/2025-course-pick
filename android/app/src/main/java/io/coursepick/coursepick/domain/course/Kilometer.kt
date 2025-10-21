6233a3aa4bed50142efcf9f2159c1af1abc4e6bd
DoableTuple <doabletuple@gmail.com>
2025-10-21 14:53:34 +0900

    feat: Kilometer 클래스 구현

---
+++ android/app/src/main/java/io/coursepick/coursepick/domain/course/Kilometer.kt
@@ -0,0 +1,16 @@
+package io.coursepick.coursepick.domain.course
+
+@JvmInline
+value class Kilometer(
+    val value: Double,
+) : Comparable<Kilometer> {
+    override fun compareTo(other: Kilometer): Int = this.value.compareTo(other.value)
+
+    fun toMeter(): Meter = Meter(value * METRIC_MULTIPLIER / Meter.METRIC_MULTIPLIER)
+
+    fun toInt(): Int = value.toInt()
+
+    companion object {
+        const val METRIC_MULTIPLIER = 1_000
+    }
+}
