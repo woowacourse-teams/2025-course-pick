6a3db2f9b15a1fb69bea96481bb4b05fd22f1b0d
지원 <cheerfulboys12@gmail.com>
2025-10-23 18:04:05 +0900

    refactor: CoursePickRetrofit, KakaoRetrofit 구현 및 적용

--- android/app/src/main/java/io/coursepick/coursepick/di/NetworkModule.kt
+++ android/app/src/main/java/io/coursepick/coursepick/di/NetworkModule.kt
@@ -11,6 +11,7 @@ import io.coursepick.coursepick.BuildConfig
 import io.coursepick.coursepick.data.DefaultNetworkMonitor
 import io.coursepick.coursepick.data.NetworkMonitor
 import io.coursepick.coursepick.data.interceptor.ClientIdInterceptor
+import io.coursepick.coursepick.data.interceptor.KakaoAuthInterceptor
 import io.coursepick.coursepick.data.interceptor.OffLineInterceptor
 import io.coursepick.coursepick.data.interceptor.PrettyPrintLogger
 import io.coursepick.coursepick.presentation.InstallationId
@@ -19,6 +20,13 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class CoursePickRetrofit
+
+@Qualifier
+annotation class KakaoRetrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -39,8 +47,9 @@ object NetworkModule {
         @ApplicationContext applicationContext: Context,
     ): NetworkMonitor = DefaultNetworkMonitor(applicationContext)
 
+    @CoursePickRetrofit
     @Provides
-    fun provideRetrofit(
+    fun provideCoursePickRetrofit(
         loggingInterceptor: HttpLoggingInterceptor,
         networkMonitor: NetworkMonitor,
         installationId: InstallationId,
@@ -61,6 +70,33 @@ object NetworkModule {
             .Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .client(client)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    @KakaoRetrofit
+    @Provides
+    fun provideKakaoRetrofit(
+        loggingInterceptor: HttpLoggingInterceptor,
+        networkMonitor: NetworkMonitor,
+    ): Retrofit {
+        val kakaoClient: OkHttpClient =
+            OkHttpClient
+                .Builder()
+                .addInterceptor(KakaoAuthInterceptor)
+                .addInterceptor(loggingInterceptor)
+                .addInterceptor(OffLineInterceptor(networkMonitor))
+                .build()
+
+        val json =
+            Json {
+                ignoreUnknownKeys = true
+            }
+
+        return Retrofit
+            .Builder()
+            .baseUrl(BuildConfig.KAKAO_BASE_URL)
+            .client(kakaoClient)
             .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
