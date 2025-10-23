8da5e67286f89717012355756d7c247f7f0db90f
지원 <cheerfulboys12@gmail.com>
2025-10-23 17:02:59 +0900

    refactor: RepositoryModule 구현

--- android/app/src/main/java/io/coursepick/coursepick/data/favorites/DefaultFavoritesRepository.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/favorites/DefaultFavoritesRepository.kt
@@ -2,15 +2,18 @@ package io.coursepick.coursepick.data.favorites
 
 import io.coursepick.coursepick.domain.favorites.FavoritesRepository
 import io.coursepick.coursepick.presentation.preference.CoursePickPreferences
+import javax.inject.Inject
 
-class DefaultFavoritesRepository : FavoritesRepository {
-    override fun favoriteCourseIds(): Set<String> = CoursePickPreferences.favoritedCourseIds()
+class DefaultFavoritesRepository
+    @Inject
+    constructor() : FavoritesRepository {
+        override fun favoriteCourseIds(): Set<String> = CoursePickPreferences.favoritedCourseIds()
 
-    override fun addFavoriteCourse(courseId: String) {
-        CoursePickPreferences.addFavorite(courseId)
+        override fun addFavoriteCourse(courseId: String) {
+            CoursePickPreferences.addFavorite(courseId)
+        }
+
+        override fun removeFavoriteCourse(courseId: String) {
+            CoursePickPreferences.removeFavorite(courseId)
+        }
     }
-
-    override fun removeFavoriteCourse(courseId: String) {
-        CoursePickPreferences.removeFavorite(courseId)
-    }
-}
---
+++ android/app/src/main/java/io/coursepick/coursepick/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package io.coursepick.coursepick.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.coursepick.coursepick.data.course.DefaultCourseRepository
+import io.coursepick.coursepick.data.favorites.DefaultFavoritesRepository
+import io.coursepick.coursepick.domain.course.CourseRepository
+import io.coursepick.coursepick.domain.favorites.FavoritesRepository
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindCourseRepository(implementation: DefaultCourseRepository): CourseRepository
+
+    @Binds
+    abstract fun bindFavoritesRepository(implementation: DefaultFavoritesRepository): FavoritesRepository
+}
