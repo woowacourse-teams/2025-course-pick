3151ab0f62afad5abd81e5da3ec50e1645f8b3b5
GiSeok Kang <ime.rokz@gmail.com>
2025-10-27 15:58:10 +0900

    feat: 배치 작업 시 코스 이름 대신 코스 파일의 id로 체크 후 해시 비교 하도록 변경

--- backend/src/main/java/coursepick/coursepick/batch/CourseProcessor.java
+++ backend/src/main/java/coursepick/coursepick/batch/CourseProcessor.java
@@ -2,6 +2,15 @@ package coursepick.coursepick.batch;
 
 import coursepick.coursepick.domain.Course;
 import coursepick.coursepick.domain.CourseRepository;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
@@ -16,5 +25,48 @@ public class CourseProcessor implements ItemProcessor<Course, Course> {
     public Course process(Course item) {
         boolean exists = courseRepository.existsByName(item.name());
         return exists ? null : item;
+    }
+
+    public Course process2(Course item) {
+        if (item.id() == null) {
+            return item;
+        }
+
+        Optional<Course> findCourse = courseRepository.findById(item.id());
+        if (findCourse.isEmpty()) {
+            return item;
+        }
+        Course course = findCourse.get();
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] courseBytes = getCourseToBytes(course);
+            byte[] itemBytes = getCourseToBytes(item);
+            byte[] courseDigest = md.digest(courseBytes);
+            byte[] itemDigest = md.digest(itemBytes);
+
+            String courseHash = HexFormat.of().formatHex(courseDigest);
+            String itemHash = HexFormat.of().formatHex(itemDigest);
+
+            if (courseHash.equals(itemHash)) {
+                return null;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return item;
+    }
+
+    private byte[] getCourseToBytes(Course course) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            oos.writeObject(course);
+            oos.flush();
+
+            return bos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
