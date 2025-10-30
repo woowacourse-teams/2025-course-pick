5d4ab12a749158a14a86defb9a4634b533a2baf8
GiSeok Kang <ime.rokz@gmail.com>
2025-10-30 15:41:14 +0900

    feat: 검증 지역구를 알리기 위한 Notice 기능 추가

---
+++ backend/src/main/java/coursepick/coursepick/application/Notice.java
@@ -0,0 +1,38 @@
+package coursepick.coursepick.application;
+
+public enum Notice {
+    VERIFIED_LOCATION(
+            "verified_location",
+            "/images/verified_location.png",
+            "강남·송파 코스는 저희가 검증했어요\n 다른 지역은 아직 검증 중이에요 🏃",
+            "* 메뉴 탭에서 다시 확인할 수 있어요."
+    );
+
+    private String id;
+    private String imageUrl;
+    private String title;
+    private String description;
+
+    Notice(String id, String imageUrl, String title, String description) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/NoticeResponse.java
@@ -0,0 +1,14 @@
+package coursepick.coursepick.presentation.dto;
+
+import coursepick.coursepick.application.Notice;
+
+public record NoticeResponse(String id, String imageUrl, String title, String description) {
+    public static NoticeResponse create(Notice notice) {
+        return new NoticeResponse(
+                notice.getId(),
+                notice.getImageUrl(),
+                notice.getTitle(),
+                notice.getDescription()
+        );
+    }
+}
