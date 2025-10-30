6ba40ea00e89bc5497ca3cec78568d23d717a2fa
GiSeok Kang <ime.rokz@gmail.com>
2025-10-30 16:46:31 +0900

    refactor: application -> domain으로 패키지 이동

--- backend/src/main/java/coursepick/coursepick/presentation/dto/NoticeResponse.java
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/NoticeResponse.java
@@ -1,6 +1,6 @@
 package coursepick.coursepick.presentation.dto;
 
-import coursepick.coursepick.application.Notice;
+import coursepick.coursepick.domain.Notice;
 
 public record NoticeResponse(String id, String imageUrl, String title, String description) {
     public static NoticeResponse create(Notice notice) {
