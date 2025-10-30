c8c317ea040985df4cf8033e64f5a224edfe2548
슥 <ime.rokz@gmail.com>
2025-10-30 11:29:55 +0900

    배치 작업 시 코스 변경점 파악을 해시를 통한 비교로 개선 (#523)
    * feat: 배치 작업 시 코스 이름 대신 코스 파일의 id로 체크 후 해시 비교 하도록 변경
    
    * refactor: getCourseToBytes 메서드명 변경
    
    * refactor: 해시 과정 메서드 분리
    
    * refactor: OutputStream 대신 Jackson 라이브러리 활용
    
    * refactor: process 로직 교체
    
    * test: CourseProcessor 테스트 코드 작성
    
    * refactor: gpx의 id 태그를 Gpx 객체에 저장하도록 변경
    
    * test: GpxTestUtil에서 id 태그를 포함한 포맷 생성할 수 있도록 변경
    
    * test: 기존에 존재하던 배치 테스트의 이름 일치 테스트 -> 해시 비교 테스트로 변경
    
    * fix: KmlCourseParser의 id 관련 오류 해결
    
    * fix: Course에서 id 인자를 받는 생성자 추가
    
    * refactor: CourseProcessor 예외 메시지 추가
    
    * refactor: Gpx id 태그가 extensions 내부에 있어야 파싱하도록 변경
    
    * refactor: Course 생성자 중복 로직 제거

--- backend/src/main/java/coursepick/coursepick/batch/CourseProcessor.java
+++ backend/src/main/java/coursepick/coursepick/batch/CourseProcessor.java
@@ -1,20 +1,68 @@
 package coursepick.coursepick.batch;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import coursepick.coursepick.domain.Course;
 import coursepick.coursepick.domain.CourseRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+import java.util.Optional;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CourseProcessor implements ItemProcessor<Course, Course> {
 
+    private final ObjectMapper objectMapper;
     private final CourseRepository courseRepository;
 
     @Override
     public Course process(Course item) {
-        boolean exists = courseRepository.existsByName(item.name());
-        return exists ? null : item;
+        if (item.id() == null) {
+            return item;
+        }
+
+        Optional<Course> findCourse = courseRepository.findById(item.id());
+        if (findCourse.isEmpty()) {
+            return item;
+        }
+        Course course = findCourse.get();
+
+        String courseHash = hash(course);
+        String itemHash = hash(item);
+
+        if (courseHash.equals(itemHash)) {
+            return null;
+        }
+
+        return item;
+    }
+
+    private byte[] serializeCourse(Course course) {
+        try {
+            return objectMapper.writeValueAsString(course).getBytes(StandardCharsets.UTF_8);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("코스 직렬화에 실패했습니다", e);
+        }
+    }
+
+    private String hash(Course course) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] bytes = serializeCourse(course);
+            byte[] digest = md.digest(bytes);
+
+            return HexFormat.of().formatHex(digest);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("해시 계산에 실패했습니다.", e);
+        }
     }
 }
