154a826b189cd3fa24873d6a2f3aab050c62f17b
GiSeok Kang <ime.rokz@gmail.com>
2025-10-30 16:46:59 +0900

    refactor: NoticeController로 이름 변경

--- backend/src/main/java/coursepick/coursepick/presentation/NoticeController.java
+++ backend/src/main/java/coursepick/coursepick/presentation/NoticeController.java
@@ -1,13 +1,13 @@
 package coursepick.coursepick.presentation;
 
-import coursepick.coursepick.application.Notice;
+import coursepick.coursepick.domain.Notice;
 import coursepick.coursepick.presentation.dto.NoticeResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StaticImageController {
+public class NoticeController {
 
     /*
     {
