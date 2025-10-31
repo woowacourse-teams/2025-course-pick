ac325d8661f6c2022bc37cf1616c261deadb4dde
GiSeok Kang <ime.rokz@gmail.com>
2025-10-31 14:25:18 +0900

    refactor: 컨트롤러 추상 계층 통일
    - swagger를 위한 인터페이스 추가

--- backend/src/main/java/coursepick/coursepick/presentation/NoticeWebController.java
+++ backend/src/main/java/coursepick/coursepick/presentation/NoticeWebController.java
@@ -1,22 +1,15 @@
 package coursepick.coursepick.presentation;
 
 import coursepick.coursepick.domain.Notice;
+import coursepick.coursepick.presentation.api.NoticeWebApi;
 import coursepick.coursepick.presentation.dto.NoticeResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class NoticeController {
+public class NoticeWebController implements NoticeWebApi {
 
-    /*
-    {
-        "id": ~
-        "imageUrl": ~
-        "title": ~
-        "description": ~
-    }
-     */
     @GetMapping("/notice/{id}")
     public NoticeResponse getNotice(@PathVariable("id") String id) {
         return NoticeResponse.create(Notice.VERIFIED_LOCATION);
---
+++ backend/src/main/java/coursepick/coursepick/presentation/api/NoticeWebApi.java
@@ -0,0 +1,15 @@
+package coursepick.coursepick.presentation.api;
+
+import coursepick.coursepick.presentation.dto.NoticeResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "다이얼로그 노티")
+public interface NoticeWebApi {
+
+    @Operation(summary = "노티를 위한 조회")
+    @ApiResponse(responseCode = "200")
+    NoticeResponse getNotice(@Parameter(example = "verified_location", required = false) String id);
+}
