9710ccbfb6bfa5da52ab1943990dd144c3a634fd
GiSeok Kang <ime.rokz@gmail.com>
2025-11-06 00:08:09 +0900

    feat: Swagger 문서에서 예외 상황 처리되도록 설정

--- backend/src/main/java/coursepick/coursepick/presentation/api/NoticeWebApi.java
+++ backend/src/main/java/coursepick/coursepick/presentation/api/NoticeWebApi.java
@@ -3,13 +3,24 @@ package coursepick.coursepick.presentation.api;
 import coursepick.coursepick.presentation.dto.NoticeWebResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "다이얼로그 노티")
 public interface NoticeWebApi {
 
-    @Operation(summary = "노티를 위한 조회")
-    @ApiResponse(responseCode = "200")
-    NoticeWebResponse getNotice(@Parameter(example = "verified_location", required = false) String id);
+    @Operation(summary = "공지 사항 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", content = @Content(examples = {
+                    @ExampleObject(
+                            name = "존재하지 않는 공지 사항 조회하는 경우",
+                            ref = "#/components/examples/NOT_FOUND_NOTICE"
+                    )
+            }))
+    })
+    NoticeWebResponse getNotice(@Parameter(example = "verified_location", required = true, description = "공지 사항 ID") String id);
 }
