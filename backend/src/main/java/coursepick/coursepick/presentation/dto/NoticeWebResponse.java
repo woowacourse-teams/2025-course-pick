ee96187314cc8b7a642af964621f0d4c3fffcab2
슥 <ime.rokz@gmail.com>
2025-11-12 21:31:56 +0900

    기존에 고정된 경로를 프로필 별로 구분 (#551)
    * refactor: 기존에 고정된 경로를 프로필 별로 구분
    
    * refactor: 테스트 설정 파일 base_url 추가

--- backend/src/main/java/coursepick/coursepick/presentation/dto/NoticeWebResponse.java
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/NoticeWebResponse.java
@@ -3,10 +3,11 @@ package coursepick.coursepick.presentation.dto;
 import coursepick.coursepick.domain.Notice;
 
 public record NoticeWebResponse(String id, String imageUrl, String title, String description) {
-    public static NoticeWebResponse create(Notice notice) {
+    public static NoticeWebResponse create(Notice notice, String baseUrl) {
+        String fullImageUrl = baseUrl + "/" + notice.getImageFileName();
         return new NoticeWebResponse(
                 notice.getId(),
-                notice.getImageUrl(),
+                fullImageUrl,
                 notice.getTitle(),
                 notice.getDescription()
         );
