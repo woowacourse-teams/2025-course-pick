6e1f5a4ffef2f3dd7b6799a5da1574f78e6f9437
지원 <cheerfulboys12@gmail.com>
2025-11-16 21:25:14 +0900

    refactor: 코스 탐색 화면의 xml에 Composable 함수 사용을 위해 ComposeView 추가

--- android/app/src/main/java/io/coursepick/coursepick/presentation/course/ExploreCoursesFragment.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/course/ExploreCoursesFragment.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSmoothScroller
+import io.coursepick.coursepick.R
 import io.coursepick.coursepick.databinding.FragmentExploreCoursesBinding
+import io.coursepick.coursepick.presentation.filter.CourseFilterBottomSheet
+import io.coursepick.coursepick.presentation.search.ui.theme.CoursePickTheme
 
 class ExploreCoursesFragment(
     listener: CourseItemListener,
@@ -18,6 +26,7 @@ class ExploreCoursesFragment(
     private val binding get() = _binding!!
     private val viewModel: CoursesViewModel by activityViewModels()
     private val courseAdapter by lazy { CourseAdapter(listener) }
+    private var showFilterDialog by mutableStateOf(false)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +42,27 @@ class ExploreCoursesFragment(
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ComposeView>(R.id.composeView)?.apply {
+            setContent {
+                CoursePickTheme {
+                    val uiState by viewModel.state.observeAsState()
+                    if (showFilterDialog && uiState != null) {
+                        CourseFilterBottomSheet(
+                            coursesUiState = uiState!!,
+                            onDismissRequest = {
+                                showFilterDialog = false
+                            },
+                            onRangeSliderValueChange = { range ->
+                                viewModel.updateLengthRange(
+                                    range.start.toDouble(),
+                                    range.endInclusive.toDouble(),
+                                )
+                            },
+                        )
+                    }
+                }
+            }
+        }
         setUpBindingVariables()
         setUpStateObserver()
     }
@@ -46,12 +76,17 @@ class ExploreCoursesFragment(
         binding.lifecycleOwner = viewLifecycleOwner
         binding.adapter = courseAdapter
         binding.viewModel = viewModel
+        binding.showFiltersListener = this
     }
 
     private fun setUpStateObserver() {
         viewModel.state.observe(viewLifecycleOwner) { state: CoursesUiState ->
             courseAdapter.submitList(state.courses)
         }
+    }
+
+    override fun showFilters() {
+        showFilterDialog = true
     }
 
     fun scrollTo(courseItem: CourseItem) {
