c770b22a40b03ab55776022bd0362f085d719af3
지원 <cheerfulboys12@gmail.com>
2025-11-16 20:51:02 +0900

    refactor: 코스 필터 BottomSheet UI Compose 마이그레이션

---
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/filter/RoundedCornerButton.kt
@@ -0,0 +1,60 @@
+package io.coursepick.coursepick.presentation.filter
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.coursepick.coursepick.R
+import io.coursepick.coursepick.presentation.search.ui.theme.CoursePickTheme
+
+@Composable
+fun RoundedCornerButton(
+    label: String,
+    enabled: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier =
+            modifier
+                .clip(RoundedCornerShape(size = 8.dp))
+                .background(colorResource(R.color.point_secondary))
+                .padding(vertical = 20.dp)
+                .clickable(
+                    enabled = enabled,
+                    onClick = onClick,
+                ).padding(horizontal = 4.dp),
+        contentAlignment = Alignment.Center,
+        content = {
+            Text(
+                text = label,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.background_primary),
+            )
+        },
+    )
+}
+
+@PreviewLightDark
+@Composable
+private fun RoundedCornerButton() {
+    CoursePickTheme {
+        RoundedCornerButton(
+            label = "쉬움",
+            enabled = true,
+            onClick = { },
+        )
+    }
+}
