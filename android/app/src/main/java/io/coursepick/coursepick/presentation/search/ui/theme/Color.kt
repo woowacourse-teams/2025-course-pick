8fb54cc259df09ce9a743b1ea10b8df7ce1ab4a5
지원 <cheerfulboys12@gmail.com>
2025-11-17 17:30:15 +0900

    refactor: Coursepick 색상 시스템 추가

--- android/app/src/main/java/io/coursepick/coursepick/presentation/search/ui/theme/Color.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/search/ui/theme/Color.kt
@@ -9,3 +9,31 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF1A1A1A)
+val White = Color(0xFFFAFAF9)
+
+val Gray1 = Color(0xFFF0F0EF)
+val Gray2 = Color(0xFFD9D9D9)
+val Gray3 = Color(0xFFA3A3A3)
+val Gray4 = Color(0xFF4D4D4D)
+
+val ItemPrimary = Color(0xFF1A1B20)
+val ItemSecondary = Color(0xFF44464F)
+val ItemTertiary = Color(0xFF5F5F62)
+
+val BackgroundPrimary = Color(0xFFFFFFFF)
+val BackgroundSecondary = Color(0xFFFAF8FF)
+val BackgroundTertiary = Color(0xFFEEEDF4)
+val BackgroundBorder = Color(0xFFC5C6D0)
+
+val PointPrimary = Color(0xFFFF8314)
+val PointSecondary = Color(0xFFFFA055)
+val PointTertiary = Color(0xFFFFF1E6)
+
+val CourseDownhill = Color(0xFF5B8DEF)
+val CourseFlat = Color(0xFF6BBE92)
+val CourseUphill = Color(0xFFE75B64)
+val CourseUnknown = Color(0xFFB4B4B4)
+val CourseUnselected = Color(0xFFB4B4B4)
+val CourseRoute = Color(0xFFB4B4B4)
