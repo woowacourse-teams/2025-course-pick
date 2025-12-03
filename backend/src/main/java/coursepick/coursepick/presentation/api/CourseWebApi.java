03e475011188327c1206f35a0fe8aa7de0db6671
GiSeok Kang <ime.rokz@gmail.com>
2025-12-03 16:11:32 +0900

    feat: 파라미터 부연 설명 추가

--- backend/src/main/java/coursepick/coursepick/presentation/api/CourseWebApi.java
+++ backend/src/main/java/coursepick/coursepick/presentation/api/CourseWebApi.java
@@ -14,7 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
-@Tag(name = "코스")
+@Tag(name = "러닝 코스 (Course)")
 public interface CourseWebApi {
 
     @Operation(summary = "좌표 근처 1km 내 코스 전체 조회")
@@ -32,12 +32,12 @@ public interface CourseWebApi {
             })),
     })
     CoursesWebResponse findNearbyCourses(
-            @Parameter(example = "37.5165004", required = true) double mapLatitude,
-            @Parameter(example = "127.1040109", required = true) double mapLongitude,
-            @Parameter(example = "38.5165004") Double userLatitude,
-            @Parameter(example = "126.1040109") Double userLongitude,
-            @Parameter(example = "1000", required = true) int scope,
-            @Parameter(example = "1") Integer page
+            @Parameter(description = "지도 중심의 위도(-90 ~ 90)", example = "37.5165004", required = true) double mapLatitude,
+            @Parameter(description = "지도 중심의 경도(-180 ~ 180)",example = "127.1040109", required = true) double mapLongitude,
+            @Parameter(description = "사용자 위치의 위도(-90 ~ 90)", example = "38.5165004") Double userLatitude,
+            @Parameter(description = "사용자 위치의 경도(-180 ~ 180)",example = "126.1040109") Double userLongitude,
+            @Parameter(description = "좌표 중심으로부터 탐색하고자 하는 범위", example = "1000", required = true) int scope,
+            @Parameter(description = "페이지 번호", example = "1") Integer page
     );
 
     @Operation(summary = "좌표에서 가장 가까운 코스 위 좌표 조회")
@@ -61,9 +61,9 @@ public interface CourseWebApi {
             })),
     })
     CoordinateWebResponse findClosestCoordinate(
-            @Parameter(example = "689c3143182cecc6353cca7b", required = true) String id,
-            @Parameter(example = "37.5165004", required = true) double latitude,
-            @Parameter(example = "127.1040109", required = true) double longitude
+            @Parameter(description = "코스 ID", example = "689c3143182cecc6353cca7b", required = true) String id,
+            @Parameter(description = "사용자 위도(-90 ~ 90)", example = "37.5165004", required = true) double latitude,
+            @Parameter(description = "사용자 경도(-180 ~ 180)", example = "127.1040109", required = true) double longitude
     );
 
     @Operation(summary = "특정 코스까지의 길찾기")
@@ -87,16 +87,16 @@ public interface CourseWebApi {
             })),
     })
     List<CoordinateWebResponse> routeToCourse(
-            @Parameter(example = "689c3143182cecc6353cca7b", required = true) String id,
-            @Parameter(example = "37.5165004", required = true) double latitude,
-            @Parameter(example = "127.1040109", required = true) double longitude
+            @Parameter(description = "코스 ID", example = "689c3143182cecc6353cca7b", required = true) String id,
+            @Parameter(description = "사용자 위도(-90 ~ 90)", example = "37.5165004", required = true) double latitude,
+            @Parameter(description = "사용자 경도(-180 ~ 180)", example = "127.1040109", required = true) double longitude
     );
 
     @Operation(summary = "즐겨찾기 코스 조회")
     @ApiResponse(responseCode = "200")
     List<CourseWebResponse> findFavoriteCourses(
             @Parameter(
-                    description = "조회할 코스 ID 목록",
+                    description = "코스 ID 목록",
                     required = true,
                     example = "689c3143182cecc6353cca7b,689c3143182cecc6353cca7c,689c3143182cecc6353cca7d",
                     schema = @Schema(type = "array", implementation = String.class)
