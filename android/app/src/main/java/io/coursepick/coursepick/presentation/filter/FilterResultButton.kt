0890a338b55698b769064a5404708471926159e5
지원 <cheerfulboys12@gmail.com>
2025-12-04 00:53:37 +0900

    refactor: RoundedCornerToggleButton을 FilterResultButton으로 이름 및 toggleable -> clickable 변경

--- android/app/src/main/java/io/coursepick/coursepick/presentation/filter/FilterResultButton.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/filter/FilterResultButton.kt
@@ -1,17 +1,20 @@
 package io.coursepick.coursepick.presentation.filter
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -20,45 +23,48 @@ import io.coursepick.coursepick.R
 import io.coursepick.coursepick.presentation.search.ui.theme.CoursePickTheme
 
 @Composable
-fun RoundedCornerToggleButton(
+fun FilterResultButton(
     label: String,
     isActive: Boolean,
     onActiveChanged: () -> Unit,
     modifier: Modifier = Modifier,
-    enabled: Boolean = true,
 ) {
     Box(
         modifier =
             modifier
-                .toggleable(
-                    value = isActive,
-                    enabled = enabled,
-                    role = Role.Checkbox,
-                    onValueChange = { onActiveChanged() },
+                .clickable(
+                    enabled = isActive,
+                    onClick = onActiveChanged,
                 ).clip(RoundedCornerShape(size = 8.dp))
-                .background(if (isActive) colorResource(R.color.point_secondary) else colorResource(R.color.gray2))
-                .padding(vertical = 20.dp)
-                .padding(horizontal = 4.dp),
+                .background(
+                    if (isActive) {
+                        colorResource(R.color.point_secondary)
+                    } else {
+                        colorResource(
+                            R.color.gray2,
+                        )
+                    },
+                ).padding(vertical = 20.dp, horizontal = 4.dp),
         contentAlignment = Alignment.Center,
-        content = {
-            Text(
-                text = label,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                color = colorResource(R.color.background_primary),
-            )
-        },
-    )
+    ) {
+        Text(
+            text = label,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.background_primary),
+        )
+    }
 }
 
 @PreviewLightDark
 @Composable
-private fun RoundedCornerToggleButton() {
+private fun FilterResultButtonPreview() {
     CoursePickTheme {
-        RoundedCornerToggleButton(
+        var isActive by remember { mutableStateOf(true) }
+        FilterResultButton(
             label = "쉬움",
-            isActive = true,
-            onActiveChanged = { },
+            isActive = isActive,
+            onActiveChanged = { isActive = !isActive },
         )
     }
 }
