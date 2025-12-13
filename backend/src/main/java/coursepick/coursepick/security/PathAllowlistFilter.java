e5009741d0657f403d6ff2084004cedef5e6fae9
GiSeok Kang <ime.rokz@gmail.com>
2025-12-13 13:30:53 +0900

    refactor: PathAllowlistFilter 어드민 기능 호출 통합

--- backend/src/main/java/coursepick/coursepick/security/PathAllowlistFilter.java
+++ backend/src/main/java/coursepick/coursepick/security/PathAllowlistFilter.java
@@ -32,10 +32,7 @@ public class PathAllowlistFilter extends OncePerRequestFilter {
             Pattern.compile("^/admin/import$"),
             Pattern.compile("^/admin/courses$"),
             Pattern.compile("^/admin/courses/edit$"),
-            Pattern.compile("^/admin/api/login$"),
-            Pattern.compile("^/admin/api/import$"),
-            Pattern.compile("^/admin/api/courses$"),
-            Pattern.compile("^/admin/api/courses/[^/]+$"),
+            Pattern.compile("^/admin/api/.*$"),
             Pattern.compile("^/notices/[^/]+$"),
             Pattern.compile("^/images/verified_location.png$")
     );
