24ec6c47b93d12bf16cf5e7811bc05b718b7379d
Dompoo <dlckdrms0517@gmail.com>
2025-12-13 15:50:18 +0900

    feat: NotFoundException 제거, NoSuchElementException으로 변경

--- backend/src/main/java/coursepick/coursepick/application/CourseParserService.java
+++ backend/src/main/java/coursepick/coursepick/application/CourseParserService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.List;
 
 import static coursepick.coursepick.application.exception.ErrorType.INVALID_FILE_EXTENSION;
