10370953ee52da826f8cfac787597ca0a3a59963
Dompoo <dlckdrms0517@gmail.com>
2025-12-17 14:24:36 +0900

    rename: CourseParserService -> CourseServiceFacade 명시적 이름 사용

--- backend/src/main/java/coursepick/coursepick/application/CourseParserFacade.java
+++ backend/src/main/java/coursepick/coursepick/application/CourseParserFacade.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.List;
 
 import static coursepick.coursepick.application.exception.ErrorType.INVALID_FILE_EXTENSION;
@@ -15,7 +14,7 @@ import static coursepick.coursepick.application.exception.ErrorType.INVALID_FILE
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CourseParserService {
+public class CourseParserFacade {
 
     private final List<CourseParser> parsers;
 
