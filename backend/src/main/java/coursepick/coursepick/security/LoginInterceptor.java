6e18a8a7049d6f25459aa4b1a68213fcbc7d8f56
Dompoo <dlckdrms0517@gmail.com>
2025-12-30 16:22:59 +0900

    feat: 로그인 검증 인터셉터 추가

---
+++ backend/src/main/java/coursepick/coursepick/security/LoginInterceptor.java
@@ -0,0 +1,50 @@
+package coursepick.coursepick.security;
+
+import coursepick.coursepick.domain.user.Authentication;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+import static coursepick.coursepick.application.exception.ErrorType.AUTHENTICATION_FAIL;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private final SecretKey secretKey;
+
+    public LoginInterceptor(@Value("${jwt.secret-key}") String secretKeyString) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (!(handler instanceof HandlerMethod handlerMethod)) {
+            return true;
+        }
+
+        if (!handlerMethod.hasMethodAnnotation(Login.class)) {
+            return true;
+        }
+
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            throw AUTHENTICATION_FAIL.create();
+        }
+
+        String token = authorizationHeader.substring("Bearer ".length());
+        
+        Authentication authentication = new Authentication(token);
+        String userId = authentication.validateAndGetUserId(secretKey);
+        request.setAttribute("AUTH_USER_ID", userId);
+        return true;
+    }
+}
