4c1e1f83e6bdd1fb6a5132cca0720f93a2009f88
Dompoo <dlckdrms0517@gmail.com>
2025-12-30 14:44:13 +0900

    feat: 카카오 로그인용 RestClient 빈 등록

--- backend/src/main/java/coursepick/coursepick/infrastructure/RestClientConfig.java
+++ backend/src/main/java/coursepick/coursepick/infrastructure/RestClientConfig.java
@@ -1,7 +1,5 @@
 package coursepick.coursepick.infrastructure;
 
-import java.time.Duration;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +7,29 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
+import java.time.Duration;
+
 @Configuration
 @Profile({"dev", "prod"})
 public class RestClientConfig {
 
+    @Bean(name = "kakaoRestClient")
+    public RestClient kakaoRestClient(
+            @Value("${kakao.connect-timeout:1}") int connectTimeoutSeconds,
+            @Value("${kakao.read-timeout:5}") int readTimeoutSeconds
+    ) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(connectTimeoutSeconds));
+        requestFactory.setReadTimeout(Duration.ofSeconds(readTimeoutSeconds));
+
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .baseUrl("https://kapi.kakao.com")
+                .build();
+    }
+
     @Bean(name = "osrmRestClient")
-    public RestClient restClient(
+    public RestClient osrmRestClient(
             @Value("${osrm.url}") String osrmUrl,
             @Value("${osrm.connect-timeout:1}") int connectTimeoutSeconds,
             @Value("${osrm.read-timeout:5}") int readTimeoutSeconds
