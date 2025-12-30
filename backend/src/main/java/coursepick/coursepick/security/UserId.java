6f3c5e271f8e323627834d03f6ce9c9157f61baa
Dompoo <dlckdrms0517@gmail.com>
2025-12-30 16:36:30 +0900

    docs: UserId 애노테이션에 사용법 주석 추가

--- backend/src/main/java/coursepick/coursepick/security/UserId.java
+++ backend/src/main/java/coursepick/coursepick/security/UserId.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/// 인가 처리된 회원의 ID를 받기 위한 애노테이션입니다.
+/// 인가 처리를 수행하지 않습니다. 따라서 `@Login` 없이 사용하면 예외가 발생합니다.
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UserId {
