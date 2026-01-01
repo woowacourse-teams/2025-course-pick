13c055638b927d50e82abe14ad82b37b189c9901
GiSeok Kang <ime.rokz@gmail.com>
2026-01-01 20:01:05 +0900

    feat: Match API Request 객체 검증 추가

--- backend/src/main/java/coursepick/coursepick/presentation/dto/CoordinatesMatchWebRequest.java
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/CoordinatesMatchWebRequest.java
@@ -2,9 +2,16 @@ package coursepick.coursepick.presentation.dto;
 
 import coursepick.coursepick.domain.Coordinate;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public record CoordinatesMatchWebRequest(
+        @NotNull
+        @Size(min = 2)
         List<Coordinate> coordinates
 ) {
 }
