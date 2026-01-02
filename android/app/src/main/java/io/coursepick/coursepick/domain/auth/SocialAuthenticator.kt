835e1d6122b271bd578e0d067926ffbb42d3112f
Giovanni Junseo Kim <kingjunseo@gmail.com>
2026-01-02 19:03:14 +0900

    refactor: SocialAuthenticator#authenticate 주석 추가

--- android/app/src/main/java/io/coursepick/coursepick/domain/auth/SocialAuthenticator.kt
+++ android/app/src/main/java/io/coursepick/coursepick/domain/auth/SocialAuthenticator.kt
@@ -2,10 +2,15 @@ package io.coursepick.coursepick.domain.auth
 
 interface SocialAuthenticator {
     /**
-     * @return social accessToken을 반환한다.
-     * */
+     * 외부 소셜 서비스(Google, Kakao 등)를 통해 인증을 수행합니다.
+     *
+     * @param onSuccess 인증 성공 시 호출되는 콜백 함수입니다.
+     * 인증 결과로 발급된 소셜 서비스의 액세스 토큰을 인자로 전달합니다.
+     * @param onFailure 인증 실패 시 호출되는 콜백 함수입니다.
+     * 인증 과정에서 발생한 에러를 인자로 전달합니다.
+     */
     fun authenticate(
-        onSuccess: (String) -> Unit,
-        onFailure: (Throwable) -> Unit,
+        onSuccess: (token: String) -> Unit,
+        onFailure: (error: Throwable) -> Unit,
     )
 }
