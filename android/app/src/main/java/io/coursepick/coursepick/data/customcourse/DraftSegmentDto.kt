7927dd339c74f3132cbdaa5c34ba6de3579631a1
DoableTuple <doabletuple@gmail.com>
2026-01-03 15:27:11 +0900

    feat: DraftSegmentDto -> DraftSegment 변환 로직 구현

--- android/app/src/main/java/io/coursepick/coursepick/data/customcourse/DraftSegmentDto.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/customcourse/DraftSegmentDto.kt
@@ -1,8 +1,16 @@
 package io.coursepick.coursepick.data.customcourse
 
 import io.coursepick.coursepick.data.course.CoordinateDto
+import io.coursepick.coursepick.domain.course.Length
+import io.coursepick.coursepick.domain.customcourse.DraftSegment
 
 data class DraftSegmentDto(
     val coordinates: List<CoordinateDto>,
     val length: Double,
-)
+) {
+    fun toDraftSegment(): DraftSegment =
+        DraftSegment(
+            coordinates = coordinates.map(CoordinateDto::toCoordinate),
+            length = Length(length),
+        )
+}
