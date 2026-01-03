3d037da830eb65a89462df31b828870bb8308ec7
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-01-03 13:31:25 +0900

    feat: 의존성 주입

--- android/app/src/main/java/io/coursepick/coursepick/data/auth/DefaultAuthRepository.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/auth/DefaultAuthRepository.kt
@@ -2,15 +2,18 @@ package io.coursepick.coursepick.data.auth
 
 import io.coursepick.coursepick.domain.auth.AuthRepository
 import io.coursepick.coursepick.domain.auth.SocialToken
+import javax.inject.Inject
 
-class DefaultAuthRepository(
-    val service: SignService,
-) : AuthRepository {
-    override suspend fun sign(
-        socialType: String,
-        socialToken: SocialToken,
-    ): String {
-        val token = TokenDto(socialToken.accessToken)
-        return service.sign(socialType, token).accessToken
+class DefaultAuthRepository
+    @Inject
+    constructor(
+        private val service: SignService,
+    ) : AuthRepository {
+        override suspend fun sign(
+            socialType: String,
+            socialToken: SocialToken,
+        ): String {
+            val token = TokenDto(socialToken.accessToken)
+            return service.sign(socialType, token).accessToken
+        }
     }
-}
--- android/app/src/main/java/io/coursepick/coursepick/presentation/auth/AuthViewModel.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/auth/AuthViewModel.kt
@@ -2,39 +2,44 @@ package io.coursepick.coursepick.presentation.auth
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import io.coursepick.coursepick.domain.auth.AuthRepository
 import io.coursepick.coursepick.domain.auth.SocialAuthenticator
 import io.coursepick.coursepick.domain.auth.SocialToken
 import io.coursepick.coursepick.presentation.ui.MutableSingleLiveData
 import io.coursepick.coursepick.presentation.ui.SingleLiveData
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AuthViewModel(
-    private val socialAuthenticator: SocialAuthenticator,
-    private val authRepository: AuthRepository,
-) : ViewModel() {
-    private val _event: MutableSingleLiveData<AuthUiEvent> = MutableSingleLiveData()
-    val event: SingleLiveData<AuthUiEvent> get() = _event
+@HiltViewModel
+class AuthViewModel
+    @Inject
+    constructor(
+        private val socialAuthenticator: SocialAuthenticator,
+        private val authRepository: AuthRepository,
+    ) : ViewModel() {
+        private val _event: MutableSingleLiveData<AuthUiEvent> = MutableSingleLiveData()
+        val event: SingleLiveData<AuthUiEvent> get() = _event
 
-    fun authenticate(socialType: String) {
-        socialAuthenticator.authenticate(
-            onSuccess = { socialAccessToken: String ->
-                viewModelScope.launch {
-                    runCatching {
-                        authRepository.sign(
-                            socialType,
-                            SocialToken(socialAccessToken),
-                        )
-                    }.onSuccess { token: String ->
-                        _event.value = AuthUiEvent.AuthenticateSuccess
-                    }.onFailure {
-                        _event.value = AuthUiEvent.AuthenticateFailure
+        fun authenticate(socialType: String) {
+            socialAuthenticator.authenticate(
+                onSuccess = { socialAccessToken: String ->
+                    viewModelScope.launch {
+                        runCatching {
+                            authRepository.sign(
+                                socialType,
+                                SocialToken(socialAccessToken),
+                            )
+                        }.onSuccess { token: String ->
+                            _event.value = AuthUiEvent.AuthenticateSuccess
+                        }.onFailure {
+                            _event.value = AuthUiEvent.AuthenticateFailure
+                        }
                     }
-                }
-            },
-            onFailure = { error: Throwable ->
-                _event.value = AuthUiEvent.AuthenticateFailure
-            },
-        )
+                },
+                onFailure = { error: Throwable ->
+                    _event.value = AuthUiEvent.AuthenticateFailure
+                },
+            )
+        }
     }
-}
