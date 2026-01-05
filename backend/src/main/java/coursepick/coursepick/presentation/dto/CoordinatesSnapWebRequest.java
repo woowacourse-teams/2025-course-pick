06b784fab3eb4fa4768710654fba08eb9496d54b
GiSeok Kang <ime.rokz@gmail.com>
2026-01-05 14:03:16 +0900

    refactor: 사용하는 import 제거

--- backend/src/main/java/coursepick/coursepick/presentation/dto/CoordinatesSnapWebRequest.java
+++ backend/src/main/java/coursepick/coursepick/presentation/dto/CoordinatesSnapWebRequest.java
@@ -2,7 +2,6 @@ package coursepick.coursepick.presentation.dto;
 
 import coursepick.coursepick.domain.course.Coordinate;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
