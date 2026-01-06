2db0fae4331308839a622e153f83cdff66ecd616
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-01-06 19:38:11 +0900

    refactor: ktlint 수정

--- android/app/src/main/java/io/coursepick/coursepick/di/AuthModule.kt
+++ android/app/src/main/java/io/coursepick/coursepick/di/AuthModule.kt
@@ -4,7 +4,6 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.components.ViewModelComponent
 import io.coursepick.coursepick.domain.auth.SocialAuthenticator
 import io.coursepick.coursepick.presentation.auth.KakaoAuthenticator
 
