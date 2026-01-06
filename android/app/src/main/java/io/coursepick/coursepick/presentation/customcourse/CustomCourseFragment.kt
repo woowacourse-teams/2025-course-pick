9978a23bc86f55aec547bc38de720d889eecb9cd
DoableTuple <doabletuple@gmail.com>
2026-01-06 15:08:04 +0900

    feat: CustomCourseFragment 생성

---
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseFragment.kt
@@ -0,0 +1,32 @@
+package io.coursepick.coursepick.presentation.customcourse
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.fragment.app.Fragment
+import io.coursepick.coursepick.databinding.FragmentCustomCoursesBinding
+import io.coursepick.coursepick.presentation.search.ui.theme.CoursePickTheme
+
+class CustomCourseFragment : Fragment() {
+    @Suppress("ktlint:standard:backing-property-naming")
+    private var _binding: FragmentCustomCoursesBinding? = null
+    private val binding: FragmentCustomCoursesBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentCustomCoursesBinding.inflate(inflater, container, false)
+        binding.customCourse.setContent {
+            CoursePickTheme {
+                LazyColumn {
+
+                }
+            }
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
