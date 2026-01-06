b86a5aeee35581ce4186fac93a63779108cb5a7b
DoableTuple <doabletuple@gmail.com>
2026-01-06 18:03:19 +0900

    feat: CustomCourseUiModel 데이텉 포매팅 기능 구현
    (cherry picked from commit ad74489efd6302af28eda722097fc745d447b842)

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseUiModel.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseUiModel.kt
@@ -1,8 +1,40 @@
 package io.coursepick.coursepick.presentation.customcourse
 
+import androidx.annotation.StringRes
+import io.coursepick.coursepick.R
 import io.coursepick.coursepick.domain.course.Course
+import io.coursepick.coursepick.domain.course.Distance
+import io.coursepick.coursepick.domain.course.InclineSummary
 
 data class CustomCourseUiModel(
     val course: Course,
     val selected: Boolean,
-)
+) {
+    val distance: String? =
+        course.distance?.let { distance: Distance ->
+            if (distance.meter < 1000) {
+                "${distance.meter.value.toInt()}m"
+            } else {
+                String.format(
+                    locale = null,
+                    format = "%.2f",
+                    distance.meter.toKilometer().value,
+                ) + "km"
+            }
+        }
+
+    @StringRes
+    val inclineSummaryStringResourceId: Int = course.inclineSummary.stringResourceId
+
+    private val InclineSummary.stringResourceId: Int
+        get() =
+            when (this) {
+                InclineSummary.MOSTLY_FLAT -> R.string.incline_summary_flat
+                InclineSummary.REPEATING_HILLS -> R.string.incline_summary_repeating_hills
+                InclineSummary.SOMETIMES_UPHILL -> R.string.incline_summary_sometimes_uphill
+                InclineSummary.SOMETIMES_DOWNHILL -> R.string.incline_summary_sometimes_downhill
+                InclineSummary.CONTINUOUS_UPHILL -> R.string.incline_summary_continuous_uphill
+                InclineSummary.CONTINUOUS_DOWNHILL -> R.string.incline_summary_continuous_downhill
+                InclineSummary.UNKNOWN -> R.string.incline_summary_continuous_unknown
+            }
+}
