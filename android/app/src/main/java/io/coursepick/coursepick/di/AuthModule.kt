e028ef7d2a32a4faa8d27b8dcaae334b7e5033d4
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-01-06 19:34:01 +0900

    refactor: viewModelComponent -> ActivityComponent 로 생명주기 변경

--- android/app/src/main/java/io/coursepick/coursepick/di/AuthModule.kt
+++ android/app/src/main/java/io/coursepick/coursepick/di/AuthModule.kt
@@ -3,12 +3,13 @@ package io.coursepick.coursepick.di
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ViewModelComponent
 import io.coursepick.coursepick.domain.auth.SocialAuthenticator
 import io.coursepick.coursepick.presentation.auth.KakaoAuthenticator
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(ActivityComponent::class)
 abstract class AuthModule {
     @Binds
     abstract fun bindSocialAuthenticator(implementation: KakaoAuthenticator): SocialAuthenticator
