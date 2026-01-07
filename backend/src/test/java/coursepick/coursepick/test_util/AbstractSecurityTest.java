30a54035d79d9bbd4ac79c9186a30d2d91274467
GiSeok Kang <ime.rokz@gmail.com>
2026-01-07 17:31:00 +0900

    refactor: AbstractSecurityTest 추상 클래스로 변경

--- backend/src/test/java/coursepick/coursepick/test_util/AbstractSecurityTest.java
+++ backend/src/test/java/coursepick/coursepick/test_util/AbstractSecurityTest.java
@@ -29,7 +29,7 @@ import java.util.UUID;
 })
 @EnableWebMvc
 @AutoConfigureMockMvc
-public class AbstractSecurityTest {
+public abstract class AbstractSecurityTest {
 
     @Autowired
     protected MockMvc mockMvc;
