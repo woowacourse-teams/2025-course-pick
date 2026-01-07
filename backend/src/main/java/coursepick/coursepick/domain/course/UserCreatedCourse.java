9a41d6ba690aca7e140744738055e1d6af916602
GiSeok Kang <ime.rokz@gmail.com>
2026-01-07 15:00:20 +0900

    refactor: 모든 파라미터 생성자를 Persistence에서 활용하도록 수정

--- backend/src/main/java/coursepick/coursepick/domain/course/UserCreatedCourse.java
+++ backend/src/main/java/coursepick/coursepick/domain/course/UserCreatedCourse.java
@@ -3,10 +3,11 @@ package coursepick.coursepick.domain.course;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED, onConstructor_ = @PersistenceCreator)
 public class UserCreatedCourse {
 
     @Id
