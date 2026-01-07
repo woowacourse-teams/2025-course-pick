505fae78c421293b67643f6a9634adf25ca1bb16
지원 <cheerfulboys12@gmail.com>
2026-01-07 14:54:35 +0900

    feat: `CustomCourseScreen`에 UI 구현

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseScreen.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseScreen.kt
@@ -2,29 +2,83 @@ package io.coursepick.coursepick.presentation.customcourse
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.coursepick.coursepick.R
+import io.coursepick.coursepick.domain.course.Coordinate
+import io.coursepick.coursepick.domain.course.Course
+import io.coursepick.coursepick.domain.course.CourseName
+import io.coursepick.coursepick.domain.course.Distance
+import io.coursepick.coursepick.domain.course.InclineSummary
+import io.coursepick.coursepick.domain.course.InclineType
+import io.coursepick.coursepick.domain.course.Latitude
+import io.coursepick.coursepick.domain.course.Length
+import io.coursepick.coursepick.domain.course.Longitude
+import io.coursepick.coursepick.domain.course.Segment
 
 @Composable
-fun CustomCourseScreen() {
+fun CustomCourseScreen(uiState: CustomCourseUiState) {
     Column(
         modifier = Modifier.fillMaxSize(),
     ) {
         Text(
             text = "나의 코스",
             fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
             color = colorResource(R.color.item_primary),
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
         )
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(items = uiState.courses) { course: CustomCourseUiModel ->
+                CustomCourseItem(course)
+            }
+        }
     }
 }
 
 @PreviewLightDark
 @Composable
 fun CustomCourseScreenPreview() {
-    CustomCourseScreen()
+    CustomCourseScreen(
+        uiState =
+            CustomCourseUiState(
+                courses =
+                    List(10) {
+                        CustomCourseUiModel(
+                            course =
+                                Course(
+                                    id = "0",
+                                    name = CourseName("Preview Course"),
+                                    distance = Distance(123),
+                                    length = Length(456),
+                                    roadType = "보도",
+                                    difficulty = "쉬움",
+                                    inclineSummary = InclineSummary.MOSTLY_FLAT,
+                                    segments =
+                                        listOf(
+                                            Segment(
+                                                inclineType = InclineType.UNKNOWN,
+                                                coordinates =
+                                                    listOf(
+                                                        Coordinate(Latitude(0.0), Longitude(0.0)),
+                                                        Coordinate(Latitude(0.0), Longitude(0.0)),
+                                                    ),
+                                            ),
+                                        ),
+                                ),
+                            selected = false,
+                        )
+                    },
+            ),
+    )
 }
