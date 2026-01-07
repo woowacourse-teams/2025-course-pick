d587f1d6a06979b8ac366dac0b59b3c014dc65dc
지원 <cheerfulboys12@gmail.com>
2026-01-07 16:11:12 +0900

    feat: `CustomCourseScreen` 배경색 설정 및 FAB 추가

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseFragment.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseFragment.kt
@@ -1,9 +1,16 @@
 package io.coursepick.coursepick.presentation.customcourse
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.fragment.app.Fragment
 import io.coursepick.coursepick.databinding.FragmentCustomCoursesBinding
 import io.coursepick.coursepick.presentation.search.ui.theme.CoursePickTheme
@@ -21,7 +28,18 @@ class CustomCourseFragment : Fragment() {
         _binding = FragmentCustomCoursesBinding.inflate(inflater, container, false)
         binding.customCourse.setContent {
             CoursePickTheme {
-                CustomCourseScreen(CustomCourseUiState(listOf()))
+                Scaffold(
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = { Log.d("TAG", "onCreateView: click - FAB") },
+                            shape = CircleShape,
+                        ) {
+                            Icon(imageVector = Icons.Default.Add, contentDescription = "추가")
+                        }
+                    },
+                ) {
+                    CustomCourseScreen(CustomCourseUiState(listOf()))
+                }
             }
         }
         return binding.root
