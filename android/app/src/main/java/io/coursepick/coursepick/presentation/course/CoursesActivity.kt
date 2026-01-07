144e189f1f75afb7130ad1775c5d7d96abd71a21
지원 <cheerfulboys12@gmail.com>
2026-01-07 15:56:51 +0900

    feat: `CustomCourseFragment` <-> 바텀 네비게이션 연결

--- android/app/src/main/java/io/coursepick/coursepick/presentation/course/CoursesActivity.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/course/CoursesActivity.kt
@@ -320,9 +320,7 @@ class CoursesActivity :
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             supportFragmentManager.fragments.forEach { fragment: Fragment ->
-                if (fragment is ExploreCoursesFragment || fragment is FavoriteCoursesFragment) {
-                    hide(fragment)
-                }
+                hide(fragment)
             }
             supportFragmentManager
                 .findFragmentByTag(content.fragmentClass.name)
@@ -353,6 +351,11 @@ class CoursesActivity :
                     viewModel.showCourses()
                     viewModel.switchContent(CoursesContent.FAVORITES)
                     viewModel.fetchFavorites()
+                    true
+                }
+
+                R.id.customCourseMenu -> {
+                    switchContent(CoursesContent2.FragmentWithoutTitle.CustomCourse)
                     true
                 }
 
