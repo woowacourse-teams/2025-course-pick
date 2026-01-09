38421674ab2f89e6b2b2d7f524a08db795c5a4bb
DoableTuple <doabletuple@gmail.com>
2026-01-09 14:57:54 +0900

    refactor: CustomCourseScreen 구조 변경 및 FAB 추가

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseScreen.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseScreen.kt
@@ -1,14 +1,22 @@
 package io.coursepick.coursepick.presentation.customcourse
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -16,36 +24,50 @@ import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.coursepick.coursepick.R
-import io.coursepick.coursepick.domain.course.Coordinate
-import io.coursepick.coursepick.domain.course.Course
-import io.coursepick.coursepick.domain.course.CourseName
-import io.coursepick.coursepick.domain.course.Distance
-import io.coursepick.coursepick.domain.course.InclineSummary
-import io.coursepick.coursepick.domain.course.InclineType
-import io.coursepick.coursepick.domain.course.Latitude
-import io.coursepick.coursepick.domain.course.Length
-import io.coursepick.coursepick.domain.course.Longitude
-import io.coursepick.coursepick.domain.course.Segment
 
 @Composable
-fun CustomCourseScreen(uiState: CustomCourseUiState) {
-    Column(
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .background(color = colorResource(R.color.background_primary)),
+fun CustomCourseScreen(
+    uiState: CustomCourseUiState,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
     ) {
-        Text(
-            text = "나의 코스",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            color = colorResource(R.color.item_primary),
-            modifier = Modifier.padding(horizontal = 20.dp),
-        )
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(items = uiState.courses) { course: CustomCourseUiModel ->
-                CustomCourseItem(course)
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .background(color = colorResource(R.color.background_primary)),
+        ) {
+            Text(
+                text = "나의 코스",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.item_primary),
+                modifier = Modifier.padding(horizontal = 20.dp),
+            )
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(items = uiState.courses) { course: CustomCourseUiModel ->
+                    CustomCourseItem(course)
+                }
             }
+        }
+
+        FloatingActionButton(
+            onClick = {},
+            shape = CircleShape,
+            containerColor = colorResource(R.color.point_primary),
+            modifier =
+                Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(20.dp)
+                    .size(50.dp),
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null,
+                modifier = Modifier.size(40.dp),
+            )
         }
     }
 }
