3dc63fc7662748dbaea6cad589a4496a9a124406
DoableTuple <doabletuple@gmail.com>
2026-01-09 14:57:03 +0900

    refactor: CustomCourseFragment Scaffold 제거

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseFragment.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseFragment.kt
@@ -1,19 +1,11 @@
 package io.coursepick.coursepick.presentation.customcourse
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.fragment.app.Fragment
 import io.coursepick.coursepick.databinding.FragmentCustomCoursesBinding
-import io.coursepick.coursepick.presentation.search.ui.theme.CoursePickTheme
 
 class CustomCourseFragment : Fragment() {
     @Suppress("ktlint:standard:backing-property-naming")
@@ -27,20 +19,11 @@ class CustomCourseFragment : Fragment() {
     ): View {
         _binding = FragmentCustomCoursesBinding.inflate(inflater, container, false)
         binding.customCourse.setContent {
-            CoursePickTheme {
-                Scaffold(
-                    floatingActionButton = {
-                        FloatingActionButton(
-                            onClick = { Log.d("TAG", "onCreateView: click - FAB") },
-                            shape = CircleShape,
-                        ) {
-                            Icon(imageVector = Icons.Default.Add, contentDescription = "추가")
-                        }
-                    },
-                ) {
-                    CustomCourseScreen(CustomCourseUiState(listOf()))
-                }
-            }
+            CustomCourseScreen(
+                CustomCourseUiState(
+                    courses = List(10) { CustomCourseUiModel.CUSTOM_COURSE_FIXTURE },
+                ),
+            )
         }
         return binding.root
     }
