a27ac14c07a83cb98a5ff9ced4d4eb921a7cda9a
DoableTuple <doabletuple@gmail.com>
2026-01-09 15:43:39 +0900

    refactor: CoursesContent.CUSTOM_COURSE 추가

--- android/app/src/main/java/io/coursepick/coursepick/presentation/course/CoursesActivity.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/course/CoursesActivity.kt
@@ -355,7 +355,8 @@ class CoursesActivity :
                 }
 
                 R.id.customCourseMenu -> {
-                    switchContent(CoursesContent2.FragmentWithoutTitle.CustomCourse)
+                    viewModel.showCourses()
+                    viewModel.switchContent(CoursesContent.CUSTOM_COURSE)
                     true
                 }
 
