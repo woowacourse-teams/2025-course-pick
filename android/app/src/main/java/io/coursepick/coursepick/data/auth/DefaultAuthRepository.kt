98055686766fa772eaba37963ace7e7fb61c7402
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-01-10 14:38:15 +0900

    feat: AuthRepository에 액세스 토큰 관리 메서드 추가 및 구현

--- android/app/src/main/java/io/coursepick/coursepick/data/auth/DefaultAuthRepository.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/auth/DefaultAuthRepository.kt
@@ -7,6 +7,7 @@ import javax.inject.Inject
 class DefaultAuthRepository
     @Inject
     constructor(
+        private val tokenLocalDataSource: TokenLocalDataSource,
         private val service: SignService,
     ) : AuthRepository {
         override suspend fun sign(
@@ -16,4 +17,12 @@ class DefaultAuthRepository
             val token = TokenDto(socialToken.accessToken)
             return service.sign(socialType, token).accessToken
         }
+
+        override suspend fun saveAccessToken(token: String) {
+            tokenLocalDataSource.saveAccessToken(token)
+        }
+
+        override suspend fun accessToken(): String? = tokenLocalDataSource.accessToken()
+
+        override suspend fun clearAccessToken() = tokenLocalDataSource.clearAccessToken()
     }
