d4e5c525d830a154fdb461fd93a26623ce2c4b74
DoableTuple <doabletuple@gmail.com>
2026-01-11 15:32:24 +0900

    refactor: CreateCustomCourseScreen에 코스 길이 텍스트 추가 및 FAB 크기 조절

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CreateCustomCourseScreen.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CreateCustomCourseScreen.kt
@@ -1,14 +1,19 @@
 package io.coursepick.coursepick.presentation.customcourse
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -24,17 +29,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.coursepick.coursepick.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateCustomCourseScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .navigationBarsPadding()
+    ) {
         CenterAlignedTopAppBar(
             title = { Text(text = "코스 추가") },
             navigationIcon = {
@@ -60,10 +68,27 @@ fun CreateCustomCourseScreen(modifier: Modifier = Modifier) {
                     actionIconContentColor = colorResource(R.color.item_primary),
                 ),
         )
+
         Row(
-            modifier = Modifier.align(alignment = Alignment.BottomCenter).fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier =
+                Modifier
+                    .align(alignment = Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
         ) {
-            Column{
+            Text(
+                text = "123km",
+                fontSize = 24.sp,
+                color = colorResource(R.color.item_primary),
+                modifier =
+                    Modifier
+                        .clip(shape = CircleShape)
+                        .background(color = colorResource(R.color.background_primary))
+                        .padding(20.dp)
+                        .align(Alignment.Bottom),
+            )
+
             Column {
                 IconButton(
                     onClick = {},
@@ -71,13 +96,13 @@ fun CreateCustomCourseScreen(modifier: Modifier = Modifier) {
                         Modifier
                             .clip(shape = CircleShape)
                             .background(colorResource(R.color.background_primary))
-                            .size(50.dp),
+                            .size(70.dp),
                 ) {
                     Icon(
                         painter = painterResource(R.drawable.icon_undo),
                         contentDescription = null,
                         tint = colorResource(R.color.item_primary),
-                        modifier = Modifier.size(30.dp),
+                        modifier = Modifier.size(40.dp),
                     )
                 }
 
@@ -89,13 +114,13 @@ fun CreateCustomCourseScreen(modifier: Modifier = Modifier) {
                         Modifier
                             .clip(shape = CircleShape)
                             .background(colorResource(R.color.background_primary))
-                            .size(50.dp),
+                            .size(70.dp),
                 ) {
                     Icon(
                         imageVector = Icons.Default.Add,
                         contentDescription = null,
                         tint = colorResource(R.color.item_primary),
-                        modifier = Modifier.size(30.dp),
+                        modifier = Modifier.size(40.dp),
                     )
                 }
             }
