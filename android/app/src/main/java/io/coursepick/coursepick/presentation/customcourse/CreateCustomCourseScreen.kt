eeec7f6585e80d9844570397550ca8fc8391997c
지원 <cheerfulboys12@gmail.com>
2026-01-11 15:01:35 +0900

    feat: `CreateCustomCourseScreen`에 실행 취소/추가 버튼 구현

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CreateCustomCourseScreen.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CreateCustomCourseScreen.kt
@@ -1,19 +1,32 @@
 package io.coursepick.coursepick.presentation.customcourse
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import io.coursepick.coursepick.R
@@ -47,6 +60,39 @@ fun CreateCustomCourseScreen(modifier: Modifier = Modifier) {
                     actionIconContentColor = colorResource(R.color.item_primary),
                 ),
         )
+        Row(
+            modifier = Modifier.align(alignment = Alignment.BottomCenter).fillMaxWidth()
+        ) {
+            Column{
+                IconButton(
+                    onClick = {},
+                    modifier =
+                        Modifier
+                            .clip(shape = CircleShape)
+                            .background(colorResource(R.color.background_primary)),
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.icon_undo),
+                        contentDescription = null,
+                        tint = colorResource(R.color.item_primary),
+                    )
+                }
+                Spacer(modifier = Modifier.size(10.dp))
+                IconButton(
+                    onClick = {},
+                    modifier =
+                        Modifier
+                            .clip(shape = CircleShape)
+                            .background(colorResource(R.color.background_primary)),
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = null,
+                        tint = colorResource(R.color.item_primary),
+                    )
+                }
+            }
+        }
     }
 }
 
