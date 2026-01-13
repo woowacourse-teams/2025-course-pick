4bf9193d0d7157f86e3f0f393d2e6a625383f175
DoableTuple <doabletuple@gmail.com>
2026-01-13 19:14:37 +0900

    refactor: 도메인 모델 변경에 따른 CourseItem 수정

--- android/app/src/main/java/io/coursepick/coursepick/presentation/course/CourseItem.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/course/CourseItem.kt
@@ -1,11 +1,7 @@
 package io.coursepick.coursepick.presentation.course
 
-import androidx.annotation.StringRes
-import io.coursepick.coursepick.R
+import io.coursepick.coursepick.domain.course.Coordinate
 import io.coursepick.coursepick.domain.course.Course
-import io.coursepick.coursepick.domain.course.InclineSummary
-import io.coursepick.coursepick.domain.course.Segment
-import io.coursepick.coursepick.presentation.model.Difficulty
 
 data class CourseItem(
     val course: Course,
@@ -22,24 +18,5 @@ data class CourseItem(
     val length: Int =
         course.length.meter.value
             .toInt()
-    val segments: List<Segment> = course.segments
-    val roadType: String = course.roadType
-    val difficulty: Difficulty = Difficulty(course.difficulty)
-
-    @StringRes
-    val inclineSummaryStringResourceId: Int = course.inclineSummary.stringResourceId
-
-    val isInclineSummaryUnknown: Boolean = course.inclineSummary == InclineSummary.UNKNOWN
-
-    private val InclineSummary.stringResourceId: Int
-        get() =
-            when (this) {
-                InclineSummary.MOSTLY_FLAT -> R.string.incline_summary_flat
-                InclineSummary.REPEATING_HILLS -> R.string.incline_summary_repeating_hills
-                InclineSummary.SOMETIMES_UPHILL -> R.string.incline_summary_sometimes_uphill
-                InclineSummary.SOMETIMES_DOWNHILL -> R.string.incline_summary_sometimes_downhill
-                InclineSummary.CONTINUOUS_UPHILL -> R.string.incline_summary_continuous_uphill
-                InclineSummary.CONTINUOUS_DOWNHILL -> R.string.incline_summary_continuous_downhill
-                InclineSummary.UNKNOWN -> R.string.incline_summary_continuous_unknown
-            }
+    val coordinate: List<Coordinate> = course.coordinates
 }
