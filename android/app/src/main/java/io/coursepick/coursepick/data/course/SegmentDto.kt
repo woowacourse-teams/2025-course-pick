f1f30f2d5c5919c88b0554e1d54994bfd2bd410c
DoableTuple <doabletuple@gmail.com>
2026-01-15 17:39:03 +0900

    refactor: 사용되지 않는 SegmentDto 제거

--- android/app/src/main/java/io/coursepick/coursepick/data/course/SegmentDto.kt
+++
@@ -1,9 +0,0 @@
-package io.coursepick.coursepick.data.course
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class SegmentDto(
-    private val inclineType: String,
-    val coordinates: List<CoordinateDto>,
-)
