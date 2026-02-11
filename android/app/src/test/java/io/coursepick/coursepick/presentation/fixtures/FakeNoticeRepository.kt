3ae2909db02da25c083a5db152194fafbf49e45a
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-02-11 10:19:56 +0900

    test: `FakeNoticeRepository`에서 불필요한 `verifiedLocations` 메서드 제거

--- android/app/src/test/java/io/coursepick/coursepick/presentation/fixtures/FakeNoticeRepository.kt
+++ android/app/src/test/java/io/coursepick/coursepick/presentation/fixtures/FakeNoticeRepository.kt
@@ -5,6 +5,4 @@ import io.coursepick.coursepick.domain.notice.NoticeRepository
 
 class FakeNoticeRepository : NoticeRepository {
     override suspend fun notice(id: String): Notice = NOTICE_FIXTURE
-
-    override suspend fun verifiedLocations(): Notice = NOTICE_FIXTURE
 }
