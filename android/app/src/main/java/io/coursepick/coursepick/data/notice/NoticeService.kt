a98bcbef0527ada2d1f1971b58ef5f5268dac80c
DoableTuple <doabletuple@gmail.com>
2026-03-02 16:11:19 +0900

    refactor: 실제 공지 API를 사용하도록 변경

--- android/app/src/main/java/io/coursepick/coursepick/data/notice/NoticeService.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/notice/NoticeService.kt
@@ -3,6 +3,6 @@ package io.coursepick.coursepick.data.notice
 import retrofit2.http.GET
 
 interface NoticeService {
-    @GET("notices/list")
+    @GET("notices")
     suspend fun notices(): List<NoticeDto>
 }
