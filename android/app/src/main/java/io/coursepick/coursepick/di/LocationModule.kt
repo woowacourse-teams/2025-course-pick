fbe015bfcf85e32cfdcae286caa9805c3714f734
DoableTuple <doabletuple@gmail.com>
2026-03-04 18:17:46 +0900

    refactor: provideLocationRepository에 @Singleton 어노테이션 추가

--- android/app/src/main/java/io/coursepick/coursepick/di/LocationModule.kt
+++ android/app/src/main/java/io/coursepick/coursepick/di/LocationModule.kt
@@ -8,11 +8,13 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.coursepick.coursepick.data.location.DefaultLocationRepository
 import io.coursepick.coursepick.domain.location.LocationRepository
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object LocationModule {
     @Provides
+    @Singleton
     fun provideLocationRepository(
         @ApplicationContext applicationContext: Context,
     ): LocationRepository = DefaultLocationRepository(applicationContext)
