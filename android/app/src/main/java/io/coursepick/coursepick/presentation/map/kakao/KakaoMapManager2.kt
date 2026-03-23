72c3c45bf0a795e8c9d7943c63bd885694f39538
DoableTuple <doabletuple@gmail.com>
2026-03-23 22:59:02 +0900

    refactor: KakaoMapManager2 구현

---
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/map/kakao/KakaoMapManager2.kt
@@ -0,0 +1,163 @@
+package io.coursepick.coursepick.presentation.map.kakao
+
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.MapGravity
+import com.kakao.vectormap.MapView
+import io.coursepick.coursepick.R
+import io.coursepick.coursepick.domain.course.Coordinate
+import io.coursepick.coursepick.domain.course.Scope
+import io.coursepick.coursepick.domain.location.Location
+import io.coursepick.coursepick.presentation.course.CourseItem
+import io.coursepick.coursepick.presentation.map.MapManager
+import timber.log.Timber
+
+class KakaoMapManager2(
+    private val mapView: MapView,
+    lifecycle: Lifecycle,
+) : MapManager,
+    DefaultLifecycleObserver {
+    private var kakaoMap: KakaoMap? = null
+    private var drawer: KakaoMapDrawer? = null
+    private val cameraController = KakaoMapCameraController(mapView.context)
+    private val eventHandler = KakaoMapEventHandler()
+
+    private var courses: List<CourseItem> = emptyList()
+
+    override val cameraPosition: Coordinate?
+        get() = kakaoMap?.cameraPosition?.position?.toCoordinate()
+
+    override val scope: Scope?
+        get() {
+            val screenDiagonalTop: LatLng = kakaoMap?.fromScreenPoint(0, 0) ?: return null
+            val mapCenter: Coordinate = cameraPosition ?: return null
+            val distance: Int =
+                DistanceCalculator.distance(mapCenter, screenDiagonalTop.toCoordinate())
+                    ?: return null
+            return Scope(distance)
+        }
+
+    init {
+        lifecycle.addObserver(this)
+    }
+
+    override fun startMap(onMapReady: () -> Unit) {
+        KakaoMapLifecycleHandler(mapView).start { map: KakaoMap ->
+            kakaoMap = map
+            drawer = KakaoMapDrawer(mapView.context, map)
+
+            val offsetPx: Float =
+                mapView.context.resources.getDimension(R.dimen.map_logo_position_offset)
+            map.logo?.setPosition(
+                MapGravity.BOTTOM or MapGravity.LEFT,
+                offsetPx,
+                offsetPx,
+            )
+
+            onMapReady()
+        }
+    }
+
+    override fun draw(course: CourseItem) {
+        courses = listOf(course)
+        drawer?.drawCourse(course) ?: Timber.w("KakaoMapDrawer is null")
+    }
+
+    override fun draw(courses: List<CourseItem>) {
+        this.courses = courses
+        drawer?.drawCourses(courses) ?: Timber.w("KakaoMapDrawer is null")
+    }
+
+    override fun drawRouteToCourse(
+        route: List<Coordinate>,
+        course: CourseItem,
+    ) {
+        courses = listOf(course)
+        drawer?.drawRouteToCourse(route, course) ?: Timber.w("KakaoMapDrawer is null")
+    }
+
+    override fun removeAllRouteLines() {
+        drawer?.removeAllLines() ?: Timber.w("KakaoMapDrawer is null")
+    }
+
+    override fun drawSearchPosition(coordinate: Coordinate) {
+        drawer?.drawSearchPosition(coordinate) ?: Timber.w("KakaoMapDrawer is null")
+    }
+
+    override fun drawUserPosition(location: Location) {
+        drawer?.drawUserPosition(location) ?: Timber.w("KakaoMapDrawer is null")
+    }
+
+    override fun hideUserPosition() {
+        drawer?.hideUserPosition() ?: Timber.w("KakaoMapDrawer is null")
+    }
+
+    override fun fitTo(coordinates: List<Coordinate>) {
+        kakaoMap?.let { kakaoMap: KakaoMap ->
+            cameraController.fitTo(coordinates, kakaoMap)
+        } ?: Timber.w("kakaoMap is null")
+    }
+
+    override fun fitTo(course: CourseItem) {
+        kakaoMap?.let { kakaoMap: KakaoMap ->
+            cameraController.fitTo(course, kakaoMap)
+        } ?: Timber.w("kakaoMap is null")
+    }
+
+    override fun setOnCourseClickListener(onClick: (CourseItem) -> Unit) {
+        kakaoMap?.let { kakaoMap: KakaoMap ->
+            eventHandler.setOnCourseClickListener(kakaoMap, courses) { course: CourseItem ->
+                onClick(course)
+            }
+        } ?: Timber.w("kakaoMap is null")
+    }
+
+    override fun setOnCameraMoveListener(onCameraMove: () -> Unit) {
+        kakaoMap?.let { kakaoMap: KakaoMap ->
+            eventHandler.setOnCameraMoveListener(kakaoMap) {
+                onCameraMove()
+            }
+        } ?: Timber.w("kakaoMap is null")
+    }
+
+    override fun moveTo(coordinate: Coordinate) {
+        kakaoMap?.let { kakaoMap: KakaoMap ->
+            cameraController.moveTo(kakaoMap, coordinate)
+        } ?: Timber.w("kakaoMap is null")
+    }
+
+    override fun resetZoom() {
+        kakaoMap?.let { kakaoMap: KakaoMap ->
+            cameraController.resetZoomLevel(kakaoMap)
+        } ?: Timber.w("kakaoMap is null")
+    }
+
+    override fun setPadding(
+        left: Int,
+        top: Int,
+        right: Int,
+        bottom: Int,
+    ) {
+        kakaoMap?.let { kakaoMap: KakaoMap ->
+            kakaoMap.setPadding(0, 0, 0, bottom)
+        } ?: Timber.w("kakaoMap is null")
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
+        mapView.pause()
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        super.onResume(owner)
+        mapView.resume()
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        mapView.finish()
+    }
+}
