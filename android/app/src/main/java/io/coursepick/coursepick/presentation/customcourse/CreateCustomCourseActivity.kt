b8b76bc83fbb1c569d454c56069a4e3a6cf415f3
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-04-04 16:11:10 +0900

    feat: `나의 코스 만들기` 화면에 지도 추가

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CreateCustomCourseActivity.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CreateCustomCourseActivity.kt
@@ -3,14 +3,32 @@ package io.coursepick.coursepick.presentation.customcourse
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import io.coursepick.coursepick.databinding.ActivityCustomCourseBinding
+import io.coursepick.coursepick.presentation.InstallStateObserver
+import io.coursepick.coursepick.presentation.map.MapManager
+import io.coursepick.coursepick.presentation.map.MapManagerFactory
 import io.coursepick.coursepick.presentation.search.ui.theme.CoursePickTheme
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class CreateCustomCourseActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityCustomCourseBinding.inflate(layoutInflater) }
+
+    @Inject
+    lateinit var mapManagerFactory: MapManagerFactory
+    private val mapManager: MapManager by lazy { mapManagerFactory.create(binding.mapContainer) }
+
+    init {
+        lifecycle.addObserver(InstallStateObserver(this))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContentView(binding.root)
+        mapManager.startMap {}
+        binding.composeView.setContent {
             CoursePickTheme {
                 CreateCustomCourseScreen(onClose = { finish() })
             }
