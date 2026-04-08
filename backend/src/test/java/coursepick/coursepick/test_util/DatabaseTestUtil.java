bb2d462f8987244b6ddcdf5cb096b81311025b68
choizz156 <tkfka156@gmail.com>
2026-04-08 15:36:08 +0900

    test: findCourseByName 쿼리 추가

--- backend/src/test/java/coursepick/coursepick/test_util/DatabaseTestUtil.java
+++ backend/src/test/java/coursepick/coursepick/test_util/DatabaseTestUtil.java
@@ -4,9 +4,11 @@ import coursepick.coursepick.domain.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @TestComponent
 public class DatabaseTestUtil {
@@ -32,5 +34,10 @@ public class DatabaseTestUtil {
 
     public Course findCourseById(String id) {
         return mongoTemplate.findById(id, Course.class);
+    }
+
+    public Course findCourseByName(String name) {
+        Query query = Query.query(Criteria.where("name").is(name));
+        return mongoTemplate.findOne(query, Course.class);
     }
 }
