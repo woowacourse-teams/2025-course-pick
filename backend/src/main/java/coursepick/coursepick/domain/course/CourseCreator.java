8608a606132480cb4ead71756fcaa54ced434fd3
choizz156 <tkfka156@gmail.com>
2026-04-09 12:07:18 +0900

    refactor: 불필요 팩터리 메서드 제거

--- backend/src/main/java/coursepick/coursepick/domain/course/CourseCreator.java
+++ backend/src/main/java/coursepick/coursepick/domain/course/CourseCreator.java
@@ -3,12 +3,4 @@ package coursepick.coursepick.domain.course;
 public record CourseCreator(
         String id
 ) {
-
-    public static CourseCreator fromAdmin() {
-        return new CourseCreator("admin");
-    }
-
-    public static CourseCreator fromUser(String userId) {
-        return new CourseCreator(userId);
-    }
 }
