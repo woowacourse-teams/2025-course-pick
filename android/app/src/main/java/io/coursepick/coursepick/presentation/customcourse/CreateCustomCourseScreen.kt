46e3ab8d5b963bf90e7621ddd34a47c7c57922e5
DoableTuple <doabletuple@gmail.com>
2026-04-11 20:49:14 +0900

    refactor: 사용되지 않는 import 제거

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CreateCustomCourseScreen.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CreateCustomCourseScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
