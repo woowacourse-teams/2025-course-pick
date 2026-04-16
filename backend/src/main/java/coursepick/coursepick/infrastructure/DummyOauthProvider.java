c931c24cbe3c55c546730fa654c5a364e0ef47de
choizz156 <tkfka156@gmail.com>
2026-04-16 18:55:58 +0900

    refactor: dummy provderId 랜덤 생성
    로컬에서 다수 유저 생성을 위함

--- backend/src/main/java/coursepick/coursepick/infrastructure/DummyOauthProvider.java
+++ backend/src/main/java/coursepick/coursepick/infrastructure/DummyOauthProvider.java
@@ -1,5 +1,7 @@
 package coursepick.coursepick.infrastructure;
 
+import java.util.UUID;
+
 import coursepick.coursepick.domain.user.OAuthProvider;
 import org.springframework.context.annotation.Fallback;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,6 @@ public class DummyOauthProvider implements OAuthProvider {
 
     @Override
     public String getProviderId(String oauthAccessToken) {
-        return "authentication-complete";
+        return UUID.randomUUID().toString();
     }
 }
