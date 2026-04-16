f13b7c54eb11894831682eac9ae9ded564ff7b86
choizz156 <tkfka156@gmail.com>
2026-04-17 04:13:56 +0900

    refactor: course report 메서드 역할 분리

--- backend/src/main/java/coursepick/coursepick/application/CourseApplicationService.java
+++ backend/src/main/java/coursepick/coursepick/application/CourseApplicationService.java
@@ -7,6 +7,7 @@ import coursepick.coursepick.application.exception.ErrorType;
 import coursepick.coursepick.domain.course.*;
 import coursepick.coursepick.domain.user.User;
 import coursepick.coursepick.domain.user.UserRepository;
+import coursepick.coursepick.infrastructure.discord.DiscordAlerter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.Nullable;
@@ -28,15 +29,15 @@ public class CourseApplicationService {
     private final UserRepository userRepository;
     private final RouteFinder routeFinder;
     private final UserRepository userRepository;
-    private final Alerter alerter;
-    private final Environment environment;
+    private final DiscordAlerter discordAlerter;
+
 
 
     @Transactional
     public void addCustomCourse(String name, List<Coordinate> coordinates, String userId) {
         CourseName courseName = new CourseName(name);
         validateDuplicatedCourseName(courseName);
-        User user = findUser(userId);
+        User user = getUser(userId);
 
         Course newCourse = new Course(null, courseName, coordinates, user);
         courseRepository.save(newCourse);
@@ -46,12 +47,14 @@ public class CourseApplicationService {
     public void report(String courseId, String userId) {
         Course course = courseRepository.findById(courseId)
                 .orElseThrow(() -> NOT_EXIST_COURSE.create(courseId));
-        User user = findUser(userId);
-        String activeProfile = String.join(",", environment.getActiveProfiles());
+        User user = getUser(userId);
 
-        course.report(user, alerter, activeProfile);
-
+        course.addReport(user);
         courseRepository.save(course);
+
+        if (course.isReportThreshold()) {
+            discordAlerter.alert(course);
+        }
     }
 
     private void validateDuplicatedCourseName(CourseName courseName) {
@@ -66,7 +69,7 @@ public class CourseApplicationService {
         return CoursesResponse.from(coursesWithinScope, createUserPositionOrNull(userLatitude, userLongitude));
     }
 
-    private User findUser(String userId) {
+    private User getUser(String userId) {
         return userRepository.findById(userId).
                 orElseThrow(() -> NOT_EXIST_USER.create(userId));
     }
