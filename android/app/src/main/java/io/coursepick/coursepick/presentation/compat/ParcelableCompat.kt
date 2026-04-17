6a95ef2b61c1e56fe7e6f057ff15ba9a38dca997
DoableTuple <doabletuple@gmail.com>
2026-04-17 20:15:42 +0900

    refactor: BundleCompat 및 IntentCompat을 사용하도록 변경

--- android/app/src/main/java/io/coursepick/coursepick/presentation/compat/ParcelableCompat.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/compat/ParcelableCompat.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.core.content.IntentCompat
+import androidx.core.os.BundleCompat
 
 inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String): T? =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelable(key, T::class.java)
+        BundleCompat.getParcelable(this, key, T::class.java)
     } else {
         @Suppress("DEPRECATION")
         getParcelable(key) as? T
@@ -15,7 +17,7 @@ inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String): T? 
 
 inline fun <reified T : Parcelable> Intent.getParcelableCompat(key: String): T? =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelableExtra(key, T::class.java)
+        IntentCompat.getParcelableExtra(this, key, T::class.java)
     } else {
         @Suppress("DEPRECATION")
         getParcelableExtra(key) as? T
