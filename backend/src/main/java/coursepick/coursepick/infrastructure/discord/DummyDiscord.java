3a7dfb73e65a788c52c0d6b07f3667bc0eeb946d
kirby <mmm307955@gmail.com>
2026-04-19 18:38:53 +0900

    refactor: DummyDiscord를 CourseReportAlerter를 구현하도록 수정

--- backend/src/main/java/coursepick/coursepick/infrastructure/discord/DummyDiscord.java
+++ backend/src/main/java/coursepick/coursepick/infrastructure/discord/DummyDiscord.java
@@ -1,5 +1,7 @@
 package coursepick.coursepick.infrastructure.discord;
 
+import coursepick.coursepick.application.CourseReportAlerter;
+import coursepick.coursepick.domain.course.Course;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Fallback;
 import org.springframework.stereotype.Component;
@@ -7,9 +9,23 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @Fallback
-public class DummyDiscord {
+public class DummyDiscord implements CourseReportAlerter {
 
-    public void alert(String message) {
+    @Override
+    public void alert(Course course) {
+        String message = generateReportMessage(course);
         log.info("[DummyDiscord] {}", message);
+    }
+
+    private String generateReportMessage(Course course) {
+
+        String reporterIds = String.join(", ", course.reportUserIds());
+        return """
+                [%s] 코스 신고 알림
+                - 코스 ID: %s
+                - 코스 이름: %si
+                - 신고 수: %d
+                - 신고자 ID: [%s]
+                """.formatted("local", course.id(), course.name().value(), course.reportUserIds().size(), reporterIds);
     }
 }
