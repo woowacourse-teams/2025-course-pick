cb64255adda314157d19c0144f1519d518911bab
DoableTuple <doabletuple@gmail.com>
2026-04-22 21:41:14 +0900

    refactor: DraftCourseDto 데이터 형식 변경

--- android/app/src/main/java/io/coursepick/coursepick/data/customcourse/DraftCourseDto.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/customcourse/DraftCourseDto.kt
@@ -1,22 +1,19 @@
 package io.coursepick.coursepick.data.customcourse
 
-import io.coursepick.coursepick.domain.course.Coordinate
+import io.coursepick.coursepick.data.course.CoordinateDto
 import io.coursepick.coursepick.domain.customcourse.DraftCourse
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DraftCourseDto(
     val name: String,
-    val coordinates: List<List<Double>>,
+    val coordinates: List<CoordinateDto>,
 ) {
     companion object {
         operator fun invoke(draftCourse: DraftCourse): DraftCourseDto =
             DraftCourseDto(
                 name = draftCourse.name.value,
-                coordinates =
-                    draftCourse.coordinates.map { coordinate: Coordinate ->
-                        listOf(coordinate.latitude.value, coordinate.longitude.value)
-                    },
+                coordinates = draftCourse.coordinates.map(CoordinateDto::invoke),
             )
     }
 }
