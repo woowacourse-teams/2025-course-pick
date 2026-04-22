c5682e8000d0a03bf27a1f67a2f6e062734d4ba0
kirby <mmm307955@gmail.com>
2026-04-22 20:03:14 +0900

    refactor: 코스 파스 시 코스파일 객체와 유저 객체를 받도록 수정

--- backend/src/main/java/coursepick/coursepick/application/CourseParserFacade.java
+++ backend/src/main/java/coursepick/coursepick/application/CourseParserFacade.java
@@ -36,6 +36,16 @@ public class CourseParserFacade {
         return result;
     }
 
+    public List<Course> parse(CourseFile file, User user) {
+        CourseParser parser = findParser(file);
+        log.debug("코스 파싱을 시작합니다. 선택된 구현체={}", parser.getClass().getSimpleName());
+
+        List<Course> result = parser.parse(file, user);
+
+        log.debug("{}개의 코스를 파싱했습니다.", result.size());
+        return result;
+    }
+
     private CourseParser findParser(CourseFile file) {
         return parsers.stream()
                 .filter(parser -> parser.canParse(file))
