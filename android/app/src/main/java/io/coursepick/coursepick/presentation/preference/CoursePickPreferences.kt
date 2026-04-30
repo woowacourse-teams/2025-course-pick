aef8296134a1b877661c5a0e81eb73ce3da064b0
DoableTuple <doabletuple@gmail.com>
2026-05-01 02:07:11 +0900

    refactor: 사용되지 않는 파일 및 프로퍼티 제거

--- android/app/src/main/java/io/coursepick/coursepick/presentation/preference/CoursePickPreferences.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/preference/CoursePickPreferences.kt
@@ -11,69 +11,15 @@ import io.coursepick.coursepick.presentation.routefinder.RouteFinderApplication
 
 object CoursePickPreferences {
     private lateinit var preferences: SharedPreferences
-    private lateinit var selectedRouteFinderApplicationKey: String
     private lateinit var favoritedCoursesKey: String
     private lateinit var doNotShowNoticesKey: String
-    private lateinit var inApp: String
-    private lateinit var kakaoMap: String
-    private lateinit var naverMap: String
-    private lateinit var none: String
 
-    private val mapPreferencesChangeListener =
-        OnSharedPreferenceChangeListener { _, key: String? ->
-            when (key) {
-                selectedRouteFinderApplicationKey -> {
-                    Logger.log(
-                        Logger.Event.PreferenceChange(key),
-                        "map_preference_change" to preferences.getString(key, null).toString(),
-                    )
-                }
-
-                favoritedCoursesKey -> {
-                    Logger.log(
-                        Logger.Event.PreferenceChange(key),
-                        "map_preference_change" to
-                            preferences
-                                .getStringSet(key, null)
-                                ?.joinToString()
-                                .toString(),
-                    )
-                }
-            }
-        }
 
     fun init(context: Context) {
         preferences = PreferenceManager.getDefaultSharedPreferences(context)
-
-        selectedRouteFinderApplicationKey =
-            context.getString(R.string.selected_route_finder_application_key)
         favoritedCoursesKey = context.getString(R.string.favorited_courses_key)
         doNotShowNoticesKey = context.getString(R.string.do_not_show_notices_key)
-
-        inApp = context.getString(R.string.selected_route_finder_application_value_in_app)
-        kakaoMap = context.getString(R.string.selected_route_finder_application_value_kakao)
-        naverMap = context.getString(R.string.selected_route_finder_application_value_naver)
-        none = context.getString(R.string.selected_route_finder_application_value_none)
-
-        preferences.registerOnSharedPreferenceChangeListener(mapPreferencesChangeListener)
     }
-
-    var selectedRouteFinder: RouteFinderApplication?
-        get() =
-            when (preferences.getString(selectedRouteFinderApplicationKey, null)) {
-                inApp -> RouteFinderApplication.InApp
-                kakaoMap -> RouteFinderApplication.KakaoMap
-                naverMap -> RouteFinderApplication.NaverMap
-                else -> null
-            }
-        set(mapApplication) {
-            preferences.edit {
-                putString(
-                    selectedRouteFinderApplicationKey,
-                    mapApplication.serialized,
-                )
-            }
-        }
 
     fun favoritedCourseIds(): Set<String> = preferences.getStringSet(favoritedCoursesKey, emptySet()) ?: emptySet()
 
@@ -110,13 +56,4 @@ object CoursePickPreferences {
             putStringSet(doNotShowNoticesKey, doNotShowNoticeIds.intersect(currentNoticeIds))
         }
     }
-
-    private val RouteFinderApplication?.serialized: String
-        get() =
-            when (this) {
-                is RouteFinderApplication.InApp -> inApp
-                is RouteFinderApplication.KakaoMap -> kakaoMap
-                is RouteFinderApplication.NaverMap -> naverMap
-                null -> none
-            }
 }
