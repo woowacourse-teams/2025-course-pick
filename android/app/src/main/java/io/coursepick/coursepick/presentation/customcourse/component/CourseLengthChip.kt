1dad3eccae0c53d287e8ae55adf0e75e964606ff
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-05-08 18:43:04 +0900

    refactor: 자 아이콘 색상 `item_primary`로 변경

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/component/CourseLengthChip.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/component/CourseLengthChip.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -47,7 +48,7 @@ fun CourseLengthChip(
         Icon(
             painter = painterResource(R.drawable.icon_length),
             contentDescription = null,
-            tint = colorResource(R.color.item_primary),
+            tint = Color.Unspecified,
         )
 
         Spacer(modifier = Modifier.width(6.dp))
