d59c52006b95d7da14fc41fec598e1fa63994b73
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-05-09 15:33:46 +0900

    refactor: 사용하지 않는 import 제거

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/component/CourseLengthChip.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/component/CourseLengthChip.kt
@@ -1,6 +1,5 @@
 package io.coursepick.coursepick.presentation.customcourse.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
