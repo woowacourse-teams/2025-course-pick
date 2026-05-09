21838fe805ebb11b25ddb965d226685da63c73e8
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-05-09 15:34:46 +0900

    feat: 나의 코스 조회 api 연결

--- android/app/src/main/java/io/coursepick/coursepick/data/customcourse/CustomCourseService.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/customcourse/CustomCourseService.kt
@@ -1,8 +1,11 @@
 package io.coursepick.coursepick.data.customcourse
 
+import io.coursepick.coursepick.data.course.CoursesPageDto
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface CustomCourseService {
     @POST("courses/draft/route")
@@ -14,4 +17,10 @@ interface CustomCourseService {
     suspend fun submitCourse(
         @Body course: DraftCourseDto,
     ): Response<Unit>
+
+    @GET("courses/custom")
+    suspend fun customCourses(
+        @Query("userLat") userLatitude: Double?,
+        @Query("userLng") userLongitude: Double?,
+    ): CoursesPageDto
 }
