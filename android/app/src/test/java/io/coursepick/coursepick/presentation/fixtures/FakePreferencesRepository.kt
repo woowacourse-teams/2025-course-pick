1abe5b565097ec114aaa9326b3dde10919c536c8
DoableTuple <doabletuple@gmail.com>
2026-05-10 22:22:20 +0900

    test: FakePreferencesRepository 구현

---
+++ android/app/src/test/java/io/coursepick/coursepick/presentation/fixtures/FakePreferencesRepository.kt
@@ -0,0 +1,18 @@
+package io.coursepick.coursepick.presentation.fixtures
+
+import io.coursepick.coursepick.domain.preference.PreferencesRepository
+import io.coursepick.coursepick.domain.preference.RouteFinder
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class FakePreferencesRepository(
+    initialRouteFinder: RouteFinder? = null,
+) : PreferencesRepository {
+    private val _routeFinder = MutableStateFlow(initialRouteFinder)
+    override val routeFinder: Flow<RouteFinder?> get() = _routeFinder.asStateFlow()
+
+    override suspend fun setRouteFinder(routeFinder: RouteFinder?) {
+        _routeFinder.value = routeFinder
+    }
+}
