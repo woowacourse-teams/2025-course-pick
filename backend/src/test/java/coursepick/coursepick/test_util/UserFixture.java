77eaa2cc179fe79191d478cb0c5fd3c9983895bd
dompoo <dlckdrms0517@gmail.com>
2026-05-11 22:20:21 +0900

    test: 테스트용 User가 제대로 선언되도록 수정

--- backend/src/test/java/coursepick/coursepick/test_util/UserFixture.java
+++ backend/src/test/java/coursepick/coursepick/test_util/UserFixture.java
@@ -1,7 +1,8 @@
 package coursepick.coursepick.test_util;
 
 import coursepick.coursepick.domain.user.User;
+import coursepick.coursepick.domain.user.UserProvider;
 
 public abstract class UserFixture {
-    public static final User ADMIN_USER = new User(null, "adminId", "admin");
+    public static final User ADMIN_USER = new User(UserProvider.NONE, "adminId", "admin");
 }
