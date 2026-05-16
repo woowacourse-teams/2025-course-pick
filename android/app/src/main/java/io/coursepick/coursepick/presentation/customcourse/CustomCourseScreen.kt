5c8fa99ef93bb7944a577897c83ac7f4cd8f7eae
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-05-16 18:05:44 +0900

    refactor: 사용하지 않는 import 삭제

--- android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseScreen.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/customcourse/CustomCourseScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.animateItem
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
