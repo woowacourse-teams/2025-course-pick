5740ff18f1fe6b0b7c21dfe3a52a9257e1bcc489
kirby <mmm307955@gmail.com>
2026-05-17 01:52:48 +0900

    refactor: Review에서 ObjectId FQCN을 import로 변경

--- backend/src/main/java/coursepick/coursepick/domain/course/Review.java
+++ backend/src/main/java/coursepick/coursepick/domain/course/Review.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.PersistenceCreator;
 
 import java.time.Instant;
@@ -30,7 +31,7 @@ public class Review {
     private final Instant createdAt;
 
     public Review(User author, String content, int rating) {
-        this(new org.bson.types.ObjectId().toHexString(), author.id(), author.nickname().value(), content, rating, new HashSet<>(), Instant.now());
+        this(new ObjectId().toHexString(), author.id(), author.nickname().value(), content, rating, new HashSet<>(), Instant.now());
         validateContent(content);
         validateRating(rating);
     }
