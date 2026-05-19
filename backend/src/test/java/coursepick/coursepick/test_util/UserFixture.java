3c0384b5bd724510988fbaa292d685074a955a8d
choizz156 <tkfka156@gmail.com>
2026-05-19 23:22:02 +0900

    refactor: CourseFixture 추가 및 UserFixture 테스트 데이터 수정

--- backend/src/test/java/coursepick/coursepick/test_util/UserFixture.java
+++ backend/src/test/java/coursepick/coursepick/test_util/UserFixture.java
@@ -5,8 +5,8 @@ import coursepick.coursepick.domain.user.User;
 import coursepick.coursepick.domain.user.UserProvider;
 
 public abstract class UserFixture {
-    public static final User ADMIN_USER = new User(null, "adminId", "admin");
-    public static final User TEST_USER = new User("userId1", UserProvider.KAKAO, "proviederId", new Nickname("testUser"));
-    public static final User TEST_USER2 = new User("userId2", UserProvider.KAKAO, "proviederId", new Nickname("testUser2"));
-    public static final User TEST_USER3 = new User("userId3", UserProvider.KAKAO, "proviederId", new Nickname("testUser3"));
+    public static final User ADMIN_USER = new User("673c242c332e2c244c000001", UserProvider.NONE, "adminId", new Nickname("admin"));
+    public static final User TEST_USER = new User("673c242c332e2c244c000002", UserProvider.KAKAO, "proviederId1", new Nickname("testUser"));
+    public static final User TEST_USER2 = new User("673c242c332e2c244c000003", UserProvider.KAKAO, "proviederId2", new Nickname("testUser2"));
+    public static final User TEST_USER3 = new User("673c242c332e2c244c000004", UserProvider.KAKAO, "proviederId3", new Nickname("testUser3"));
 }
