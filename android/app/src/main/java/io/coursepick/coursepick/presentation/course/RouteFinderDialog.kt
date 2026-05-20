e3477f79884ea9f9c5a886f7b299179b6abb6f54
DoableTuple <doabletuple@gmail.com>
2026-05-20 14:39:44 +0900

    refactor: configuration change 발생 시 기본 앱으로 설정하기 체크 상태가 유지되도록 변경

--- android/app/src/main/java/io/coursepick/coursepick/presentation/course/RouteFinderDialog.kt
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/course/RouteFinderDialog.kt
@@ -16,7 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -66,7 +66,7 @@ fun RouteFinderDialog(
 
             Spacer(Modifier.height(16.dp))
 
-            var rememberChoice by remember { mutableStateOf(false) }
+            var rememberChoice by rememberSaveable { mutableStateOf(false) }
 
             RouteFinderOptions(
                 onSelectOption = { option: RouteFinderApplication -> onConfirm(option.routeFinder, rememberChoice) },
