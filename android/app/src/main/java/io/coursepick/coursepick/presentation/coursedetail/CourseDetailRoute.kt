bb775f2d5f1d863acd4941f20e8948257c4fde59
DoableTuple <doabletuple@gmail.com>
2026-05-22 14:55:17 +0900

    refactor: CourseDetailActivity에 내비게이션 적용

---
+++ android/app/src/main/java/io/coursepick/coursepick/presentation/coursedetail/CourseDetailRoute.kt
@@ -0,0 +1,12 @@
+package io.coursepick.coursepick.presentation.coursedetail
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+sealed interface CourseDetailRoute : NavKey {
+    @Serializable
+    data object CourseDetail : CourseDetailRoute
+
+    @Serializable
+    data object WriteReview : CourseDetailRoute
+}
