778b190f161e4684da4d000fef6566172b9445d3
DoableTuple <doabletuple@gmail.com>
2026-05-24 15:21:45 +0900

    refactor: DataStore 읽기 시 IOException 예외 처리 추가

--- android/app/src/main/java/io/coursepick/coursepick/data/favorites/FavoritesDataSource.kt
+++ android/app/src/main/java/io/coursepick/coursepick/data/favorites/FavoritesDataSource.kt
@@ -1,12 +1,15 @@
 package io.coursepick.coursepick.data.favorites
 
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.IOException
 import androidx.datastore.preferences.core.MutablePreferences
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringSetPreferencesKey
 import io.coursepick.coursepick.di.Favorites
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -16,9 +19,9 @@ class FavoritesDataSource
         @Favorites private val dataStore: DataStore<Preferences>,
     ) {
         val courseIds: Flow<Set<String>> =
-            dataStore.data.map { preferences: Preferences ->
-                preferences[FAVORITE_COURSE_IDS_KEY] ?: emptySet()
-            }
+            dataStore.data
+                .catch { exception: Throwable -> if (exception is IOException) emit(emptyPreferences()) else throw exception }
+                .map { preferences: Preferences -> preferences[FAVORITE_COURSE_IDS_KEY] ?: emptySet() }
 
         suspend fun addFavorite(courseId: String) {
             dataStore.edit { preferences: MutablePreferences ->
