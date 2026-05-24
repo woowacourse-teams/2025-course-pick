6e6134c280442e9e4ada23a0ce273ba02a12a1e1
DoableTuple <doabletuple@gmail.com>
2026-05-24 15:46:23 +0900

    test: FakeNoticeRepository에 공지 다시 보지 않기 기능 구현

--- android/app/src/test/java/io/coursepick/coursepick/presentation/fixtures/FakeNoticeRepository.kt
+++ android/app/src/test/java/io/coursepick/coursepick/presentation/fixtures/FakeNoticeRepository.kt
@@ -4,5 +4,11 @@ import io.coursepick.coursepick.domain.notice.Notice
 import io.coursepick.coursepick.domain.notice.NoticeRepository
 
 class FakeNoticeRepository : NoticeRepository {
-    override suspend fun notices(): List<Notice> = listOf(NOTICE_FIXTURE)
+    private var mutedNoticeIds = emptySet<String>()
+
+    override suspend fun notices(): List<Notice> = listOf(NOTICE_FIXTURE).filterNot { notice: Notice -> mutedNoticeIds.contains(notice.id) }
+
+    override suspend fun muteNotice(noticeId: String) {
+        mutedNoticeIds += noticeId
+    }
 }
