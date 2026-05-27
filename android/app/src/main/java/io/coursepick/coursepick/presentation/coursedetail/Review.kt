2b4717d49a8c8230dec33a6632b928c94b4499ee
DoableTuple <doabletuple@gmail.com>
2026-05-27 16:55:05 +0900

    refactor: 사용되지 않는 클래스 제거

--- android/app/src/main/java/io/coursepick/coursepick/presentation/coursedetail/Review.kt
+++
@@ -1,9 +0,0 @@
-package io.coursepick.coursepick.presentation.coursedetail
-
-data class Review(
-    val id: String,
-    val username: String,
-    val isMine: Boolean,
-    val rating: Float,
-    val comment: String,
-)
