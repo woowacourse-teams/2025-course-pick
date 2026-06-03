3e4e89790d4c24fccd18a71ab46bf19cdf1c2c50
dompoo <dlckdrms0517@gmail.com>
2026-06-03 15:33:40 +0900

    test: 추가된 기능에 대한 서비스 테스트 추가

--- backend/src/test/java/coursepick/coursepick/application/CourseApplicationServiceTest.java
+++ backend/src/test/java/coursepick/coursepick/application/CourseApplicationServiceTest.java
@@ -1,5 +1,7 @@
 package coursepick.coursepick.application;
 
+import coursepick.coursepick.application.dto.CourseFile;
+import coursepick.coursepick.application.dto.CourseFileExtension;
 import coursepick.coursepick.application.dto.CourseResponse;
 import coursepick.coursepick.application.exception.UnauthorizedException;
 import coursepick.coursepick.domain.course.*;
@@ -21,6 +23,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static coursepick.coursepick.test_util.CourseFixture.*;
+import static coursepick.coursepick.test_util.GpxTestUtil.createGpxInputStreamOf;
 import static coursepick.coursepick.test_util.UserFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -354,6 +357,58 @@ class CourseApplicationServiceTest extends AbstractIntegrationTest {
 
             var result = dbUtil.findCourseByName(expectedName);
             assertThat(result.name().value()).isEqualTo(expectedName);
+        }
+    }
+
+    @Nested
+    class GPX_파일_코스_등록 {
+
+        @Test
+        void GPX_파일로_코스를_저장한다() {
+            var inputStream = createGpxInputStreamOf(
+                    new Coordinate(0, 0),
+                    new Coordinate(0.00001, 0.00001),
+                    new Coordinate(0, 0)
+            );
+            var courseFile = new CourseFile("나만의 GPX 코스", CourseFileExtension.GPX, inputStream);
+
+            sut.addGpxCourse(courseFile, TEST_USER.id());
+
+            var result = dbUtil.findCourseByName("나만의 GPX 코스");
+            assertThat(result.name().value()).isEqualTo("나만의 GPX 코스");
+            assertThat(result.creatorId()).isEqualTo(TEST_USER.id());
+            assertThat(result.coordinates()).containsExactly(
+                    new Coordinate(0, 0),
+                    new Coordinate(0.00001, 0.00001),
+                    new Coordinate(0, 0)
+            );
+        }
+
+        @Test
+        void 중복된_코스_이름이면_예외가_발생한다() {
+            dbUtil.saveCourse(createCourse("나만의 GPX 코스", 한강_좌표));
+            var inputStream = createGpxInputStreamOf(
+                    new Coordinate(0, 0),
+                    new Coordinate(0.00001, 0.00001),
+                    new Coordinate(0, 0)
+            );
+            var courseFile = new CourseFile("나만의 GPX 코스", CourseFileExtension.GPX, inputStream);
+
+            assertThatThrownBy(() -> sut.addGpxCourse(courseFile, TEST_USER.id()))
+                    .isInstanceOf(IllegalStateException.class);
+        }
+
+        @Test
+        void 유저가_존재하지_않을_경우_예외가_발생한다() {
+            var inputStream = createGpxInputStreamOf(
+                    new Coordinate(0, 0),
+                    new Coordinate(0.00001, 0.00001),
+                    new Coordinate(0, 0)
+            );
+            var courseFile = new CourseFile("나만의 GPX 코스", CourseFileExtension.GPX, inputStream);
+
+            assertThatThrownBy(() -> sut.addGpxCourse(courseFile, "notExistUserId"))
+                    .isInstanceOf(UnauthorizedException.class);
         }
     }
 
