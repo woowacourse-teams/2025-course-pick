a58304db661afcec4c5289ae59f26a42ccd0c370
DoableTuple <doabletuple@gmail.com>
2026-06-09 22:34:44 +0900

    refactor: AccessTokenInterceptor가 AuthRepository2를 사용하도록 변경

--- android/app/src/main/java/io/coursepick/coursepick/di/RepositoryModule.kt
+++ android/app/src/main/java/io/coursepick/coursepick/di/RepositoryModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.coursepick.coursepick.data.auth.DefaultAuthRepository
+import io.coursepick.coursepick.data.auth.DefaultAuthRepository2
 import io.coursepick.coursepick.data.course.DefaultCourseRepository
 import io.coursepick.coursepick.data.customcourse.DefaultCustomCourseRepository
 import io.coursepick.coursepick.data.favorites.DefaultFavoriteCourseRepository
@@ -12,6 +13,7 @@ import io.coursepick.coursepick.data.notice.DefaultNoticeRepository
 import io.coursepick.coursepick.data.preference.DefaultPreferencesRepository
 import io.coursepick.coursepick.data.search.DefaultSearchRepository
 import io.coursepick.coursepick.domain.auth.AuthRepository
+import io.coursepick.coursepick.domain.auth.AuthRepository2
 import io.coursepick.coursepick.domain.course.CourseRepository
 import io.coursepick.coursepick.domain.customcourse.CustomCourseRepository
 import io.coursepick.coursepick.domain.favorites.FavoriteCourseRepository
@@ -36,6 +38,9 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindAuthRepository(implementation: DefaultAuthRepository): AuthRepository
+
+    @Binds
+    abstract fun bindAuthRepository2(implementation: DefaultAuthRepository2): AuthRepository2
 
     @Binds
     abstract fun bindCustomCourseRepository(implementation: DefaultCustomCourseRepository): CustomCourseRepository
