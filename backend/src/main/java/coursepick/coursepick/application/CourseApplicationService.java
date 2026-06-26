7ff3428df53ec2bdab3583b0b748eed09dc7422f
kirby <mmm307955@gmail.com>
2026-06-26 20:21:27 +0900

    feat: 서비스에 deleteCourse() 추가

--- backend/src/main/java/coursepick/coursepick/application/CourseApplicationService.java
+++ backend/src/main/java/coursepick/coursepick/application/CourseApplicationService.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.Nullable;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -42,6 +41,14 @@ public class CourseApplicationService {
 
         Course newCourse = new Course(null, courseName, coordinates, user);
         courseRepository.save(newCourse);
+    }
+
+    @Transactional
+    public void deleteCourse(String courseId, String userId) {
+        Course course = getCourse(courseId);
+        course.verifyDeletable(userId);
+
+        courseRepository.delete(course);
     }
 
     @Transactional
